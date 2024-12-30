929123622a20986bcc9fe5e36b71e7f47982977f
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2024-12-30 12:57:57 +0530

    config project settings and customuser model

--- project/apps/library/views.py
+++ project/apps/library/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from django.db.models import Q
-from django.contrib import messages
+# from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+# from django.core.paginator import Paginator
+# from django.db.models import Q
+# from django.contrib import messages
 
-from utils.general import model_helpers
-from . import forms, models
-from htmx.hx_library.views import SearchList
+# from utils.general import model_helpers
+# from . import forms, models
+# from htmx.hx_library.views import SearchList
 
-import json
+# import json
 
-def index(request):
-    form = forms.SearchForm(data=request.GET)
-    return render(request, 'library/index.html', {'form':form})
+# def index(request):
+#     form = forms.SearchForm(data=request.GET)
+#     return render(request, 'library/index.html', {'form':form})
 
