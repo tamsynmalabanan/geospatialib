b7a1be823f0fd25e4288b10eabc35ff23b44abaf
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2024-12-31 10:18:40 +0530

    config project settings and customuser model

--- project/static/base/js/theme.js
+++ project/static/base/js/theme.js
@@ -24,7 +24,7 @@ const setAsThemedControl = (element) => {
 const getThemedControls = (parent=document) => {
     return [
         {
-            elements: parent.querySelectorAll(`[onclick='toggleDarkMode(event)']`).children[0],
+            elements: parent.querySelectorAll(`[onclick='toggleDarkMode(event)']`),
             classes: {
                 light: ['bi-moon'],
                 dark: ['bi-moon-fill'],
