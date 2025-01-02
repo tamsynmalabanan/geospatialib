ed418d5857f16d3a5c7966d4c6d5986016fb9047
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-02 15:43:47 +0530

    config project settings and customuser model

--- project/utils/gis/dataset_helpers.py
+++ project/utils/gis/dataset_helpers.py
@@ -5,6 +5,7 @@ from django.contrib.gis.gdal import SpatialReference
 from owslib import wms, wfs
 from urllib.parse import urlparse, urlunparse
 import json
+import validators
 
 from . import geom_helpers
 from ..general import util_helpers, model_helpers
@@ -89,8 +90,6 @@ class OGCHandlers(DatasetHandler):
                 return handler(self.access_url)
             except:
                 pass
-
-class WMSHandler(OGCHandlers):
     
     def get_layers(self, service):
         contents = service.contents
@@ -157,7 +156,7 @@ class WMSHandler(OGCHandlers):
         for obj in [id, layer]:
             if obj and hasattr(obj, 'abstract'):
                 abstract = obj.abstract
-                if isinstance(abstract, str) and abstract.strip() != '':
+                if isinstance(abstract, str) and abstract.strip() not in ['', 'None', 'null']:
                     abstracts.append(abstract)
         return '<br><br>'.join(abstracts)
 
@@ -209,7 +208,7 @@ class WMSHandler(OGCHandlers):
                 dataset.default_style = name
                 
                 url = styles[name].get('legend')
-                if url:
+                if validators(url):
                     url_instance, created = models.URL.objects.get_or_create(url=url)
                     if url_instance:
                         dataset.default_legend = url_instance
@@ -238,136 +237,11 @@ class WMSHandler(OGCHandlers):
                 except Exception as e:
                     return None
 
+class WMSHandler(OGCHandlers):
+    pass
+
 class WFSHandler(OGCHandlers):
-
-    def get_layers(self, service):
-        contents = service.contents
-        layers = {}
-        for layer_name in contents:
-            layers[layer_name] = service[layer_name].title
-        return layers
-
-    def handler(self):
-        clean_url = util_helpers.remove_query_params(self.url)
-        self.access_url = clean_url
-
-        service = self.get_service()
-        if service:
-            self.layers = self.get_layers(service)
-
-    def get_title(self, layer):
-        if layer and hasattr(layer, 'title'):
-            title = layer.title
-        else:
-            title = self.dataset.name
-        return title.replace('_', ' ')
-
-    def get_bbox(self, layer):
-        bbox = None
-        
-        if layer:
-            for attr in ['boundingBoxWGS84', 'boundingBox']:
-                if hasattr(layer, attr) and isinstance(getattr(layer, attr), (list, tuple)):
-                    bbox = getattr(layer, attr)
-                    break
-        
-        if bbox:
-            w,s,e,n,*srid = bbox
-            bbox_corners = [(w,s), (e,s), (e,n), (w,n), (w,s)]
-            if len(srid) != 0 and ':' in srid[0]:
-                bbox_srid = int(srid[0].split(':')[1])
-            else:
-                bbox_srid = 4326
-            
-            geom = Polygon(bbox_corners, srid=bbox_srid)
-            if bbox_srid == 4326:
-                return geom
-            else:
-                wgs84_srs = SpatialReference(4326)
-                return geom.transform(wgs84_srs, clone=True)
-        else:
-            return geom_helpers.WORLD_GEOM
-
-    def get_tags(self, id, layer):
-        url_tag_instances = model_helpers.collect_url_tags(self.access_url)
-
-        keywords = []
-        for obj in [id, layer]:
-            if obj and hasattr(obj, 'keywords') and isinstance(obj.keywords, (list, tuple)):
-                keywords = keywords + list(obj.keywords)
-        keywords = list(set([kw for kw in keywords if isinstance(kw, str)]))
-        print(keywords)
-        kw_tag_instances = model_helpers.list_to_tags(keywords)
-
-        return url_tag_instances + kw_tag_instances
-
-    def get_abstract(self, id, layer):
-        abstracts = []
-        for obj in [id, layer]:
-            if obj and hasattr(obj, 'abstract'):
-                abstract = obj.abstract
-                if isinstance(abstract, str) and abstract.strip() != '':
-                    abstracts.append(abstract)
-        return '<br><br>'.join(abstracts)
-
-    def get_extra_data(self, id, provider, layer):
-        data = {}
-        
-        if id:
-            id_vars = {}
-            for attr in ['accessconstraints', 'fees']:
-                if hasattr(id, attr):
-                    id_vars[attr] = getattr(id, attr)
-            data['id'] = id_vars
-        
-        if layer:
-            layer_vars = {}
-            for attr in ['queryable', 'styles', 'dataUrls', 'metadataUrls']:
-                if hasattr(layer, attr):
-                    layer_vars[attr] = getattr(layer, attr)
-            if hasattr(layer, 'auth') and hasattr(getattr(layer, 'auth'), '__dict__'):
-                layer_vars['auth'] = vars(getattr(layer, 'auth'))
-            data['layer'] = layer_vars
-
-        if provider:
-            provider_vars = {}
-            for attr in ['name', 'url']:
-                if hasattr(provider, attr):
-                    provider_vars[attr] = getattr(provider, attr)
-            if hasattr(provider, 'contact') and hasattr(getattr(provider, 'contact'), '__dict__'):
-                provider_vars['contact'] = vars(getattr(provider, 'contact'))
-            data['provider'] = provider_vars
-
-        return data
-
-    def populate_dataset(self, dataset):
-        self.dataset = dataset
-
-        service = self.get_service()
-        if service:
-            id = service.identification
-            provider = service.provider
-            layer = service[dataset.name]
-
-            extra_data = self.get_extra_data(id, provider, layer)
-            dataset.extra_data = json.dumps(extra_data)
-            
-            styles = extra_data.get('layer', {}).get('styles', {})
-            if styles:
-                name = list(styles.keys())[0]
-                dataset.default_style_name = name
-                
-                url = styles[name].get('legend')
-                url_instance, created = models.URL.objects.get_or_create(url=url)
-                if url_instance:
-                    dataset.default_style_url = url_instance
-            
-            dataset.title = self.get_title(layer)
-            dataset.bbox = self.get_bbox(layer)
-            dataset.abstract = self.get_abstract(id, layer)
-            dataset.tags.set(self.get_tags(id, layer))
-            dataset.save()
-
+    pass
 
 
 def get_dataset_handler(format, **kwargs):
