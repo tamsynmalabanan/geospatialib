ce35028ea7e470a36b506e199c3388389b7b90d0
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-06 14:33:37 +0530

    config project settings and customuser model

--- project/static/geog/js/data.js
+++ project/static/geog/js/data.js
@@ -445,7 +445,7 @@ const fetchWFSData = async (event, layer, options={}) => {
         version: '2.0.0',
         request: 'GetFeature',
         typeNames: layer.data.layerName,
-        srsname: layer.data.layerCrs,
+        srsname: `EPSG:${layer.data.layerCrs}`,
         outputFormat: 'json',
     }
 
