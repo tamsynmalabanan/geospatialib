21263c649fec144c8ae112e32a4ec343da9f8cf3
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-06 10:55:43 +0530

    config project settings and customuser model

--- project/htmx/hx_library/views.py
+++ project/htmx/hx_library/views.py
@@ -164,7 +164,7 @@ class SearchList(ListView):
             fillers = util_helpers.find_nearest_divisible(count, [2,3])-count
 
             context['count'] = count
-            context['fillers'] = fillers
+            context['fillers'] = range(fillers-1)
             context['filters'] = self.get_filters()
         return context
 
