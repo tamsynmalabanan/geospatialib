ac5d8761962a3bbbb0410652724d8af6af11dacc
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-07 09:58:14 +0530

    config project settings and customuser model

--- project/static/base/js/elements.js
+++ project/static/base/js/elements.js
@@ -126,7 +126,8 @@ const createButtonAndCollapse = (id, options={}) => {
 
 const createFormCheck = (id, options={}) => {
     const formCheck = document.createElement('div')
-    formCheck.className = `form-check d-flex flex-grow-1 ${options.formCheckClass}`
+    formCheck.className = `d-flex flex-grow-1 ${options.formCheckClass}`
+    // formCheck.className = `form-check d-flex flex-grow-1 ${options.formCheckClass}`
 
     const checkbox = document.createElement('input')
     checkbox.id = id
