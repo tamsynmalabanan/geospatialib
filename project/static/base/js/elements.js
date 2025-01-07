58ce0ab338ee54d0a8241dc5b6127315f89a99dc
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-07 11:41:32 +0530

    config project settings and customuser model

--- project/static/base/js/elements.js
+++ project/static/base/js/elements.js
@@ -167,7 +167,7 @@ const createFormCheck = (id, options={}) => {
         }
 
         const buttonContainer = document.createElement('div')
-        buttonContainer.classList.add('ms-auto', 'd-inline')
+        buttonContainer.classList.add('ms-auto', 'd-inline', 'hstack', 'gap-2')
         buttonContainer.appendChild(button)
         formCheck.appendChild(buttonContainer)
         
