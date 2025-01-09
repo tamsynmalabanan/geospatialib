a190db7fdabff332b260d9d70c66cc9b93018b6b
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-09 12:21:03 +0530

    config project settings and customuser model

--- project/apps/library/management/commands/onboard_datasets.py
+++ project/apps/library/management/commands/onboard_datasets.py
@@ -2,43 +2,68 @@ from django.core.management.base import BaseCommand
 from django.contrib.postgres.aggregates import ArrayAgg
 from apps.library import models
 from utils.gis import dataset_helpers
+
 class Command(BaseCommand):
     help = 'Onboard other datasets through URLS of existing datasets.'
 
     def handle(self, *args, **kwargs):
         self.stdout.write(self.style.SUCCESS('Onboard other datasets through URLS of existing datasets.'))
-
+        
         url_instances = (models.URL.objects
             .annotate(
-                dataset_formats=ArrayAgg('datasets__format', distinct=True), 
-                dataset_names=ArrayAgg('datasets__name', distinct=True),
+                formats=ArrayAgg('datasets__format', distinct=True), 
+                names=ArrayAgg('datasets__name', distinct=True),
             )
             .values(
+                'id', 
                 'url', 
-                'dataset_formats', 
-                'dataset_names', 
+                'formats', 
+                'names', 
             )
             .filter(datasets__isnull=False)
             .distinct()
         )
         
         for url_instance in url_instances:
+            id = url_instance['id']
             url = url_instance['url']
-            dataset_formats = url_instance['dataset_formats']
-            dataset_names = url_instance['dataset_names']
+            formats = url_instance['formats']
+            names = url_instance['names']
 
-            print(url)
+            print(f'URL: {url}')
+            print(f'EXISTING LAYERS: {names}')
 
-            for format in dataset_formats:
-                print(format)
+            for format in formats:
+                print(f'FORMAT: {format}')
 
-                handler = dataset_helpers.get_dataset_handler(
-                    format, 
-                    url=url,
-                )
+                try:
+                    handler = dataset_helpers.get_dataset_handler(
+                        format, 
+                        url=url,
+                    )
+                    layers = list(handler.layers.keys())
+                    print(f'LAYERS: {layers}')
 
-                print(handler)
+                    new_layers = [layer for layer in layers if layer not in names]
+                    print(f'LAYERS TO ONBOARD: {new_layers}')
+
+                    for layer in new_layers[:1]:
+                        print(f'NEW LAYER: {layer}')
+                        dataset_instance, created = models.Dataset.objects.get_or_create(
+                            url__id=id,
+                            format=format,
+                            name=layer
+                        )
+                        if dataset_instance and created:
+                            try:
+                                handler.populate_dataset(dataset_instance)
+                                print(f'SUCCESSFUL DATASET ONBOARDING!')
+                            except Exception as e:
+                                dataset_instance.delete()
+                                print(f'FAILED TO CREATE DATASET: {e}')
+                except Exception as e:
+                    print(f'FAILED TO RETRIEVE LAYERS: {e}')
 
             print('\n')
 
-        print(url_instances.count())
+        print(f'TOTAL: {url_instances.count()}')
