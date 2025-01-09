2a58cd6b28fff8f8141ed30ff4677bdb7ae7efa5
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-09 16:44:01 +0530

    config project settings and customuser model

--- project/static/geog/js/layers.js
+++ project/static/geog/js/layers.js
@@ -508,8 +508,7 @@ const createWFSLayer = (data) => {
 
                 if ((mapScale && mapScale > 10000) || (!mapScale && map.getZoom() < 9)) {
                     if (featureCount > 1000) {
-                        console.log(L.rectangle(L.geoJSON(geojson).getBounds()).toGeoJSON())
-                        geojson.features = [turf.bboxPolygon(turf.bbox(geojson))]
+                        geojson.features = [L.rectangle(L.geoJSON(geojson).getBounds()).toGeoJSON()]
                         geojson.tooltip = defaultTooltip
                         prefix = 'Bounding'
                         suffix = `for ${formatNumberWithCommas(featureCount)} features`
