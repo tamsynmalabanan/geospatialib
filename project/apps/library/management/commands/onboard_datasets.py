8cffdc59dc845250848c4f7fa3fb30b990baa824
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-09 10:37:29 +0530

    config project settings and customuser model

---
+++ project/apps/library/management/commands/onboard_datasets.py
@@ -0,0 +1,7 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Onboard other datasets through URLS of existing datasets.'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.SUCCESS('Onboard other datasets through URLS of existing datasets.'))
