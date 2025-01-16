3bc2053eab57f1e43ad026d5a73f691d713c4b63
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-16 11:53:39 +0530

    config project settings and customuser model

--- project/static/base/js/utils.js
+++ project/static/base/js/utils.js
@@ -176,7 +176,7 @@ const fetchDataWithTimeout = async (url, options={}) => {
         if (response.ok) {
             const data = await response.clone().text(); 
             const headers = {}
-            console.log('response.headers', response.headers, response.headers.get('Content-Type'))
+            console.log('response.headers', typeof response.headers, response.headers.get('Content-Type'))
             response.headers.forEach(key, value => {
                 headers[key] = value
             })
