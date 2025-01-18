3d6a18a9303013aa634914e6443f58cdea54ae3d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-18 11:35:40 +0530

    config project settings and customuser model

--- project/apps/library/management/commands/test.py
+++ project/apps/library/management/commands/test.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = 'Test'
 
     def handle(self, *args, **kwargs):
-        dataset_urls = models.Dataset.objects.values_list('url', flat=True)
+        dataset_urls = set(models.Dataset.objects.values_list('url__url', flat=True))
         print(dataset_urls)
 
         self.stdout.write(self.style.SUCCESS('Test'))
