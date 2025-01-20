940425de599aa9109443c1b8af7ae5b47d1eb22a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-20 11:32:01 +0530

    fixing cached geojson

--- project/static/geog/js/layers.js
+++ project/static/geog/js/layers.js
@@ -622,7 +622,7 @@ const createGeoJSONLayer = (data) => {
                     if (featureCount > 1000 && ((mapScale && mapScale > 10000) || (!mapScale && mapZoom < 10))) {
                         if (featureCount > 2000 || ((mapScale && mapScale > 100000) || (!mapScale && mapZoom < 6))) {
                             const boundsGeoJSON = L.rectangle(L.geoJSON(geojson).getBounds()).toGeoJSON()
-                            const feature = turf.polygonToLine(boundsGeoJSON)
+                            const feature = turf.polygonToLine(Object.assign({}, boundsGeoJSON))
                             console.log(
                                 'crs', getGeoJSONCRS(geojson), 
                                 'geojson layer', L.geoJSON(geojson), 
