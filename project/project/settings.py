3c8b8d46ae2f71a03a34567581df3aab7b390c21
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-20 09:35:56 +0530

    try to point to local versions of leaflet-geocoder

--- project/project/settings.py
+++ project/project/settings.py
@@ -117,12 +117,12 @@ LEAFLET_CONFIG = {
     'PLUGINS': {
         'geocoder': {
             'css': [
-                '/static/plugins/leaflet-geocoder/css/Control.Geocoder.css', 
-                # 'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css', 
+                # '/static/plugins/leaflet-geocoder/css/Control.Geocoder.css', 
+                'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css', 
             ],
             'js': [
-                '/static/plugins/leaflet-geocoder/js/Control.Geocoder.js', 
-                # 'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js',
+                # '/static/plugins/leaflet-geocoder/js/Control.Geocoder.js', 
+                'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js',
             ],
             'auto_include': True,
         },
