7e7c1b0e033eef7b66816f7e617de4f330a442ee
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-23 18:22:02 +0530

    config showHideBtn for legend

--- project/static/geog/js/init.js
+++ project/static/geog/js/init.js
@@ -137,7 +137,7 @@ const handleMapLayerGroups = (map) => {
 
         layerGroup.getHiddenLayer = (leafletId) => {
             console.log(leafletId)
-            const matches = layerGroup.hiddenLayers.filter(layer => layer._leaflet_id === leafletId)
+            const matches = layerGroup.hiddenLayers.filter(layer => layer._leaflet_id.toString() === leafletId.toString())
             console.log(layerGroup.hiddenLayers, matches)
             if (matches.length !== 0) {
                 return matches[0]
