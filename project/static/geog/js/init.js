8830ecd8dc4f08aaebccaa6a1ca82eae9dedb095
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-23 18:49:14 +0530

    config showHideBtn for legend

--- project/static/geog/js/init.js
+++ project/static/geog/js/init.js
@@ -189,9 +189,7 @@ const handleMapLayerGroups = (map) => {
                 const layerLegend = legend?.querySelector(`[data-leaflet-id="${currentLayer._leaflet_id}"]`)
                 if (!layerLegend) {return}
                 
-                console.log(options)
                 const index = options.index ?? (options.indexIncrement ? (options.indexIncrement - layerLegends.indexOf(layerLegend))) : null
-                console.log(index)
                 if (!index) {return}
                 
                 if (index === -1 || index >= legend.children.length) {
