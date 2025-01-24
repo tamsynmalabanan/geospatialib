1c02a5feb0c6e8d1a2e7d249bc658c63aec384b6
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-24 09:46:05 +0530

    config showHideBtn for legend

--- project/static/base/js/utils.js
+++ project/static/base/js/utils.js
@@ -218,7 +218,6 @@ const fetchDataWithTimeout = async (url, options={}) => {
         return response
     }).catch(async error => {
         if (error.name === 'TypeError' && error.message === 'Failed to fetch') {
-            console.log('here')
             return await fetchViaCorsProxy(url, options)
         } else {
             throw error
