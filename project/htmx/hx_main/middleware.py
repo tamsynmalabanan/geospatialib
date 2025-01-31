58ee439e550fe39b5cf4b53b5a17a5bc04d426de
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-01-31 19:30:37 +0530

    updated populateLayerDropdownMenu

--- project/htmx/hx_main/middleware.py
+++ project/htmx/hx_main/middleware.py
@@ -8,12 +8,12 @@ class HTMXDomainRestriction:
 
     def __call__(self, request):
         path = resolve(request.path)
-        # if path.url_name != 'cors_proxy':
-        if path.app_name.startswith('hx_') and not request.htmx:
-            return redirect('library:index')
-        
-        if request.META.get('HTTP_HOST') not in settings.ALLOWED_HOSTS:
-            return redirect('library:index')
+        if path.url_name != 'cors_proxy':
+            if path.app_name.startswith('hx_') and not request.htmx:
+                return redirect('library:index')
+            
+            if request.META.get('HTTP_HOST') not in settings.ALLOWED_HOSTS:
+                return redirect('library:index')
 
         response = self.get_response(request)
         return response
