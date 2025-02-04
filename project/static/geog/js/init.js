75eb4b37bafcd917fa4401cd2a6e9064b8252ecd
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-02-04 23:25:25 +0530

    updated populateLayerDropdownMenu

--- project/static/geog/js/init.js
+++ project/static/geog/js/init.js
@@ -607,7 +607,7 @@ const handleMapLegend = (map) => {
                                     box.className = 'h-0 w-100'
                                 }
                                 
-                                if (Array('Polygon', 'Box').includes(style.type)) {
+                                if (Array('Polygon').includes(style.type)) {
                                     box.className = 'h-100 w-100'
     
                                     const fillColor = styleDef.fillColor
