e60b398f7af3ad7020d7821dd285d086133369d2
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-02-06 14:04:54 +0530

    updated populateLayerDropdownMenu

--- project/static/geog/js/geojson-indexedDB.js
+++ project/static/geog/js/geojson-indexedDB.js
@@ -1,83 +1,83 @@
 const requestGeoJSONDB = () => indexedDB.open('geojsonDB', 1)
 
 requestGeoJSONDB().onupgradeneeded = (event) => {
-  const db = event.target.result;
-  if (!db.objectStoreNames.contains('geojsons')) {
-    db.createObjectStore('geojsons', {keyPath:'id'});
-  }
+    const db = event.target.result
+    
+    if (db.objectStoreNames.contains('geojsons')) return
+    db.createObjectStore('geojsons', {keyPath:'id'})
 }
 
 const saveGeoJSON = (id, geojson) => {
-    const geojsonDB = requestGeoJSONDB()
+    const request = requestGeoJSONDB()
   
-    geojsonDB.onsuccess = function(event) {
-      const db = event.target.result;
-        const transaction = db.transaction(['geojsons'], 'readwrite');
-        const objectStore = transaction.objectStore('geojsons');
-        objectStore.put({ id, geojson });
-    
-        transaction.oncomplete = function() {
-            console.log('GeoJSON saved successfully!');
-        };
-    
-        transaction.onerror = function(event) {
-            console.error('Transaction error:', event.target.errorCode);
-        };
-    };
+    request.onsuccess = (event) => {
+        const db = event.target.result
+        const transaction = db.transaction(['geojsons'], 'readwrite')
+        const objectStore = transaction.objectStore('geojsons')
+        objectStore.put({ id, geojson })
+
+        transaction.oncomplete = () => {
+            console.log('GeoJSON saved successfully!')
+        }
+
+        transaction.onerror = (event) => {
+            console.error('Transaction error:', event.target.errorCode)
+        }
+    }
   
-    geojsonDB.onerror = function(event) {
-        console.error('Database error:', event.target.errorCode);
-    };
+    request.onerror = (event) => {
+        console.error('Database error:', event.target.errorCode)
+    }
 }
 
-function getGeoJSON(id) {
-    const geojsonDB = requestGeoJSONDB()
+const getGeoJSON = (id) => {
+    const request = requestGeoJSONDB()
   
-    geojsonDB.onsuccess = function(event) {
-        const db = event.target.result;
-        const transaction = db.transaction(['geojsons'], 'readonly');
-        const objectStore = transaction.objectStore('geojsons');
-        const geojsonRequest = objectStore.get(id);
+    request.onsuccess = (event) => {
+        const db = event.target.result
+        const transaction = db.transaction(['geojsons'], 'readonly')
+        const objectStore = transaction.objectStore('geojsons')
+        const geojsonRequest = objectStore.get(id)
     
-        geojsonRequest.onsuccess = function(event) {
-            const result = event.target.result;
+        geojsonRequest.onsuccess = (event) => {
+            const result = event.target.result
             if (result) {
-                console.log('GeoJSON retrieved successfully:', result.geojson);
+                console.log('GeoJSON retrieved successfully:', result.geojson)
             } else {
-                console.log('No GeoJSON found with ID:', id);
+                console.log('No GeoJSON found with ID:', id)
             }
-        };
+        }
     
         geojsonRequest.onerror = function(event) {
-                console.error('GeoJSON retrieval error:', event.target.errorCode);
-        };
-    };
+                console.error('GeoJSON retrieval error:', event.target.errorCode)
+        }
+    }
   
-    geojsonDB.onerror = function(event) {
-        console.error('Database error:', event.target.errorCode);
-    };
+    request.onerror = (event) => {
+        console.error('Database error:', event.target.errorCode)
+    }
 }
 
-function deleteGeoJSON(id) {
-    const geojsonDB = requestGeoJSONDB()
+const deleteGeoJSON = (id) => {
+    const request = requestGeoJSONDB()
     
-    geojsonDB.onsuccess = function(event) {
-        const db = event.target.result;
-        const transaction = db.transaction(['geojsons'], 'readwrite');
-        const objectStore = transaction.objectStore('geojsons');
-        const deleteRequest = objectStore.delete(id);
+    request.onsuccess = (event) => {
+        const db = event.target.result
+        const transaction = db.transaction(['geojsons'], 'readwrite')
+        const objectStore = transaction.objectStore('geojsons')
+        const deleteRequest = objectStore.delete(id)
     
-        deleteRequest.onsuccess = function() {
-            console.log('GeoJSON deleted successfully!');
-        };
+        deleteRequest.onsuccess = () => {
+            console.log('GeoJSON deleted successfully!')
+        }
     
-        deleteRequest.onerror = function(event) {
-            console.error('GeoJSON deletion error:', event.target.errorCode);
-        };
-    };
+        deleteRequest.onerror = (event) => {
+            console.error('GeoJSON deletion error:', event.target.errorCode)
+        }
+    }
   
-    geojsonDB.onerror = function(event) {
-        console.error('Database error:', event.target.errorCode);
-    };
+    request.onerror = (event) => {
+        console.error('Database error:', event.target.errorCode)
+    }
 }
   
