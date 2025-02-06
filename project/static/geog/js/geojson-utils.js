d644f6ad6bbcda5ecbe55d8b6ed78c9ed0f78eeb
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-02-06 10:11:36 +0530

    updated populateLayerDropdownMenu

--- project/static/geog/js/geojson-utils.js
+++ project/static/geog/js/geojson-utils.js
@@ -199,7 +199,7 @@ const simplifyGeoJSON = (geojson, map) => {
         : pathsGeoJSON.features.push(feature)
     })
 
-    pointsGeoJSON.features.length > 0 && simplifyPointGeoJSON(pointsGeoJSON, getMeterScale(map)/1000/10)
+    pointsGeoJSON.features.length > 0 && simplifyPointGeoJSON(pointsGeoJSON, getMeterScale(map)/1000/20)
     pathsGeoJSON.features.length > 0 && simplifyPathGeoJSON(pathsGeoJSON)
 
     geojson.features = pointsGeoJSON.features.concat(pathsGeoJSON.features)
