aa289ca48cae8ac7eb29397f3512f4776ee653c4
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-02-06 15:10:38 +0530

    updated populateLayerDropdownMenu

--- project/static/geog/js/geojson-utils.js
+++ project/static/geog/js/geojson-utils.js
@@ -115,7 +115,7 @@ const updateGeoJSONData = async (event) => {
         if (signal.aborted) return
         geojson = await (async () => {
             const cachedGeoJSON = await getFromGeoJSONDB(mapKey)
-            console.log(cachedGeoJSON)
+            console.log(typeof cachedGeoJSON)
             if (!cachedGeoJSON) return
             
             if (cachedGeoJSON.prefix) return
