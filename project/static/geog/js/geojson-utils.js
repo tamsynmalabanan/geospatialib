2326aa13964c885337b78d8ef17484eaec44c593
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-02-07 18:01:39 +0530

    updated populateLayerDropdownMenu

--- project/static/geog/js/geojson-utils.js
+++ project/static/geog/js/geojson-utils.js
@@ -258,7 +258,8 @@ const simplifyPointGeoJSON = (geojson, maxDistance, options={}) => {
         
         geojson.features = features
         geojson.prefix = 'Cluster'
-    } catch {
+    } catch (error) {
+        console.log(error)
         return
     }
 }
