f9de50516814369fb3d2d8ae0c4cdda18b781f6b
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-02-07 11:57:28 +0530

    updated populateLayerDropdownMenu

--- project/static/geog/js/geojson-utils.js
+++ project/static/geog/js/geojson-utils.js
@@ -148,7 +148,7 @@ const updateGeoJSONData = async (event) => {
                     await handleGeoJSON(geojson)
                     
                     const clone = Object.assign({}, geojson)
-                    await updateGeoJSONOnDB(mapKey, {
+                    updateGeoJSONOnDB(mapKey, {
                         type: clone.type,
                         features: clone.features,
                         mapBounds: clone.mapBounds,
