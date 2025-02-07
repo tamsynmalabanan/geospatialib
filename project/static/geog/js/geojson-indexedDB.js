59409e76c9ff7c7073e11ec59aafcf264bbcede1
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-02-07 14:24:56 +0530

    updated populateLayerDropdownMenu

--- project/static/geog/js/geojson-indexedDB.js
+++ project/static/geog/js/geojson-indexedDB.js
@@ -11,13 +11,16 @@ const requestGeoJSONDB = () => {
     return request
 }
 
-const saveToGeoJSONDB = (id, geojson) => {
+// const saveToGeoJSONDB = (id, geojson, expiration=1000*60*60*24*7) => {
+const saveToGeoJSONDB = (id, geojson, expiration=1000*10) => {
     const request = requestGeoJSONDB()
     request.onsuccess = (event) => {
         const db = event.target.result
         const transaction = db.transaction(['geojsons'], 'readwrite')
         const objectStore = transaction.objectStore('geojsons')
-        objectStore.put({ id, geojson })
+
+        const expirationTime = Date.now() + expiration
+        objectStore.put({ id, geojson, expirationTime })
     }
 }
 
@@ -92,3 +95,39 @@ const deleteFromGeoJSONDB = (id) => {
         // console.error('Database error:', event.target.errorCode)
     }
 }
+
+setInterval(async () => {
+    const request = requestGeoJSONDB()
+
+    request.onsuccess = (event) => {
+        const db = event.target.result;
+        const transaction = db.transaction(['geojsons'], 'readwrite');
+        const objectStore = transaction.objectStore('geojsons');
+        
+        objectStore.openCursor().onsuccess = (event) => {
+            const cursor = event.target.result;
+            if (cursor) {
+                const currentTime = Date.now();
+                if (cursor.value.expirationTime && cursor.value.expirationTime < currentTime) {
+                    objectStore.delete(cursor.key);
+                    console.log(`Deleted expired data with ID: ${cursor.key}`);
+                }
+                cursor.continue();
+            } else {
+                console.log('No more entries.');
+            }
+        };
+
+        transaction.oncomplete = () => {
+            console.log('Expired data check and deletion completed.');
+        };
+
+        transaction.onerror = (event) => {
+            console.error('Transaction error:', event.target.errorCode);
+        };
+    };
+
+    request.onerror = (event) => {
+        console.error('Database error:', event.target.errorCode);
+    };
+}, 1000*10);
