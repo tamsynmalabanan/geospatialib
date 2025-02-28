858e53c869df31ca12b60553c2ab547105a0eb2c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-02-28 19:04:01 +0530

    assigned STATIC_ROOT value

--- geospatialib/geospatialib/settings.py
+++ geospatialib/geospatialib/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'geospatialib.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'geospatialib',
+        'USER': 'geospatialib_admin',
+        'PASSWORD': 'case-wildlife-dumping',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -128,3 +139,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
