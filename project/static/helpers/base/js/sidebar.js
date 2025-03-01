773dccaba8cf308953b7c3b95afb40b9685dec8d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 23:14:22 +0530

    updated base.html template

--- project/static/helpers/base/js/sidebar.js
+++ project/static/helpers/base/js/sidebar.js
@@ -1,14 +1,12 @@
 const toggleSidebar = (sidebarSelector) => {
-    const button = event.target
     const sidebar = document.querySelector(sidebarSelector)
+    const button = sidebar.querySelector(`[onclick='toggleSidebar("${sidebarSelector}")']`)
     const toggle = document.querySelector(`[data-bs-toggle="offcanvas"][data-bs-target="${sidebarSelector}"]`)
     const dismiss = document.querySelector(`[data-bs-dismiss="offcanvas"][data-bs-target="${sidebarSelector}"]`)
 
     if (sidebar.classList.contains('offcanvas-lg')) {
-        if (typeof button === 'button') {
-            button.classList.remove('bi-layout-sidebar-inset')
-            button.classList.add('bi-window-sidebar')
-        }
+        button.classList.remove('bi-layout-sidebar-inset')
+        button.classList.add('bi-window-sidebar')
         
         sidebar.classList.remove('offcanvas-lg')
         sidebar.classList.contains('show') && toggle.click()
@@ -17,10 +15,8 @@ const toggleSidebar = (sidebarSelector) => {
         toggle.classList.remove('d-lg-none')
         dismiss.classList.remove('d-lg-none')
     } else {
-        if (typeof button === 'button') {
-            button.classList.remove('bi-window-sidebar')
-            button.classList.add('bi-layout-sidebar-inset')
-        }
+        button.classList.remove('bi-window-sidebar')
+        button.classList.add('bi-layout-sidebar-inset')
 
         sidebar.classList.remove('offcanvas')
         sidebar.classList.add('offcanvas-lg')
