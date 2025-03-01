950bcc4e1f7792886cbde17dc812ab444dbf4635
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 22:04:33 +0530

    updated base.html template

--- project/static/helpers/base/js/sidebar.js
+++ project/static/helpers/base/js/sidebar.js
@@ -1,11 +1,16 @@
 const toggleSidebar = (sidebarSelector) => {
     const sidebar = document.querySelector(sidebarSelector)
+    const toggle = document.querySelector(`[data-bs-target=${sidebarSelector}]`)
     
     if (sidebar.classList.contains('offcanvas-lg')) {
         sidebar.classList.remove('offcanvas-lg')
         sidebar.classList.add('offcanvas')
+
+        toggle.classList.remove('d-lg-none')
     } else {
         sidebar.classList.remove('offcanvas')
         sidebar.classList.add('offcanvas-lg')
+
+        toggle.classList.add('d-lg-none')
     }
 }
