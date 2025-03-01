0913a631186096c5f52e980087e08877b890462f
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 15:14:16 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/init.js
+++ project/static/helpers/leaflet/js/init.js
@@ -1,19 +1,13 @@
-const leafletMapHandler = (map) => {
-    const container = map.getContainer()
-    const dataset = container.parentElement.dataset
-
-    Array(
-        () => container.className = `${container.className} z-1 ${dataset.mapClass || ''}`,
-        () => elementResizeObserver(container, () => map.invalidateSize())
-    ).forEach(handler => handler())
-    
-    map._initComplete = true
-    map.fire('initComplete')
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener("map:init", (event) => {
         const map = event.detail.map
-        leafletMapHandler(map)
+        const container = map.getContainer()
+        const dataset = container.parentElement.dataset
+
+        container.className = `${container.className} z-1 ${dataset.mapClass || ''}`
+        elementResizeObserver(container, () => map.invalidateSize())
+
+        map._initComplete = true
+        map.fire('initComplete')
     })
 })
