2fbfcf92a9030f386bd661ef93eb41a557a96930
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 10:03:31 +0530

    added main app models to admin

--- project/main/admin.py
+++ project/main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.URL)
+admin.site.register(models.Collection)
+
+class LayerAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'id',
+    )
+    search_fields = ['name']
+    list_filter = ['collection']
+    list_per_page = 15
+
+admin.site.register(models.Layer, LayerAdmin)
