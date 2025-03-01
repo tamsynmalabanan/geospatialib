82b961ee53f2f63e3def4c2952cfd2b293a9e642
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 12:31:38 +0530

    updated base.html template

--- project/helpers/templatetags/utility_tags.py
+++ project/helpers/templatetags/utility_tags.py
@@ -1,7 +1,14 @@
 from django import template
 
+import string, random
+
 register = template.Library()
 
 @register.simple_tag
 def variable(value):
     return value
+
+@register.simple_tag
+def random_string():
+    chars = string.ascii_letters
+    return ''.join(random.choices(chars, k=16))
