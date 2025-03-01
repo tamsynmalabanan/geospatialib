1a2c836f50c0ec6329af0aaed8dcfc7d9a7e9647
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 13:17:14 +0530

    updated base.html template

--- project/static/helpers/js/animations.js
+++ project/static/helpers/js/animations.js
@@ -1,5 +1,6 @@
 const fadeoutElement = (element, options={}) => {
     setTimeout(() => {
         element.style.animation = `fadeoutAnimation ${options.fadeoutTimeout | 3000} forwards`
+        element.remove()
     }, options.fnTimeout | 10000);
 }
