cff44c1bd6a52734da2148be1806ffb7147e1a6d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-02 04:09:27 +0530

    updated base.html template

--- project/static/helpers/base/js/theme.js
+++ project/static/helpers/base/js/theme.js
@@ -4,11 +4,51 @@ const getPreferredTheme = () => {
     return storedTheme || colorScheme
 }
 
+const getThemeControls = (parent=document) => {
+    return [
+        {
+            elements: parent.querySelectorAll(`[onclick='toggleDarkMode(event)']`),
+            classes: {
+                light: ['bi-moon'],
+                dark: ['bi-moon-fill'],
+            }
+        },
+    ]
+}
+
+const toggleThemedElements = (theme, parent=document) => {
+    let setThemeTimeout
+    getThemeControls(parent).forEach(control => {
+        control.elements.forEach(element => {
+            for (let themeName in control.classes) {
+                const themeClasses = control.classes[themeName];
+
+                if (themeName === theme) {  
+                    themeClasses.forEach(className => {
+                        element.classList.add(className);
+                    })
+                } else {
+                    themeClasses.forEach(className => {
+                        element.classList.remove(className);
+                    })
+                }
+            }
+
+            clearTimeout(setThemeTimeout)
+            setThemeTimeout = setTimeout(() => {
+                const setThemeEvent = new Event('setTheme')
+                document.dispatchEvent(setThemeEvent)
+            }, 200)
+        })
+    })
+}
+
 const setTheme = (theme) => {
+    theme = theme ? theme : getPreferredTheme()
+    document.documentElement.setAttribute('data-bs-theme', theme)
     theme = theme === 'auto' ? getPreferredTheme() : theme
     localStorage.setItem('theme', theme)
-    console.log(theme)
-    // toggleThemedElements(theme)
+    toggleThemedElements(theme)
 }
 
 const toggleTheme = () => {
@@ -16,3 +56,7 @@ const toggleTheme = () => {
     setTheme(currentTheme === 'light' ? 'dark' : 'light')
 }
 
+document.addEventListener('htmx:afterSwap', (event) => {
+    const target = event.target.parentElement || event.target
+    toggleThemedElements(getPreferredTheme(), parent=target)
+})
