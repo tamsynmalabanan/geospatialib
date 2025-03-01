95179f3a529ecb4a7edfaf39e0d402653095f8c5
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 13:42:04 +0530

    updated base.html template

--- project/static/helpers/js/animations.js
+++ project/static/helpers/js/animations.js
@@ -1,8 +1,18 @@
 const fadeoutElement = (element, options={}) => {
-    setTimeout(() => {
-        element.style.animation = `fadeOut ${options.fadeTimeout | 4000}ms ease-in-out`
+    const fadeTimeout = options.fadeTimeout || 4000
+    const fnTimeout = options.fnTimeout || 5000
+    
+    const handler = () => setTimeout(() => {
+        element.style.animation = `fadeOut ${fadeTimeout}ms ease-in-out`
         setTimeout(() => {
             element.remove()
         }, 3000)
-    }, options.fnTimeout | 10000);
+    }, fnTimeout)
+    
+    let handlerTimeout
+    handlerTimeout = handler()
+    element.addEventListener('mouseover', () => {
+        clearTimeout(handlerTimeout)
+        handlerTimeout = handler()
+    })
 }
