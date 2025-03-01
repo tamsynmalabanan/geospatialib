5e7440c616b2cc62055b737a7cad7fb302d3ffe8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 23:58:37 +0530

    updated base.html template

--- project/static/helpers/base/js/theme.js
+++ project/static/helpers/base/js/theme.js
@@ -1,3 +1,18 @@
-const toggleTheme = () => {
-    
+const getPreferredTheme = () => {
+    const storedTheme = localStorage.getItem('theme')
+    const colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    return storedTheme || colorScheme
 }
+
+const setTheme = (theme) => {
+    theme = theme === 'auto' ? getPreferredTheme() : theme
+    localStorage.setItem('theme', theme)
+    console.log(theme)
+    // toggleThemedElements(theme)
+}
+
+const toggleTheme = () => {
+    const currentTheme = document.documentElement.getAttribute('data-bs-theme')
+    setTheme(currentTheme === 'light' ? 'dark' : 'light')
+}
+
