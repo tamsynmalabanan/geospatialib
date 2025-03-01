76e899b0fe0fbc3fa0436eb2012a3f34a939a367
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 19:28:38 +0530

    updated base.html template

--- project/main/views.py
+++ project/main/views.py
@@ -2,6 +2,5 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, HttpResponse
 
 def index(request):
-    messages.success(request, 'Message test', extra_tags='main-index-map')
     context = {}
     return render(request, 'main/index.html', context)
--- project/static/helpers/base/js/utils.js
+++ project/static/helpers/base/js/utils.js
@@ -1,4 +1,4 @@
-const elementResizeObserver = (element, handler) => {
+const elementResizeObserver = (element, callback) => {
     let resizeTimeout
     
     const resizeObserver = new ResizeObserver(entries => {
@@ -6,11 +6,46 @@ const elementResizeObserver = (element, handler) => {
             if (entry.target === element) {
                 clearTimeout(resizeTimeout);
                 resizeTimeout = setTimeout(() => {
-                    handler()
+                    callback(element)
                 }, 100)
             }
         }
     });
 
     resizeObserver.observe(element);
+}
+
+const animateElement = (element, animation, options={}) => {
+    const initTime = options.initTime || 3000
+    const timeoutMs = options.timeoutMs || 4000
+    const effect = options.effect || 'ease-in-out'
+    const resetTrigger = options.resetTrigger === false ? null : !options.resetTrigger || options.resetTrigger === true ? 'mouseover' : options.resetTrigger
+    const callback = options.callback
+
+    let handlerTimeout
+    const handler = () => setTimeout(() => {
+        element.classList.add(animation)
+        element.style.animation = `${animation} ${timeoutMs}ms ${effect}`
+        
+        setTimeout(() => {
+            if (element.classList.contains(animation)) {
+                element.classList.remove(animation)
+                callback && callback(element)
+            }
+        }, timeoutMs-100)
+    }, initTime)
+
+    if (resetTrigger) {
+        element.addEventListener(resetTrigger, () => {
+            clearTimeout(handlerTimeout)
+            element.classList.remove(animation)
+            element.style.animation = ''
+        })
+        
+        element.addEventListener('mouseout', () => {
+            handlerTimeout = handler()
+        })
+    }
+
+    handlerTimeout = handler()
 }
