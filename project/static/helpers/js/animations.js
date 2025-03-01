c0b7522b9c69b1eb2d2b868103d56dde6d703079
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 14:08:43 +0530

    updated base.html template

--- project/static/helpers/js/animations.js
+++ project/static/helpers/js/animations.js
@@ -1,15 +1,19 @@
 const fadeoutElement = (element, options={}) => {
     const initTimeout = options.initTimeout || 3000
     const fadeoutTimeoutMs = options.fadeoutTimeoutMs || 4000
-    const animation = options.animation || 'linear'
+    const animation = options.animation || 'ease-in-out'
     const resetTrigger = options.resetTrigger === false ? null : !options.resetTrigger || options.resetTrigger === true ? 'mouseover' : options.resetTrigger
+    const removeElement = options.removeElement
 
     let handlerTimeout
     const handler = () => setTimeout(() => {
         element.classList.add('fadeout')
         element.style.animation = `fadeOut ${fadeoutTimeoutMs}ms ${animation}`
         setTimeout(() => {
-            element.classList.contains('fadeout') && element.remove()
+            if (element.classList.contains('fadeout')) {
+                removeElement ? element.remove() : element.classList.add('d-none')
+            } 
+                
         }, fadeoutTimeoutMs-100)
     }, initTimeout)
 
