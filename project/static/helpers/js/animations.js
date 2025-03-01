ea9fb03f7f4ce8f9826ee54fdf4a8ed09f480349
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-01 14:01:32 +0530

    updated base.html template

--- project/static/helpers/js/animations.js
+++ project/static/helpers/js/animations.js
@@ -1,22 +1,26 @@
 const fadeoutElement = (element, options={}) => {
-    const fnTimeout = options.fnTimeout || 5000
-    const fadeTimeout = options.fadeTimeout || 4000
+    const initTimeout = options.initTimeout || 5000
+    const fadeoutTimeoutMs = options.fadeoutTimeoutMs || 4000
     const animation = options.animation || 'ease-in-out'
+    const resetTrigger = options.resetTrigger ? options.resetTrigger === true ? 'mouseover' : options.resetTrigger : null
 
+    let handlerTimeout
     const handler = () => setTimeout(() => {
         element.classList.add('fadeout')
-        element.style.animation = `fadeOut ${fadeTimeout}ms ${animation}`
+        element.style.animation = `fadeOut ${fadeoutTimeoutMs}ms ${animation}`
         setTimeout(() => {
             element.classList.contains('fadeout') && element.remove()
-        }, 3000)
-    }, fnTimeout)
+        }, fadeoutTimeoutMs-100)
+    }, initTimeout)
     
-    let handlerTimeout
+    if (resetTrigger) {
+        element.addEventListener(resetTrigger, () => {
+            clearTimeout(handlerTimeout)
+            element.style.animation = ''
+            element.classList.remove('fadeout')
+            handlerTimeout = handler()
+        })
+    }
+
     handlerTimeout = handler()
-    element.addEventListener('mouseover', () => {
-        clearTimeout(handlerTimeout)
-        element.style.animation = ''
-        element.classList.remove('fadeout')
-        handlerTimeout = handler()
-    })
 }
