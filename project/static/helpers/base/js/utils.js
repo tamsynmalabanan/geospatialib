5a98e49eba8fb9cc34c404874073104481bbd64a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-02 22:53:38 +0530

    updated base.html template

--- project/static/helpers/base/js/utils.js
+++ project/static/helpers/base/js/utils.js
@@ -62,3 +62,22 @@ const setCookie = (name, value, days) => {
     
     document.cookie = prefix + ";path=/" 
 }
+
+const getCookie = (name) => {
+    const nameEQ = name + "=";
+    const cookiesArray = document.cookie.split(';');
+    
+    for (let i = 0; i < cookiesArray.length; i++) {
+        let cookie = cookiesArray[i];
+        
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    
+    return null;
+}
