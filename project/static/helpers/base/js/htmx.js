da6663c6536f043d8048ba31d9b5ba06ee0e01d9
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-02 23:01:38 +0530

    updated base.html template

--- project/static/helpers/base/js/htmx.js
+++ project/static/helpers/base/js/htmx.js
@@ -1,8 +1,8 @@
-document.addEventListener('htmx:afterSwap', (event) => {
-    const target = event.target
+// document.addEventListener('htmx:afterSwap', (event) => {
+//     const target = event.target
 
-    toggleThemedElements(
-        getPreferredTheme(), 
-        parent=target.parentElement || target
-    )
-})
+//     toggleThemedElements(
+//         getPreferredTheme(), 
+//         parent=target.parentElement || target
+//     )
+// })
