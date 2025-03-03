d7a2cddd0a2774b5913ba2142c0536330944026b
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 21:21:23 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/utils.js
+++ project/static/helpers/leaflet/js/utils.js
@@ -1,4 +1,4 @@
-const createLeafletBasemapLayer = (map) => L.tileLayer("//tile.openstreetmap.org/{z}/{x}/{y}.png", {
+const addLeafletBasemapLayer = (map) => L.tileLayer("//tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     className: `layer-${getPreferredTheme()}`
 }).addTo(map)
