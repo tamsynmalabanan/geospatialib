bf8744d2bb68fbf5dc45858c4442c94f05db584c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 12:01:25 +0530

    updated base.html template

--- project/helpers/templatetags/utility_tags.py
+++ project/helpers/templatetags/utility_tags.py
@@ -5,14 +5,14 @@ import string, random
 register = template.Library()
 
 @register.simple_tag
-def variable(value):
-    return value
+def variable(value, sub=None):
+    return value if value else sub
 
 @register.simple_tag
 def random_string():
     chars = string.ascii_letters
     return ''.join(random.choices(chars, k=16))
 
-@register.simple_tag
-def get_theme(request):
-    return request.COOKIES.get('theme', 'light')
+# @register.simple_tag
+# def get_theme(request):
+#     return request.COOKIES.get('theme', 'light')
