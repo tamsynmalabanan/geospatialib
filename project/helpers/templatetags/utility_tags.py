45481402088f35e1057bfe92b1ba256bdda2cec8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 14:32:30 +0530

    updated base.html template

--- project/helpers/templatetags/utility_tags.py
+++ project/helpers/templatetags/utility_tags.py
@@ -9,3 +9,7 @@ def variable(value, sub=None):
 @register.filter
 def equals(value1, value2):
     return None if value1 in [None, ''] else value1 == value2
+
+@register.filter
+def get(dict, key, sub=None):
+    return dict.get(key, sub)
