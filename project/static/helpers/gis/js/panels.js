1a2e06fc3e76acc757a614a385592546a5111ae4
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 13:24:23 +0530

    updated base.html template

--- project/static/helpers/gis/js/panels.js
+++ project/static/helpers/gis/js/panels.js
@@ -2,10 +2,11 @@ const constructMapPanels = (mapContainer, options={}) => {
     const panelsContainer = options.panelsContainer || mapContainer
 
     createOffcanvas(`${mapContainer.id}-panels`, options={
+        toggleClass: 'm-10',
         toggleThemed: true,
         toggleIconClass: 'bi-layout-sidebar-inset-reverse',
         toggleLabelText: 'GeoPanel',
-        toggleLabelClass: 'm-10 d-none d-lg-block',
+        toggleLabelClass: 'd-none d-lg-block',
         toggleParent: panelsContainer,
         showOffcanvas: getCookie('show_map_panels')
     })
