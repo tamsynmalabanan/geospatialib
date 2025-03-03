66028c39a4a183c4fde9bc1dbd88e33adc3b341a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 11:53:19 +0530

    updated base.html template

--- project/static/helpers/base/js/elements.js
+++ project/static/helpers/base/js/elements.js
@@ -1,13 +1,19 @@
-const createOffcanvas = (options={}) => {
+const createOffcanvas = (id, options={}) => {
     const toggleTag = options.toggleTag || 'button'
     const toggleThemed = options.toggleThemed
+    const toggleIcon = options.toggleIcon
+    const toggleLabel = options.toggleLabel
 
     const toggle = document.createElement('button')
     toggle.className = removeWhitespace(`
         ${toggleTag === 'button' ?  `btn` : ''}
         ${toggleThemed ? `text-bg-${getPreferredTheme()}` : ''}
-        rounded-pill shadow-lg
+        rounded-circle shadow-lg
     `)
-
-    console.log(toggle)
+    if (toggleTag === 'button') toggle.type = 'button' 
+    if (toggleTag !== 'button') toggle.role = 'button'
+    toggle.setAttribute('data-bs-toggle', 'offcanvas') 
+    toggle.setAttribute('data-bs-target', `#${id}`) 
+    toggle.setAttribute('aria-controls', id) 
+    
 }
