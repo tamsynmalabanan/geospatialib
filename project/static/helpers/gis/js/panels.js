6ed0c22913796f9cecd45bfb7e5be402fdc6b6e0
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 13:13:45 +0530

    updated base.html template

--- project/static/helpers/gis/js/panels.js
+++ project/static/helpers/gis/js/panels.js
@@ -1,6 +1,6 @@
 const constructMapPanels = (container) => {
     createOffcanvas(`${container.id}-panels`, options={
-        toggleClass: 'z-2'
-        toggleParent: container
+        toggleClass: 'z-2',
+        toggleParent: container,
     })
 }
