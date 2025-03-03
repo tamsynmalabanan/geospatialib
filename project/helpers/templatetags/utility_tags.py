a5b9fb5442533f4af9207e7218b06178fc756e05
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 12:06:10 +0530

    updated base.html template

--- project/helpers/templatetags/utility_tags.py
+++ project/helpers/templatetags/utility_tags.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.simple_tag
 def variable(value, sub=None):
     return value if value else sub
+
+@register.filter
+def equals(value1, value2):
+    return value1 === value2
