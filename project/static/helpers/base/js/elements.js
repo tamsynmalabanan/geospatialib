cfc7dd81c8812403a9caa0b08ebee7df845afc9e
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 14:05:55 +0530

    updated base.html template

--- project/static/helpers/base/js/elements.js
+++ project/static/helpers/base/js/elements.js
@@ -1,33 +1,41 @@
-const createIcon = (opt={}) => {
+const createIcon = ({className, parent} = {}) => {
     const icon = document.createElement('i')
-    icon.className = opt.className || ''
-    opt.parent?.appendChild(icon)
+    icon.className = className || ''
+    parent?.appendChild(icon)
 }
 
-const createLabel = (innerText, opt={}) => {
+const createLabel = (innerText, {className, parent} = {}) => {
     const label = document.createElement('span')
     label.innerText = innerText
-    label.className = opt.className || ''
-    opt.parent?.appendChild(label)
+    label.className = className || ''
+    parent?.appendChild(label)
 }
 
-const createOffcanvasToggle = (id, opt={}) => {
-    const toggle = document.createElement(opt.tag)
+const createOffcanvasToggle = (id, {
+    tag,
+    className,
+    themed,
+    label,
+    show,
+    title,
+    parent
+} = {}) => {
+    const toggle = document.createElement(tag)
     toggle.className = removeWhitespace(`
-        ${opt.class}
-        ${opt.tag === 'button' ?  `btn` : ''}
-        ${opt.themed ? `${opt.tag === 'button' ? 'btn' : 'text-bg'}-${getPreferredTheme()}` : ''}
-        ${opt.label ? 'rounded-pill' : 'rounded-circle'}
-        ${opt.show ? 'd-lg-none' : ''}
+        ${className}
+        ${tag === 'button' ?  `btn` : ''}
+        ${themed ? `${tag === 'button' ? 'btn' : 'text-bg'}-${getPreferredTheme()}` : ''}
+        ${label ? 'rounded-pill' : 'rounded-circle'}
+        ${show ? 'd-lg-none' : ''}
         shadow-lg d-flex flex-nowrap
     `)
-    toggle.setAttribute(`${opt.tag === 'button' ? 'type' : 'role'}`, 'button') 
+    toggle.setAttribute(`${tag === 'button' ? 'type' : 'role'}`, 'button') 
     toggle.setAttribute('data-bs-toggle', 'offcanvas') 
     toggle.setAttribute('data-bs-target', `#${id}`) 
     toggle.setAttribute('aria-controls', id) 
-    toggle.setAttribute('title', opt.title)
+    toggle.setAttribute('title', title)
 
-    opt.parent?.appendChild(toggle)
+    parent?.appendChild(toggle)
 
     return toggle
 }
@@ -43,27 +51,20 @@ const createOffcanvas = (id, {
     toggleIconClass,
     toggleLabelClass = ''
 } = {}) => {
-    // const themed = opt.themed
-    // const show = opt.show
-    // const toggleTag = opt.toggleTag || 'button'
-    // const toggleClass = opt.toggleClass || ''
-    // const toggleLabelText = opt.toggleLabelText
-    // const toggleTitle = opt.toggleTitle || `Toggle ${toggleLabelText ? toggleLabelText : 'sidebar'}`
-    // const toggleParent = opt.toggleParent
-    // const toggleIconClass = opt.toggleIconClass
-    // const toggleLabelClass = opt.toggleLabelClass || ''
-
     const toggle = createOffcanvasToggle(id, {
         themed: themed,
         show: show,
         tag: toggleTag,
-        class: toggleClass,
+        className: toggleClass,
         title: toggleTitle,
         parent: toggleParent,
         label: toggleLabelText
     })
+
     if (toggleIconClass) createIcon({className: `bi ${toggleIconClass}`, parent: toggle})
     if (toggleLabelText) createLabel(toggleLabelText, {className: `ms-2 text-nowrap ${toggleLabelClass}`, parent: toggle})
+
+    
 
     return [toggle, null]
 }
