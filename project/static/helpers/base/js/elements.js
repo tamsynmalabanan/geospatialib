744b66f1d87af88a5c0259057587679b312fc0d0
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-03 13:06:44 +0530

    updated base.html template

--- project/static/helpers/base/js/elements.js
+++ project/static/helpers/base/js/elements.js
@@ -3,19 +3,32 @@ const createOffcanvas = (id, options={}) => {
     const toggleThemed = options.toggleThemed
     const toggleIconClass = options.toggleIcon
     const toggleLabel = options.toggleLabel
+    const toggleLabelClass = options.toggleLabelClass || ''
+    const toggleTitle = options.toggleTitle || `Toggle ${toggleLabel ? toggleLabel : 'sidebar'}`
+    const showOffcanvas = options.showOffcanvas
 
     const toggle = document.createElement(toggleTag)
     toggle.className = removeWhitespace(`
         ${toggleTag === 'button' ?  `btn` : ''}
         ${toggleThemed ? `text-bg-${getPreferredTheme()}` : ''}
         ${toggleLabel ? 'rounded-pill' : 'rounded-circle'}
-        shadow-lg
+        ${showOffcanvas ? 'd-lg-none' : ''}
+        shadow-lg d-flex flex-nowrap
     `)
-    if (toggleTag === 'button') toggle.type = 'button' 
-    if (toggleTag !== 'button') toggle.role = 'button'
+    toggle.setAttribute(`${toggleTag === 'button' ? 'type' : 'role'}`, 'button') 
     toggle.setAttribute('data-bs-toggle', 'offcanvas') 
     toggle.setAttribute('data-bs-target', `#${id}`) 
     toggle.setAttribute('aria-controls', id) 
+    toggle.setAttribute('title', toggleTitle)
+    
+    const toggleIcon = document.createElement('i')
+    toggleIcon.className = `bi ${toggleIconClass}`
+
+    if (toggleLabel) {
+        const toggleLabel = document.createElement('span')
+        toggleLabel.className = `ms-2 text-nowrap ${toggleLabelClass}`
+        toggleLabel.innerText = toggleLabel
+    }
     
     console.log(toggle)
 }
