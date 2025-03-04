a9769ad7a7c442b29768ccf9e6d196ecd46ff88a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-04 20:27:07 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/controls.js
+++ project/static/helpers/leaflet/js/controls.js
@@ -109,16 +109,21 @@ const handleLeafletMapControls = (map) => {
 }
 
 const applyThemeToLeafletControls = (container) => {
-    addClassListToSelection(
-        container, 
-        removeWhitespace(`
-            .leaflet-bar a, 
-            .leaflet-control a, 
-            .leaflet-control-attribution,
-            .leaflet-control-scale-line
-        `).trim(), 
-        [`text-bg-${getPreferredTheme()}`, 'text-reset']
-    )
+    // addClassListToSelection(
+    //     container, 
+    //     removeWhitespace(`
+    //         .leaflet-bar a, 
+    //         .leaflet-control a, 
+    //         .leaflet-control-attribution,
+    //         .leaflet-control-scale-line,
+    //         .leaflet-control-geocoder,
+    //     `).trim(), 
+    //     [`text-bg-${getPreferredTheme()}`, 'text-reset']
+    // )
+
+    container.querySelectorAll('leaflet-control').forEach(control => {
+        Array.from(control.children).forEach(child => child.classList.add(`text-bg-${getPreferredTheme()}`, 'text-reset'))
+    })
 }
 
 const toggleMapInteractivity = (map) => {
