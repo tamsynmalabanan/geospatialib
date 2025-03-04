17fcf4e59df7a534644d4e10e57dcf563a35c69f
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-04 23:40:56 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -4,7 +4,7 @@ const handleLeafletQueryPanel = (map, parent) => {
     parent.appendChild(container)
 
     const toolbar = document.createElement('div')
-    toolbar.className = 'border px-2'
+    toolbar.className = 'border rounded'
     container.appendChild(toolbar)
 
     const defaultBtnClassName = `btn btn-sm btn-${getPreferredTheme()}`
