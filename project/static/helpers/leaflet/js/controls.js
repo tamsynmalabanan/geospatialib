655af8d6fd8b5abfb1676a6647eaea4ce2acfbf4
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-04 20:21:16 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/controls.js
+++ project/static/helpers/leaflet/js/controls.js
@@ -30,6 +30,61 @@ const handleLeafletScaleBar = (map, include=true) => {
     L.control.scale({ position: 'bottomright' }).addTo(map)
 }
 
+const handleLeafletSearchBar = (map, include=true) => {
+    if (!include || !L.Control.geocoder) return
+
+    const geocoder = L.Control.geocoder({
+        defaultMarkGeocode: false,
+        position: 'topleft',
+    })
+    .on('markgeocode', (e) => {
+        var bbox = e.geocode.bbox;
+        var poly = L.polygon([
+            bbox.getSouthEast(),
+            bbox.getNorthEast(),
+            bbox.getNorthWest(),
+            bbox.getSouthWest()
+        ]);
+        map.fitBounds(poly.getBounds());
+    })
+    .addTo(map);
+
+    const geocoderContainer = geocoder.getContainer()
+    const topLeftContainer = map._controlCorners.topleft
+    if (topLeftContainer.firstChild !== geocoderContainer) {
+        topLeftContainer.insertBefore(geocoderContainer, topLeftContainer.firstChild);
+    }
+
+    const button = geocoderContainer.querySelector('button')
+    button.innerText = ''
+    button.innerHTML = createIcon({className: 'bi bi-binoculars-fill'}).outerHTML
+
+    const alternativesList = geocoderContainer.querySelector('.leaflet-control-geocoder-alternatives')
+    alternativesList.classList.add('list-unstyled', 'px-2')
+
+    const geocoderFieldsSelector = map.getContainer().parentElement.dataset.mapGeocoderFields
+    if (geocoderFieldsSelector) {
+        document.addEventListener('change', (event) => {
+            if (!event.target.matches(geocoderFieldsSelector)) return
+
+            const place = event.target.value
+            if (place === '') return
+         
+            geocoder.setQuery(place)
+            geocoder._geocode()
+        })
+        
+        geocoder.on('markgeocode', (e) => {
+            const geocoderFields = document.querySelectorAll(geocoderFieldsSelector)
+            geocoderFields.forEach(field => {
+                if (field.value.toLowerCase() === e.target._lastGeocode.toLowerCase()) {
+                    field.value = e.geocode.name
+                }
+            })
+        })
+    }
+}
+
 const leafletControls = {
     zoom: handleLeafletZoombar,
     scale: handleLeafletScaleBar,
