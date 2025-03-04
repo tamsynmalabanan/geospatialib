5766d4436c68dc72e6f6e7830007735713151b6a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-04 11:40:51 +0530

    updated base.html template

--- project/static/helpers/gis/js/panels.js
+++ project/static/helpers/gis/js/panels.js
@@ -3,14 +3,15 @@ const createMapPanels = (container, {} = {}) => {
     const [toggle, offcanvas] = createOffcanvas(id, {
         themed: true,
         toggleIconClass: 'bi-info-circle',
-        toggleLabelText: 'GeoPanel',
+        toggleLabelText: 'Geopanel',
         toggleLabelClass: 'd-none d-md-block',
         show: getCookie(`show_#${id}`) === 'true',
         offcanvasClass: 'offcanvas-end rounded',
         offcanvasToggleIcon: 'bi-layout-sidebar-inset-reverse',
         titleClass: 'h6'
     })
-    // create accordion, empty, handle each panel in control.js legend, query, anylists tools
+
+    // create accordion, empty, handle each panel in control.js legend, query, tools
     const [tabs, content] = createAccordion({
         'Legend': {
             id: `${id}-legend`,
