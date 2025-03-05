8286e4f79d24f4b899d8bdf5988db6750d168b3c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-05 12:58:44 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -81,7 +81,7 @@ const handleLeafletMapPanels = (map) => {
     control.onAdd = (map) => {
         const panel = L.DomUtil.create('div', 'map-panel')
         panel.classList.add('d-flex', 'flex-column', 'ms-60', 'mb-70')
-        panel.style.maxHeight = window.innerHeight - 20 + 'px';
+        panel.style.maxHeight = window.innerHeight - 100 + 'px';
         
         const [toggle, body] = createMapPanels(map.getContainer())
         panel.appendChild(toggle)
