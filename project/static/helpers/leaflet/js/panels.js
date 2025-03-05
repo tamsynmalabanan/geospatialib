86463891a1148d1aec4242d56ed67d4c30ba76be
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-06 02:49:02 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -3,7 +3,7 @@ const handleLeafletQueryPanel = (map, parent) => {
     
     const toolbar = document.createElement('div')
     toolbar.id = `${mapContainer.id}-panels-query-toolbar`
-    toolbar.className = 'd-flex px-3 py-2 border-bottom gap-2'
+    toolbar.className = 'd-flex px-3 py-2 border-bottom'
     parent.appendChild(toolbar)
 
     const results = document.createElement('div')
@@ -15,7 +15,6 @@ const handleLeafletQueryPanel = (map, parent) => {
             title: 'Query location coordinates',
             mapClickHandler: async (e) => {
                 const feature = turf.point([e.latlng.lng, e.latlng.lat])
-
             },
         },
         osmPoint: {
@@ -35,7 +34,7 @@ const handleLeafletQueryPanel = (map, parent) => {
         },
         divider: {
             tag: 'div',
-            className: 'vr',
+            className: 'vr m-2',
         },
         cancel: {
             iconClass: 'bi-arrow-counterclockwise',
