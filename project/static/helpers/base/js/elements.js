972cc218ea8530ee918ce0d8aeeac239402e1440
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-06 06:14:45 +0530

    updated base.html template

--- project/static/helpers/base/js/elements.js
+++ project/static/helpers/base/js/elements.js
@@ -272,7 +272,7 @@ const createDropdownMenuLi = ({innerText, parent}={}) => {
     parent?.appendChild(li)
     
     const btn = document.createElement('button')
-    btn.className = 'dropdown-item bg-transparent border-0 btn btn-sm fs-12'
+    btn.className = 'dropdown-item bg-transparent border-0 btn btn-sm fs-12 p-2'
     li.appendChild(btn)
 
     const label = createLabel(innerText)
