0eed824c21a9342eb079dd10a972978c669bbc31
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-07 16:10:21 +0530

    updated base.html template

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -6,34 +6,15 @@ const createGeoJSONChecklist = (geojsonList) => {
 }
 
 const createPointCoordinatesTable = (ptFeature, {precision = 6}={}) => {
-    const table = document.createElement('table')
-    table.className = `table table-borderless table-${getPreferredTheme()} table-sm m-0 p-0`
+    const container = document.createElement('div')
+    table.className = `d-flex flex-nowrap gap-2`
 
-    const tbody = document.createElement('tbody')
-    table.appendChild(tbody)
-
-
-    const headTr = document.createElement('tr')
-    tbody.appendChild(headTr)
-   
-    const th = document.createElement('th')
-    th.setAttribute('scope','col')
-    th.setAttribute('colspan','3')
-    th.innerText = 'Longitude & latitude'
-    headTr.appendChild(th)
-    
-    const valueTr = document.createElement('tr')
-    tbody.appendChild(valueTr)
     for (const coord of ptFeature.geometry.coordinates) {
-        const td = document.createElement('td')
-        td.innerText = coord.toFixed(precision)
-        valueTr.appendChild(td)
+        const span = document.createElement('span')
+        span.innerText = coord.toFixed(precision)
+        container.appendChild(span)
     }
     
-    const formatTd = document.createElement('td')
-    formatTd.className = 'text-center'
-    valueTr.appendChild(formatTd)
-
     const formatRadios = createRadios({
         'DD': {
             checked:true,
@@ -45,7 +26,52 @@ const createPointCoordinatesTable = (ptFeature, {precision = 6}={}) => {
     }, {
         containerClassName: 'd-flex flex-nowrap gap-2'
     })
-    formatTd.appendChild(formatRadios)
+    container.appendChild(formatRadios)
 
-    return table
+    return container
 }
+
+// const createPointCoordinatesTable = (ptFeature, {precision = 6}={}) => {
+//     const table = document.createElement('table')
+//     table.className = `table table-borderless table-${getPreferredTheme()} table-sm m-0 p-0`
+
+//     const tbody = document.createElement('tbody')
+//     table.appendChild(tbody)
+
+
+//     const headTr = document.createElement('tr')
+//     tbody.appendChild(headTr)
+   
+//     const th = document.createElement('th')
+//     th.setAttribute('scope','col')
+//     th.setAttribute('colspan','3')
+//     th.innerText = 'Longitude & latitude'
+//     headTr.appendChild(th)
+    
+//     const valueTr = document.createElement('tr')
+//     tbody.appendChild(valueTr)
+//     for (const coord of ptFeature.geometry.coordinates) {
+//         const td = document.createElement('td')
+//         td.innerText = coord.toFixed(precision)
+//         valueTr.appendChild(td)
+//     }
+    
+//     const formatTd = document.createElement('td')
+//     formatTd.className = 'text-center'
+//     valueTr.appendChild(formatTd)
+
+//     const formatRadios = createRadios({
+//         'DD': {
+//             checked:true,
+//             tooltip: 'Decimal degrees',
+//         },
+//         'DMS': {
+//             tooltip: 'Degrees, minutes, seconds',
+//         },
+//     }, {
+//         containerClassName: 'd-flex flex-nowrap gap-2'
+//     })
+//     formatTd.appendChild(formatRadios)
+
+//     return table
+// }
