50013d6aa7a1187c4686dcbdec8ba69afc60c015
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-08 12:01:45 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/layers.js
+++ project/static/helpers/leaflet/js/layers.js
@@ -5,53 +5,39 @@ const addLeafletBasemapLayer = (map) => L.tileLayer("//tile.openstreetmap.org/{z
 
 const getDefaultLeafletLayerStyle = (featureType, {
     color = generateRandomColor(),
+    opacity = 1,
     strokeWidth = 1,
-    pointClass = 'rounded-circle',
-    strokeColor,
     strokePattern = 'solid',
-    bodyColor = color,
-    bodyOpacity = 0.5,
-    iconSize = [12, 12]
+    strokeColor,
+    pointClass = 'rounded-circle',
+    iconSize = [12, 12],
+    fillColor = true,
+    fillOpacity = 0.25,
 } = {}) => {
     const hslaColor = manageHSLAColor(color)
-    const hslaBodyColor = manageHSLAColor(bodyColor)
 
     if (featureType.toLowerCase() === 'point') {
         const div = document.createElement('div')
         div.className = `h-100 w-100 ${pointClass}`
         div.style.border = `${strokeWidth}px ${strokePattern} ${strokeColor || hslaColor?.toString({l:hslaColor.l/2}) || 'grey'}`
-        div.style.backgroundColor = hslaBodyColor?.toString({a:bodyOpacity}) || bodyColor
-
+        div.style.backgroundColor = hslaColor?.toString({a:fillOpacity}) || color
         return L.divIcon({
             className: 'bg-transparent',
             iconSize: iconSize,
             html: div.outerHTML,
         });
     } else {
-        let opacity = options.opacity
-        if (!opacity) {
-            opacity = 1
-        }
-
         const properties = {
             color: color,
             weight: strokeWidth,
             opacity: opacity
         }
 
-        let fillColor = options.fillColor
+        fillColor = fillColor === true ? hslaColor ? hslaColor.toString({
+            l:(hslaColor.l/2*3) > 100 ? 100 : (hslaColor.l/2*3),
+        }) : 'white' : fillColor ? fillColor : null
+        
         if (fillColor) {
-            if (typeof fillColor === 'boolean') {
-                if (color.startsWith('hsla')) {
-                    [h,s,l,a] = color.split(',').map(str => parseNumberFromString(str))
-                    l = (l / 2 * 3)
-                    fillColor = `hsla(${h}, ${s}%, ${l > 100 ? 100 : l}%, ${a})`
-                } else {
-                    fillColor = white
-                }
-            }
-
-            const fillOpacity = options.fillOpacity || 0.25
             properties.fillOpacity = fillOpacity
             properties.fillColor = fillColor
         } else {
