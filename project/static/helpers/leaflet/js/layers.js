fed98c9590aa60c572917a9fd62c05c07ba9cd19
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-08 17:02:02 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/layers.js
+++ project/static/helpers/leaflet/js/layers.js
@@ -68,6 +68,11 @@ const getLeafletLayerStyle = (featureType, options={}) => {
             ${iconShadow ? `2px 2px 4px ${hslaColor?.toString({l:hslaColor.l/10}) || 'black'}` : ''}
             ${iconGlow ? `0 0 5px ${color}, 0 0 10px ${color}, 0 0 15px ${color}, 0 0 20px ${color}` : ''}
         `)
+        console.lof(div.style.textShadow)
+        console.lof(removeWhitespace(`
+            ${iconShadow ? `2px 2px 4px ${hslaColor?.toString({l:hslaColor.l/10}) || 'black'}` : ''}
+            ${iconGlow ? `0 0 5px ${color}, 0 0 10px ${color}, 0 0 15px ${color}, 0 0 20px ${color}` : ''}
+        `))
 
         return L.divIcon({
             className: 'bg-transparent',
