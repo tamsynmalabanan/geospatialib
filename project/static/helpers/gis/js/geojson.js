36423d844509a696f5c36f58843f067a647bcc5c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-08 10:16:19 +0530

    updated base.html template

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -27,7 +27,7 @@ const createPointCoordinatesTable = (ptFeature, {precision = 6}={}) => {
     titleToTooltip(copyBtn, 'Copy')
     copyBtn.addEventListener('click', () => {
         navigator.clipboard.writeText(`${latSpan.innerText} ${lngSpan.innerText}`)
-        // copyBtn.setAttribute('')
+        titleToTooltip(copyBtn, 'Copied')
     })
 
     container.appendChild(copyBtn)
