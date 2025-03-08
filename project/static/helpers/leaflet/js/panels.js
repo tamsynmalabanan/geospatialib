bf2597322b8816ff640152c770ac5ff908e474e0
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-08 14:33:34 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -13,6 +13,7 @@ const handleLeafletQueryPanel = (map, parent) => {
 
     const defaultStyle = {
         color: 'hsla(111, 100%, 54%, 1)',
+        iconStroke: false,
     }
 
     const queryTools = {
