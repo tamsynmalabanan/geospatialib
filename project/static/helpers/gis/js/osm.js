16bb89bcaaa9b9d427c439f776bc747d7a4e6d84
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-11 22:49:07 +0530

    updated base.html template

--- project/static/helpers/gis/js/osm.js
+++ project/static/helpers/gis/js/osm.js
@@ -86,17 +86,13 @@ const overpassToGeoJSON = (data, {
         const id = element.id
         const type = element.type
         const tags = element.tags || {}
-        console.log(tags)
-        console.log({...tags, ...{
-            osm_id: id,
-            osm_type: type,
-        }})
 
         const feature = turf.feature(properties={...tags, ...{
             osm_id: id,
             osm_type: type,
         }})
-        
+        console.log(feature)
+                
         if (type === 'relation') {
             const points = []
             const polygons = []
