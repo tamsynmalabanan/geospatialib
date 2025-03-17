1f40433fc09dd50b940c946c15b5bb67343a90b8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-17 21:49:30 +0530

    updated base.html template

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -142,7 +142,7 @@ const createGeoJSONChecklist = async (geojsonList, group, {
 
         const parentCheck = createFormCheck({
             parent: geojsonContainer,
-            labelInnerText: title,
+            labelInnerText: `${title} (${featureLayers.length})`,
         }).querySelector('input')
         if (disableCheck) {
             parentCheck.disabled = true
