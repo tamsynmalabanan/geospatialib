4b37cd2b26d8db6b0b7d7706ae9ecb6e5e6a9008
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-19 12:48:55 +0530

    updated base.html template

--- project/static/helpers/gis/js/proj4.js
+++ project/static/helpers/gis/js/proj4.js
@@ -2,13 +2,10 @@ const fetchProj4Def = async (crs, {
     abortBtns,
     controller,
 } = {}) => {
-    const urls = [
+    for (const url of [
         `/htmx/srs_wkt/${crs}/`,
         `https://spatialreference.org/ref/epsg/${crs}/ogcwkt`,
-    ]
-
-    for (const url of urls) {
-        console.log(url)
+    ]) {
         const def = await fetchTimeout(url, {
             abortBtns,
             controller,
@@ -27,21 +24,4 @@ const fetchProj4Def = async (crs, {
 
         if (def) return def
     }
-
-    // // const url = `https://spatialreference.org/ref/epsg/${crs}/ogcwkt`
-    // return fetchTimeout(`/htmx/srs_wkt/${crs}/`, {
-    //     abortBtns,
-    //     controller,
-    //     fetchParams: {headers: {'HX-Request': 'true'}}
-    // }).then(response => {
-    //     if (!response.ok && (response.status < 200 || response.status > 300)) {
-    //         throw new Error('Response not ok.')
-    //     }
-
-    //     return response.text()
-    // }).then(def => {
-    //     const crs_text = `EPSG:${crs}`
-    //     proj4.defs(crs_text, def)
-    //     return proj4.defs(crs_text)
-    // }).catch(error => console.error(error))
 }
