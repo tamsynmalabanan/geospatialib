73ee96eed0fcf2b7ad953711d6263f35f20d3cae
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-19 12:46:51 +0530

    updated base.html template

--- project/static/helpers/gis/js/proj4.js
+++ project/static/helpers/gis/js/proj4.js
@@ -2,21 +2,46 @@ const fetchProj4Def = async (crs, {
     abortBtns,
     controller,
 } = {}) => {
-    const url = `/htmx/srs_wkt/${crs}/`
-    // const url = `https://spatialreference.org/ref/epsg/${crs}/ogcwkt`
-    return fetchTimeout(url, {
-        abortBtns,
-        controller,
-        fetchParams: {headers: {'HX-Request': 'true'}}
-    }).then(response => {
-        if (!response.ok && (response.status < 200 || response.status > 300)) {
-            throw new Error('Response not ok.')
-        }
+    const urls = [
+        `/htmx/srs_wkt/${crs}/`,
+        `https://spatialreference.org/ref/epsg/${crs}/ogcwkt`,
+    ]
 
-        return response.text()
-    }).then(def => {
-        const crs_text = `EPSG:${crs}`
-        proj4.defs(crs_text, def)
-        return proj4.defs(crs_text)
-    }).catch(error => console.error(error))
+    for (const url of urls) {
+        console.log(url)
+        const def = await fetchTimeout(url, {
+            abortBtns,
+            controller,
+            fetchParams: {headers: {'HX-Request': 'true'}}
+        }).then(response => {
+            if (!response.ok && (response.status < 200 || response.status > 300)) {
+                throw new Error('Response not ok.')
+            }
+    
+            return response.text()
+        }).then(def => {
+            const crs_text = `EPSG:${crs}`
+            proj4.defs(crs_text, def)
+            return proj4.defs(crs_text)
+        }).catch(error => console.error(error))
+
+        if (def) return def
+    }
+
+    // // const url = `https://spatialreference.org/ref/epsg/${crs}/ogcwkt`
+    // return fetchTimeout(`/htmx/srs_wkt/${crs}/`, {
+    //     abortBtns,
+    //     controller,
+    //     fetchParams: {headers: {'HX-Request': 'true'}}
+    // }).then(response => {
+    //     if (!response.ok && (response.status < 200 || response.status > 300)) {
+    //         throw new Error('Response not ok.')
+    //     }
+
+    //     return response.text()
+    // }).then(def => {
+    //     const crs_text = `EPSG:${crs}`
+    //     proj4.defs(crs_text, def)
+    //     return proj4.defs(crs_text)
+    // }).catch(error => console.error(error))
 }
