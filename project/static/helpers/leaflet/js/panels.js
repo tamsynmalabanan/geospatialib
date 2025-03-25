f53660ccb66d87bdbe42e9a4f4ddc2db081f2c92
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-25 13:25:06 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -279,9 +279,9 @@ const handleLeafletLegendPanel = (map, parent) => {
                     }   
                     
                     const mouseUpHandler = (e3) => {
-                        const offset = container.style.top
-                        console.log(parseInt(offset))
-                        if (Math.abs(parseInt(offset)) < 10) {
+                        const offset = parseInt(container.style.top)
+                        console.log(offset)
+                        if (Math.abs(offset) < 10) {
                             container.style.top = '0px'
                             return
                         }
