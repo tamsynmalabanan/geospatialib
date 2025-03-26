76af006d8a4c2f9afa5fc4142b30aec83cfb9b9b
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-26 18:43:47 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -1,9 +1,25 @@
 const handlerLeafletRenderer =(map) => {
+    map._currentRenderer = 'svg'
+
     let timeout
     map.on('layeradd layerremove', (e) => {
         clearTimeout(timeout)
         timeout = setTimeout(() => {
-            console.log(map.getContainer().querySelectorAll('path').length)
+            const count = map.getContainer().querySelectorAll('path').length
+            if ((count > 1000 && map._currentRenderer !== 'canvas') || (count <= 1000 && map._currentRenderer !== 'svg')) {
+                map._currentRenderer = map._currentRenderer === 'canvas' ? 'svg' : 'canvas'
+                console.log(map._currentRenderer)
+                map._ch.getLayerGroups().forEach(g => {
+                    console.log()
+                    g.eachLayer(l => {
+                        console.log(l)
+                    })
+                    
+                    g._ch.getHiddenLayers().forEach(l => {
+                        console.log(l)
+                    })
+                })
+            }
         }, 100);
     })
 }
