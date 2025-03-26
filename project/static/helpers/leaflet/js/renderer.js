b7dc2d3e78a5cea88250275add148e62ba0e04a8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-26 21:06:21 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -6,6 +6,7 @@ const handlerLeafletRenderer =(map) => {
         clearTimeout(timeout)
         timeout = setTimeout(() => {
             if (e.layer instanceof L.GeoJSON) {
+                console.log(e.layer)
                 const threshold = 100
                 const count = map.getContainer().querySelectorAll('path').length
                 if ((count > threshold && map._currentRenderer !== 'canvas') || (count <= threshold && map._currentRenderer !== 'svg')) {
@@ -21,7 +22,7 @@ const handlerLeafletRenderer =(map) => {
                 
                         if (map._currentRenderer === 'svg' && layer.options.renderer instanceof L.SVG) return
                         if (map._currentRenderer === 'canvas' && layer.options.renderer instanceof L.Canvas) return
-                        
+
                         layer.fire('rendererupdated', {renderer:map._currentRenderer})
                     })
                 })
