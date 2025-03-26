ec991904555aaed7777fd7d634cd752da254de3e
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-26 19:19:48 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -5,8 +5,9 @@ const handlerLeafletRenderer =(map) => {
     map.on('layeradd layerremove', (e) => {
         clearTimeout(timeout)
         timeout = setTimeout(() => {
+            const threshold = 100
             const count = map.getContainer().querySelectorAll('path').length
-            if ((count > 1000 && map._currentRenderer !== 'canvas') || (count <= 1000 && map._currentRenderer !== 'svg')) {
+            if ((count > threshold && map._currentRenderer !== 'canvas') || (count <= threshold && map._currentRenderer !== 'svg')) {
                 map._currentRenderer = map._currentRenderer === 'canvas' ? 'svg' : 'canvas'
                 Object.values(map._ch.getLayerGroups()).forEach(g => {
                     g._ch.getAllLayers().forEach(l => {
