471e6c1aabb5f86552a0d7942e6d55b18ec780cc
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-26 21:06:59 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -6,7 +6,6 @@ const handlerLeafletRenderer =(map) => {
         clearTimeout(timeout)
         timeout = setTimeout(() => {
             if (e.layer instanceof L.GeoJSON) {
-                console.log(e.layer)
                 const threshold = 100
                 const count = map.getContainer().querySelectorAll('path').length
                 if ((count > threshold && map._currentRenderer !== 'canvas') || (count <= threshold && map._currentRenderer !== 'svg')) {
@@ -14,11 +13,13 @@ const handlerLeafletRenderer =(map) => {
                     map._currentRenderer = isCanvas ? 'svg' : 'canvas'
                 }
                 Object.values(map._ch.getLayerGroups()).forEach(group => {
+                    console.log(group._ch.getAllLayers())
                     group._ch.getAllLayers().forEach(layer => {
-                        Object.keys(layer._renderers).forEach(k => {
-                            layer._renderers[k]._container
-                            .classList.toggle('d-none', k !== map._currentRenderer)
-                        })
+                        // console.log(layer)
+                        // Object.keys(layer._renderers).forEach(k => {
+                        //     layer._renderers[k]._container
+                        //     .classList.toggle('d-none', k !== map._currentRenderer)
+                        // })
                 
                         if (map._currentRenderer === 'svg' && layer.options.renderer instanceof L.SVG) return
                         if (map._currentRenderer === 'canvas' && layer.options.renderer instanceof L.Canvas) return
