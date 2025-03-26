1884e7ddd77149536730e4877ed738f22f774ef6
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-26 15:33:44 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -1,5 +1,5 @@
 const handlerLeafletRenderer =(map) => {
-    map.on('layeradd', (e) => {
+    map.on('layeradd layerremove', (e) => {
         console.log(map.getContainer().querySelectorAll('path').length)
     })
 }
