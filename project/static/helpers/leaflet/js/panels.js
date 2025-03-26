5fadd1bd40a97291afc39cc98aa105c336bf6d18
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-26 09:13:25 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -275,7 +275,6 @@ const handleLeafletLegendPanel = (map, parent) => {
 
                     const mouseMoveHandler = (e2) => {
                         document.body.classList.add('user-select-none')
-                        
                         const newY = e2.type === 'touchmove' ? e2.touches[0].clientY : e2.clientY
                         container.style.top =`${newY - startY}px`;
                     
@@ -290,7 +289,12 @@ const handleLeafletLegendPanel = (map, parent) => {
                     
                     const mouseUpHandler = (e3) => {
                         const offset = parseInt(container.style.top)
-                        if (Math.abs(offset) > 10) {
+                        if (Math.abs(offset) < 10) {
+                            Array.from(layers.children).forEach(c => {
+                                c.style.top = '0px'
+                                c.classList.remove('highlight')
+                            })
+                        } else {
                             const referenceLegend = document.elementsFromPoint(e3.x, e3.y).find(el => {
                                 if (el.matches(`[data-layer-legend="true"]:not([data-layer-id="${layer._leaflet_id}"]`)) return el
                             }) 
@@ -312,17 +316,15 @@ const handleLeafletLegendPanel = (map, parent) => {
                             const layerLegends = Array.from(layers.children).reverse()
                             for (let i=0; i<layerLegends.length; i++) {
                                 const child = layerLegends[i]
+                                child.style.top = '0px'
+                                child.classList.remove('highlight')
+                                
                                 const paneName = child.dataset.layerPane
                                 const pane = map.getPane(paneName)
                                 pane.style.zIndex = i + 200
                             }
                         }
 
-                        Array.from(layers.children).forEach(c => {
-                            c.style.top = '0px'
-                            c.classList.remove('highlight')
-                        })
-                        
                         document.body.classList.remove('user-select-none')
                     }                
 
