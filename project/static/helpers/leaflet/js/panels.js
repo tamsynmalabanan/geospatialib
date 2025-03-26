c7bc4565c598415a288d6ce21668243f03cc2fef
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-26 08:50:48 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -276,6 +276,11 @@ const handleLeafletLegendPanel = (map, parent) => {
                         document.body.classList.add('user-select-none')
                         const newY = e2.type === 'touchmove' ? e2.touches[0].clientY : e2.clientY
                         container.style.top =`${newY - startY}px`;
+                    
+                        const referenceLegend = document.elementsFromPoint(e2.x, e2.y).find(el => {
+                            if (el.matches(`[data-layer-legend="true"]:not([data-layer-id="${layer._leaflet_id}"]`)) return el
+                        })
+                        Array.from(layers.children).forEach(c => c.classList.toggle('highlight', c === referenceLegend)) 
                     }   
                     
                     const mouseUpHandler = (e3) => {
