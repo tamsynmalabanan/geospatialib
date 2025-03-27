152aa5361daebbc4de9d555317870c6d0a5d06a9
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-27 13:28:23 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -8,32 +8,22 @@ const handlerLeafletRenderer =(map) => {
         if (feature && !isPoint) {
             clearTimeout(timeout)
             timeout = setTimeout(() => {
-                console.log('EVENT', e)
-    
                 const featureLayers = []
                 Object.values(map._ch.getLayerGroups()).forEach(group => {
-                    // console.log('group name', group._name)
                     group.eachLayer(layer => {
                         const type = getLeafletLayerType(layer)
-                        // if (!['geojson', 'feature'].includes(type)) return
-                        
                         const geojsonLayer = type === 'geojson' ? layer : findFeatureLayerGeoJSONLayer(layer)
-                        // console.log('geojsonLayer', geojsonLayer)
                         const layers = group.hasLayer(geojsonLayer) ? geojsonLayer.getLayers() : [layer]
-                        // console.log('layers', layers)
                         layers.forEach(l => {
-                            if (featureLayers.includes(l)) return console.log('already in featureLayers', l)
-                            if (!group.hasLayer(l) && !group.hasLayer(geojsonLayer)) return console.log('not shown', group.getLayers(), l)
-                            if (l.feature.geometry.type.toLowerCase().endsWith('point')) return console.log('is point', l)
+                            if (featureLayers.includes(l)) return
+                            if (!group.hasLayer(l) && !group.hasLayer(geojsonLayer)) return
+                            if (l.feature.geometry.type.toLowerCase().endsWith('point')) return
                             console.log('new feature added', l)
                             featureLayers.push(l)
                         })
-                        // console.log('featureLayers after geojson layers loop', featureLayers)
                     })
-                    // console.log('featureLayers after group loop', featureLayers)
                 })
-
-                console.log('END', featureLayers, featureLayers.length)
+                console.log(featureLayers, featureLayers.length)
             }, 100);
         }
     })
