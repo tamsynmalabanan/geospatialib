fd2aba7b9e638bb964de8d6f3a70d57a5c42d038
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-27 15:03:33 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -3,16 +3,16 @@ const handlerLeafletRenderer =(map) => {
     let timeout
     
     map.on('layeradd layerremove', (e) => {
-        
+        if (activeLayers.includes(e.layer)) {
+            if (e.type === 'layeradd') activeLayers = activeLayers.filter(l => l !== e.layer)
+            return
+        }
+
         const feature = e.layer.feature
         const isPoint = feature && feature.geometry.type.toLowerCase().endsWith('point')
         if (feature && !isPoint) {
             clearTimeout(timeout)
             timeout = setTimeout(() => {
-                if (activeLayers.includes(e.layer)) {
-                    if (e.type === 'layeradd') activeLayers = activeLayers.filter(l => l !== e.layer)
-                    return console.log('active layer', e.layer)
-                }
                 const layerGroups = Object.values(map._ch.getLayerGroups())
                 
                 const featureLayers = []
@@ -33,29 +33,19 @@ const handlerLeafletRenderer =(map) => {
                 })
                 
                 const renderer = featureLayers.length > 100 ? L.Canvas : L.SVG
-                layerGroups.forEach(group => {
-                    const isLegendGroup = map._legendLayerGroups.includes(group)
-                    group.eachLayer(layer => {
-                        const type = getLeafletLayerType(layer)
-                        if (!['geojson', 'feature'].includes(type)) return
-                        
-                        const geojsonLayer = type === 'geojson' ? layer : findFeatureLayerGeoJSONLayer(layer)
-                        const currentRenderer = layer.options.renderer || geojsonLayer?.options.renderer
-                        if (currentRenderer instanceof renderer) return
-                        
-                        const newRenderer = Object.values(geojsonLayer._renderers).find(r => {
-                            const isRenderer = r instanceof renderer
-                            r._container?.classList.toggle('d-none', !isRenderer)
-                            return isRenderer
-                        })
-                        geojsonLayer.options.renderer = newRenderer
-                        Array(geojsonLayer, ...geojsonLayer.getLayers()).forEach(l => {
-                            if (!group.hasLayer(l)) return
-                            activeLayers.push(l)
-                            isLegendGroup ? group._ch.hideLayer(l) : group.removeLayer(l)
-                            group._ch.showLayer(l)
-                        })
+                featureLayers.forEach(layer => {
+                    if (layer._renderer instanceof renderer) return
+                    
+                    const geojsonLayer = findFeatureLayerGeoJSONLayer(layer)
+                    const newRenderer = Object.values(geojsonLayer._renderers).find(r => {
+                        const isRenderer = r instanceof renderer
+                        r._container?.classList.toggle('d-none', !isRenderer)
+                        return isRenderer
                     })
+                    [layer, geojsonLayer].forEach(l => l.options.renderer = newRenderer)
+                    activeLayers.push(layer)
+                    isLegendGroup ? group._ch.hideLayer(l) : group.removeLayer(l)
+                    group._ch.showLayer(l)
                 })
             }, 100);
         }
