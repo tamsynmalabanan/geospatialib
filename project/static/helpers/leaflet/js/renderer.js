78c3e09902232c84a7ab496e19dff261f6741d99
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-27 16:19:22 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -39,8 +39,7 @@ const handlerLeafletRenderer =(map) => {
                     if (!['geojson', 'feature'].includes(type)) return
                     
                     const geojsonLayer = type === 'geojson' ? layer : findFeatureLayerGeoJSONLayer(layer)
-                    console.log(mappedLayers.keys())
-                    if (geojsonLayers.includes(geojsonLayer)) return
+                    if (geojsonLayers.includes(geojsonLayer) || Array.from(mappedLayers.keys()).includes(geojsonLayer)) return
 
                     geojsonLayers.push(geojsonLayer)
                     const layers = group.hasLayer(geojsonLayer) ? geojsonLayer.getLayers() : geojsonLayer.getLayers().filter(l => group.hasLayer(l))
