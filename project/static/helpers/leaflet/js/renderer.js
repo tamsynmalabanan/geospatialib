3ec885d8fba0271d63556c92aff122a312afe5e8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-27 12:29:44 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -3,18 +3,23 @@ const handlerLeafletRenderer =(map) => {
     
     let timeout
     map.on('layeradd layerremove', (e) => {
-        clearTimeout(timeout)
-        timeout = setTimeout(() => {
-            console.log(e)
-
-            let featureLayers = []
-            const layerGroups = Object.values(map._ch.getLayerGroups())
-            layerGroups.forEach(g => g.eachLayer(l => {
-                const feature = l.feature
-                const isPoint = feature && feature.geometry.type.toLowerCase().endsWith('point')
-                if (feature && !isPoint) featureLayers.push(l)
-            }))
-            console.log(featureLayers.length)
-        }, 1000);
+        const layer = e.layer
+        const feature = layer.feature
+        const isPoint = feature && feature.geometry.type.toLowerCase().endsWith('point')
+        if (feature && !isPoint) {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                console.log(e)
+    
+                let featureLayers = []
+                const layerGroups = Object.values(map._ch.getLayerGroups())
+                layerGroups.forEach(g => g.eachLayer(l => {
+                    const feature = l.feature
+                    const isPoint = feature && feature.geometry.type.toLowerCase().endsWith('point')
+                    if (feature && !isPoint) featureLayers.push(l)
+                }))
+                console.log(featureLayers.length)
+            }, 1000);
+        }
     })
 }
