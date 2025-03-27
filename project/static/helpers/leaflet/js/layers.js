152688c6809e106455a99cc080c0ad412752e4a4
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-27 11:12:11 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/layers.js
+++ project/static/helpers/leaflet/js/layers.js
@@ -159,9 +159,11 @@ const getLeafletLayerType = (layer) => {
 }
 
 const findFeatureLayerGeoJSONLayer = (layer) => {
+    console.log(layer)
     if (!layer.feature || !layer._eventParents?.length) return
 
     for (const p of Object.values(layer._eventParents)) {
+        console.log(p)
         if (p instanceof L.GeoJSON) return p
     }
 }
@@ -170,6 +172,7 @@ const getLeafletLayerContextMenu = (e, layer, {
     geojson = layer.toGeoJSON ? layer.toGeoJSON() : null,
 } = {}) => {
     const feature = layer.feature
+    console.log(findFeatureLayerGeoJSONLayer(layer))
     const group = feature ? findFeatureLayerGeoJSONLayer(layer)?._group : layer._group
     if (!group) return
 
