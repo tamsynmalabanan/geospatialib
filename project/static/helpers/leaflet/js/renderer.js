78e23eba846e52ea43cce657d69db2f080c8f416
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-27 12:22:48 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -5,10 +5,14 @@ const handlerLeafletRenderer =(map) => {
     map.on('layeradd layerremove', (e) => {
         clearTimeout(timeout)
         timeout = setTimeout(() => {
+            console.log(e)
+
             const featureLayers = []
             const layerGroups = Object.values(map._ch.getLayerGroups())
             layerGroups.forEach(g => g.eachLayer(l => {
-                if (l.feature && !l.feature.geometry.type.toLowerCase().endsWith('point')) featureLayers.push(l)
+                const feature = l.feature
+                const isPoint = feature && feature.geometry.type.toLowerCase().endsWith('point')
+                if (feature && !isPoint) featureLayers.push(l)
             }))
             console.log(featureLayers.length)
         }, 100);
