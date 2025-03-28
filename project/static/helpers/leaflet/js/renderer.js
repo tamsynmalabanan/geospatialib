296813d86ded8c6afb89c7f6ce78f681f6744052
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-29 00:48:26 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/renderer.js
+++ project/static/helpers/leaflet/js/renderer.js
@@ -23,42 +23,42 @@ const resolveRerenderedLeafletPathLayer = (oldLayer, newLayer) => {
 }
 
 const handlerLeafletRenderer = (map) => {
-    map._rendererFn = L.SVG
+    // map._rendererFn = L.SVG
     
-    let timeout
-    const renderingLayers = new Map()
-    map.on('layeradd layerremove', (e) => {
-        const gslId = e.layer.feature?.properties?.gslId
-        if (renderingLayers.get(gslId)) {
-            if (e.type === 'layeradd') renderingLayers.delete(gslId)
-            return
-        }
+    // let timeout
+    // const renderingLayers = new Map()
+    // map.on('layeradd layerremove', (e) => {
+    //     const gslId = e.layer.feature?.properties?.gslId
+    //     if (renderingLayers.get(gslId)) {
+    //         if (e.type === 'layeradd') renderingLayers.delete(gslId)
+    //         return
+    //     }
 
-        clearTimeout(timeout)
-        timeout = setTimeout(() => {
-            const pathLayers = getLeafletPathLayers(map)
-            const renderer = pathLayers.size > 5 ? L.Canvas : L.SVG
-            if (map._rendererFn === renderer) return
+    //     clearTimeout(timeout)
+    //     timeout = setTimeout(() => {
+    //         const pathLayers = getLeafletPathLayers(map)
+    //         const renderer = pathLayers.size > 5 ? L.Canvas : L.SVG
+    //         if (map._rendererFn === renderer) return
 
-            map._rendererFn = renderer
-            for (const [layer, parent] of pathLayers) {
-                if (layer.options.renderer instanceof renderer) return
+    //         map._rendererFn = renderer
+    //         for (const [layer, parent] of pathLayers) {
+    //             if (layer.options.renderer instanceof renderer) return
                 
-                const geojsonLayer = findLeafletFeatureLayerParent(layer)
-                geojsonLayer.options.renderer = Object.values(geojsonLayer._renderers).find(r => {
-                    const match = r instanceof renderer
-                    if (!match) r._container?.remove()
-                    return match
-                })
+    //             const geojsonLayer = findLeafletFeatureLayerParent(layer)
+    //             geojsonLayer.options.renderer = Object.values(geojsonLayer._renderers).find(r => {
+    //                 const match = r instanceof renderer
+    //                 if (!match) r._container?.remove()
+    //                 return match
+    //             })
             
-                const gslId = layer.feature.properties.gslId
-                renderingLayers.set(gslId, layer._leaflet_id)
-                layer.removeFrom(geojsonLayer)
-                geojsonLayer.addData(layer.toGeoJSON())
-                const newLayer = geojsonLayer.findGslId(gslId)
-                resolveRerenderedLeafletPathLayer(layer, newLayer)
-                parent.addLayer(newLayer)
-            }
-        }, 100);
-    })
+    //             const gslId = layer.feature.properties.gslId
+    //             renderingLayers.set(gslId, layer._leaflet_id)
+    //             layer.removeFrom(geojsonLayer)
+    //             geojsonLayer.addData(layer.toGeoJSON())
+    //             const newLayer = geojsonLayer.findGslId(gslId)
+    //             resolveRerenderedLeafletPathLayer(layer, newLayer)
+    //             parent.addLayer(newLayer)
+    //         }
+    //     }, 100);
+    // })
 }
