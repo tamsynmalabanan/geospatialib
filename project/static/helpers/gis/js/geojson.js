f8b34fe08f8d7e3ed41c61fb1cbf7d4a9b6cd92a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-03-29 10:56:52 +0530

    updated base.html template

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -87,7 +87,7 @@ const createAttributionTable = (geojson) => {
 
     if (Object.keys(info).length) {
         const infoTable = document.createElement('table')
-        infoTable.className = `table table-${getPreferredTheme()} small table-borderless table-sm m-0`
+        infoTable.className = `table small table-borderless table-sm m-0`
 
         const infoTBody = document.createElement('tbody')
         createObjectTRs(info, infoTBody)
