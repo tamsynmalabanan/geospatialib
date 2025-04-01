318da5c737fb23747358c1403646a634558285a5
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-01 17:35:46 +0530

    updated base.html template

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -405,46 +405,46 @@ const fetchGeoJSON = async ({
             geojson = await (async () => {
                 try {
                     const fromCache = (async () => {
-                        const cached = await getFromGeoJSONDB(dbKey);
-                        if (!cached) return;
+                        const cached = await getFromGeoJSONDB(dbKey)
+                        if (!cached) return
                         
                         try {
-                            const equalBounds = turf.booleanEqual(queryExtent, cached._queryExtent);
-                            const withinBounds = turf.booleanWithin(queryExtent, cached._queryExtent);
-                            if (!equalBounds && !withinBounds) return;
+                            const equalBounds = turf.booleanEqual(queryExtent, cached._queryExtent)
+                            const withinBounds = turf.booleanWithin(queryExtent, cached._queryExtent)
+                            if (!equalBounds && !withinBounds) return
                         } catch (error) {
-                            return;
+                            return
                         }
                         
                         const features = cached.features.filter(feature => {
-                            const featureBbox = turf.bboxPolygon(turf.bbox(feature));
-                            return turf.booleanIntersects(queryExtent, featureBbox);
-                        });
+                            const featureBbox = turf.bboxPolygon(turf.bbox(feature))
+                            return turf.booleanIntersects(queryExtent, featureBbox)
+                        })
                         
-                        if (features.length === 0) return;
+                        if (features.length === 0) return
                         
-                        saveToGeoJSONDB(dbKey, cached);
-                        return turf.featureCollection(features);
-                    })();
+                        saveToGeoJSONDB(dbKey, cached)
+                        return turf.featureCollection(features)
+                    })()
                     
                     const fromAPI = (async () => {
-                        const geojson = await handler(event, {...options, controller, abortBtns});
-                        if (!geojson?.features?.length) return;
-                        
-                        if (controller?.signal.aborted) return;
-                        await handleGeoJSON(geojson, {queryGeom, controller, abortBtns});
-                        
-                        geojson._queryExtent = queryExtent;
-                        const {type, features, _queryExtent} = turf.clone(geojson);
-                        await updateGeoJSONOnDB(dbKey, {type, features, _queryExtent});
-                        
-                        return geojson;
-                    })();
+                        const geojson = await handler(event, {...options, controller, abortBtns})
+                        if (!geojson?.features?.length) return
                     
-                    return await Promise.race([fromCache, fromAPI]);
+                        if (controller?.signal.aborted) return
+                        await handleGeoJSON(geojson, {queryGeom, controller, abortBtns})
+                        
+                        geojson._queryExtent = queryExtent
+                        const {type, features, _queryExtent} = turf.clone(geojson)
+                        await updateGeoJSONOnDB(dbKey, {type, features, _queryExtent})
+                        
+                        return geojson
+                    })()
+                    
+                    return await Promise.race([fromCache, fromAPI])
                 } catch (error) {
-                    console.error("Error fetching GeoJSON:", error);
-                    return null;
+                    console.error("Error fetching GeoJSON:", error)
+                    return null
                 }
             })();
             
