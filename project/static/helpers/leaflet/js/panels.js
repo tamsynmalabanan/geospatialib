9f272c539f115667c553d8d46c9ba683182f6fe2
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-02 20:06:54 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -465,7 +465,6 @@ const handleLeafletLegendPanel = (map, parent) => {
 
 const handleLeafletStylePanel = (map, parent) => {
     const container = document.createElement('form')
-    container.setAttribute('')
     container.className = 'd-flex p-3 flex-grow-1 flex-column gap-3'
     parent.appendChild(container)
 
