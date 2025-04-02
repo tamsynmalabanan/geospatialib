9280795dca7499adfe3c8cf982fdcd79e9481981
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-02 19:22:34 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -471,7 +471,7 @@ const handleLeafletStylePanel = (map, parent) => {
     const select = createFormFloating({
         parent: container,
         fieldTag: 'select', 
-        fieldClassName: 'form-select-sm'
+        fieldClassName: 'form-select-sm',
         fieldAttrs: {
             name: 'layer',
         },
