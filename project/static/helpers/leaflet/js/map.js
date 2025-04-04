7c087409b3383a09a4a2c87cd34f22f260561460
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-04 21:00:00 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/map.js
+++ project/static/helpers/leaflet/js/map.js
@@ -46,7 +46,7 @@ const zoomLeafletMapToScale = (map, scale) => {
         }
     }
 
-    const newZoom = diff[Math.min(...Object.keys(diff))]+1
+    const newZoom = diff[Math.min(...Object.keys(diff))] + (newZoom > 15 ? 1 : 0)
     map.setZoom(newZoom)
     console.log(scale, leafletZoomToMeter(newZoom), newZoom, map.getZoom())
 
