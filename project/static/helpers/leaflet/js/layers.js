f5d67bcee5539a02a447e72cd5d1ecbe91466a52
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-07 22:23:56 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/layers.js
+++ project/static/helpers/leaflet/js/layers.js
@@ -66,7 +66,7 @@ const getLeafletLayerStyle = (featureType, styleParams={}) => {
         div.style.fontSize = `${iconSize}px`
         div.style.color = manageHSLAColor(fillColor)?.toString({a:fillOpacity}) || fillColor
         div.style.WebkitTextStroke = `${strokeWidth}px ${manageHSLAColor(strokeColor)?.toString({a:strokeOpacity}) || strokeColor}`
-        const glow = hslaColor?.toString({a:fillOpacity/10}) || color
+        const glow = hslaColor?.toString({a:fillOpacity}) || color
         div.style.textShadow = Array(
             iconShadow ? `2px 2px 4px ${hslaColor?.toString({l:hslaColor.l/10,a:fillOpacity}) || 'black'}` : '',
             iconGlow ? `0 0 5px ${glow}, 0 0 10px ${glow}, 0 0 15px ${glow}, 0 0 20px ${glow}` : ''
