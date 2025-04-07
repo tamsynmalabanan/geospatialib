b79a4c170b484ab37c50bf869a2dae018fa79f0d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-07 21:01:19 +0530

    updated base.html template

--- project/static/helpers/base/js/elements.js
+++ project/static/helpers/base/js/elements.js
@@ -8,6 +8,7 @@ const customCreateElement = ({
     const element = document.createElement(tag)
     element.id = id || generateRandomString()
     element.className = className
+    console.log(parent)
     parent?.appendChild(element)
     Object.keys(style).forEach(k => element.style[k] = style[k])
     return element
--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -1232,7 +1232,6 @@ const handleLeafletMapPanels = (map) => {
 }
 
 const bootstrapIConsDatalist = customCreateElement('datalist', {parent:document.body})
-console.log(bootstrapIConsDatalist.parentElement)
 document.addEventListener('DOMContentLoaded', () => {
     fetch('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css')
     .then(response => {
