9253094095bc9b14031d8d607b7b76fdc07c05b7
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-07 21:02:01 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -1231,7 +1231,7 @@ const handleLeafletMapPanels = (map) => {
     control.addTo(map)
 }
 
-const bootstrapIConsDatalist = customCreateElement('datalist', {parent:document.body})
+const bootstrapIConsDatalist = customCreateElement({tagL:'datalist',parent:document.body})
 document.addEventListener('DOMContentLoaded', () => {
     fetch('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css')
     .then(response => {
