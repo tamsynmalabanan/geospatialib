e021174db80a5802b35560eef30c00a6d452c0d9
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-07 12:36:34 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/layers.js
+++ project/static/helpers/leaflet/js/layers.js
@@ -347,20 +347,19 @@ const getLeafletLayerContextMenu = async (e, layer, {
                 const pane = createCustomPane(map)
 
                 const geojsonLayer = findLeafletFeatureLayerParent(layer)
+                const attribution = feature ? geojsonLayer._attribution : layer._attribution
                 const styles = feature ? geojsonLayer._styles : layer._styles
-                const title = styles.title
-                const attribution = styles.attribution
                 
                 let newLayer
                 if (['feature', 'geojson'].includes(type)) {
                     newLayer = await getLeafletGeoJSONLayer({
                         geojson: layerGeoJSON,
+                        title: layer._title,
+                        attribution,
                         pane,
                         group: targetGroup,
                         fetcher: layer._fetcher,
-                        styles: isLegendGroup ? structuredClone(styles) : null,
-                        title,
-                        attribution,
+                        styles: isLegendGroup ? structuredClone(styles) : null
                     })
                 }
                 if (newLayer) targetGroup.addLayer(newLayer)
