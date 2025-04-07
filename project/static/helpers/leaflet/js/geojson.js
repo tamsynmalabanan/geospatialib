24095a8fe9219c16039bc60d697ed1a2d183ad6c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-07 21:51:05 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/geojson.js
+++ project/static/helpers/leaflet/js/geojson.js
@@ -279,7 +279,7 @@ const createGeoJSONLayerLegend = (layer, parent) => {
             
             const typeIcon = document.createElement('div')
             typeIcon.className = 'd-flex align-items-center'
-            typeIcon.style.height = '10px'
+            // typeIcon.style.height = '10px'
             typeIcon.style.width = type === 'point' ? '10px' : '16px'
             typeIcon.innerHTML = style.types[type].html
             titleToTooltip(typeIcon, `${formatNumberWithCommas(typeCount)} ${type}${typeCount > 1 ? 's' : ''}`)
