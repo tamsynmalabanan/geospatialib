63086b9752a52beb2802e5950f94875cdc138514
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-07 15:01:36 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -661,7 +661,7 @@ const handleLeafletStylePanel = (map, parent) => {
                         parent:design,
                         fieldAttrs: {
                             type: 'color',
-                            value: hslToHex(...manageHSLAColor(styleParams.fillColor)),
+                            value: hslToHex(manageHSLAColor(styleParams.fillColor)),
                         },
                         labelText: 'Fill color'
                     }).querySelector('input')
