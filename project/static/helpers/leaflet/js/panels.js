c90d82f2a64041a8a540faf0de878bdb285a02bb
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-10 11:42:09 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -1376,7 +1376,7 @@ const handleLeafletStylePanel = (map, parent) => {
                             typeFilter: {
                                 handler: createCheckboxOptions,
                                 name: 'typeFilter',
-                                containerClass: 'p-2 border rounded flex-wrap flex-grow-1 w-100 gap-3',
+                                containerClass: 'p-2 border rounded flex-wrap flex-grow-1 w-100 gap-2',
                                 options: (() => {
                                     const options = {}
                                     for (const type in filters.type.values) {
