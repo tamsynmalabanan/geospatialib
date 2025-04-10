156d81c257655126054c7c4e33e0f4defffec3fe
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-10 11:22:05 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -1036,6 +1036,7 @@ const handleLeafletStylePanel = (map, parent) => {
                                 e.target.value = ''
                             }
                             
+                            console.log(value, filter.geometry)
                             console.log(new Set(value, filter.geometry))
                             if (!value && !filter.geometry) return
                             if (value && filter.geometry && turf.booleanEqual(value, filter.geometry)) return
