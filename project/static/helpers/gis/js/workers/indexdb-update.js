bea6aafcd33f5a37a27097bd5ecea8464d988a49
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-11 10:35:11 +0530

    updated base.html template

--- project/static/helpers/gis/js/workers/indexdb-update.js
+++ project/static/helpers/gis/js/workers/indexdb-update.js
@@ -26,8 +26,8 @@ self.onmessage = (e) => {
             newGeoJSON.features = newGeoJSON.features.concat(filteredFeatures)
             console.log(
                 'union', 
-                newGeoJSON._queryExtent,
-                currentGeoJSON._queryExtent,
+                newGeoJSON._queryExtent.coordinates,
+                currentGeoJSON._queryExtent.coordinates,
             )
             // newGeoJSON._queryExtent = turf.union(turf.featureCollection([
             //     {type:'Feature',geometry:newGeoJSON._queryExtent},
