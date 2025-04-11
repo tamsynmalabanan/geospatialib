f227d5441c0ecf5a52fa01193069974bfbacff6b
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-11 13:28:41 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/geojson.js
+++ project/static/helpers/leaflet/js/geojson.js
@@ -29,7 +29,7 @@ const getLeafletGeoJSONLayer = async ({
         ), 1)
 
         fetcher = fetchStaticGeoJSON = async ({map, controller}={}) => {
-            return await fetchGeoJSONInMap(geojson, dbKey, {map, controller})
+            return await fetchGeoJSONInMap(geojson, dbKey, {controller})
         }
     }
     geojsonLayer._fetcher = fetcher
