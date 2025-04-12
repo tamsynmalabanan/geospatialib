711d7b5a08b7aa99c4ce79e59d9bc22a25e61f26
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-12 11:51:28 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -1215,7 +1215,7 @@ const handleLeafletStylePanel = (map, parent) => {
             inputTag: 'textarea',
             delimiters: null,
             enabled: 0,
-            dropdownClass:  `bs-tagify-dropdown rounded mt-1 border`,
+            dropdownClass:  `rounded mt-1 border`,
             name:  `propFilter-values-${id}`,
             placeholder: 'Select property value',
             currentValue: JSON.stringify(filter.values.map(i => {return {value:i}})),
