b89d5ee1d5c6be087ce6be07f4c9f381af51f08a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-19 13:12:30 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -636,7 +636,9 @@ const handleLeafletStylePanel = (map, parent) => {
                     const text = document.createElementNS(svgNS, 'text')
                     text.setAttribute('x', contaienrSize)
                     text.setAttribute('y', contaienrSize)
-                    text.setAttribute('font-size', iconSize)
+                    // text.setAttribute('font-size', iconSize)
+                    text.setAttribute('lengthAdjust', 'spacingAndGlyphs')
+                    text.setAttribute('textlength', iconSize)
                     
                     if (styleParams.patternFill) {
                         text.setAttribute('fill', styleParams.fillColor)
