ead39ec60f43a661d3b29746e956d60cfc58faef
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-21 17:55:45 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -756,7 +756,7 @@ const handleLeafletStylePanel = (map, parent) => {
                             data:image/svg+xml,
                             %3Csvg 
                                 xmlns='http://www.w3.org/2000/svg' 
-                                width=${svg.getAttribute('width')} 
+                                width='200' 
                                 height='100'
                             %3E
                                 %3Ctext 
