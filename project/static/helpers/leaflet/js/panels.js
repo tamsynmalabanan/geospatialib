fc96c330bc46b8a92eaa6f153b129b914a7df08c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-22 16:35:39 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -686,10 +686,8 @@ const handleLeafletStylePanel = (map, parent) => {
                 if (iconType === 'svg') {
                     defs.innerHTML = iconSpecs
                     icon = defs.firstChild
-                    icon.setAttribute('x', '50%')
-                    icon.setAttribute('y', '50%')
-                    // icon.setAttribute('text-anchor', 'middle')
-                    // icon.setAttribute('dominant-baseline', 'central')
+                    icon.setAttribute('x', '25%')
+                    icon.setAttribute('y', '25%')
                 } else {
                     icon = document.createElementNS(svgNS, 'text')
                     icon.innerHTML = iconType === 'bi' ? `&#x${bootstrapIcons[iconSpecs] ?? 'F287'};` : iconSpecs ?? ''
