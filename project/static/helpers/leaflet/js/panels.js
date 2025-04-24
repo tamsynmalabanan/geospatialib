d99b5960c5296995a3a0c774812b4e697bfbbb60
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-24 18:55:52 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -781,23 +781,23 @@ const handleLeafletStylePanel = (map, parent) => {
                     icon.style.textShadow = textShadow
                     if (Array('emoji', 'img', 'html').includes(iconType)) {
                         icon.style.opacity = fillOpacity
+                    } else {
+                        icon.setAttribute('fill', (() => {
+                            if (iconFill) icon.setAttribute('fill-opacity', fillOpacity)
+                            return iconFill ? fillColor : 'none'
+                        })())
+                        icon.setAttribute('stroke', (() => {
+                            if (iconStroke) {
+                                icon.setAttribute('stroke-opacity', strokeOpacity)
+                                icon.setAttribute('stroke-width', strokeWidth)
+                                icon.setAttribute('stroke-linecap', lineCap)
+                                icon.setAttribute('stroke-linejoin', lineJoin)
+                                icon.setAttribute('stroke-dasharray', dashArray)
+                                icon.setAttribute('stroke-dashoffset', dashOffset)
+                            }
+                            return iconStroke ? strokeColor : 'none'
+                        })())
                     }
-
-                    icon.setAttribute('fill', (() => {
-                        if (iconFill) icon.setAttribute('fill-opacity', fillOpacity)
-                        return iconFill ? fillColor : 'none'
-                    })())
-                    icon.setAttribute('stroke', (() => {
-                        if (iconStroke) {
-                            icon.setAttribute('stroke-opacity', strokeOpacity)
-                            icon.setAttribute('stroke-width', strokeWidth)
-                            icon.setAttribute('stroke-linecap', lineCap)
-                            icon.setAttribute('stroke-linejoin', lineJoin)
-                            icon.setAttribute('stroke-dasharray', dashArray)
-                            icon.setAttribute('stroke-dashoffset', dashOffset)
-                        }
-                        return iconStroke ? strokeColor : 'none'
-                    })())
 
                     const svg = document.createElementNS(svgNS, 'svg')
                     svg.id = `${id}-svg`
