586b5da1e09205bb41b53d764e6db4938f0ee333
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-24 13:16:50 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -745,7 +745,7 @@ const handleLeafletStylePanel = (map, parent) => {
                 
                 if (Array('bi', 'text').includes(iconType)) {
                     icon = document.createElementNS(svgNS, 'text')
-                    icon.innerHTML = iconType === 'bi' ? `&#x${bootstrapIcons[iconSpecs] ?? 'F287'};` : iconSpecs ?? ''
+                    icon.innerText = iconType === 'bi' ? `&#x${bootstrapIcons[iconSpecs] ?? 'F287'};` : iconSpecs ?? ''
                     icon.setAttribute('class', removeWhitespace(`
                         text-center lh-1
                         ${textWrap ? 'text-wrap' : 'text-nowrap'}
@@ -757,7 +757,6 @@ const handleLeafletStylePanel = (map, parent) => {
                     icon.setAttribute('text-anchor', 'middle')
                     icon.setAttribute('dominant-baseline', 'central')
                     icon.setAttribute('font-size', iconSize)
-                    icon.setAttribute('opacity', fillOpacity)
                     icon.setAttribute('font-family', (
                         iconType === 'bi' ? 'bootstrap-icons' :
                         fontSerif ? 'Georgia, Times, serif' :
