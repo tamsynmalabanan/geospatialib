5b35b7558958ff13d319607ec33d11457dd39d2b
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-25 13:34:57 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -767,8 +767,6 @@ const handleLeafletStylePanel = (map, parent) => {
                     y:0-(buffer/2),
                 })
 
-                console.log(dataUrl)
-
                 if (iconType === 'html' && dataUrl) {
                     icon = document.createElementNS(svgNS, 'image')
                     icon.setAttribute('href', dataUrl)
@@ -785,7 +783,6 @@ const handleLeafletStylePanel = (map, parent) => {
                     }
                 ))
 
-                console.log(img)
                 defs.appendChild(img)
 
                 if (icon) {
