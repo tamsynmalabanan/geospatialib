05c46a74718af62b3c759ea0a704e3499a75656d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-26 09:27:14 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/layers.js
+++ project/static/helpers/leaflet/js/layers.js
@@ -482,7 +482,7 @@ const getLeafletLayerContextMenu = async (e, layer, {
                 addLayer(layer)
             }
         },
-        visibility: isLegendFeature || checkbox ? null : {
+        visibility: feature || checkbox ? null : {
             innerText: `Toggle visibility`,
             btnCallback: () => {
                 group._ch.hasHiddenLayer(layer) 
