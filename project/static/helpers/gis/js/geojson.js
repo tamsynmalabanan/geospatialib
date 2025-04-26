c363702e52098d418b81c0ac6be8475c714f96b8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-27 00:39:10 +0530

    updated base.html template

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -20,30 +20,42 @@ const handleGeoJSON = async (geojson, {
     }
 }
 
-const sortGeoJSONFeaturesByType = (geojson, { reverse = false }) => {
+const sortGeoJSONFeaturesByType = (geojson, {
+    reverse=false,
+}) => {
     geojson.features.sort((a, b) => {
         const featureTypeA = a.geometry.type;
         const featureTypeB = b.geometry.type;
-
-        const featureOrder = [
-            "Point",
-            "MultiPoint",
-            "LineString",
-            "MultiLineString",
-            "Polygon",
-            "MultiPolygon",
-        ];
-
-        const orderA = featureOrder.indexOf(featureTypeA);
-        const orderB = featureOrder.indexOf(featureTypeB);
-
-        // Handle regular sorting
-        const comparison = orderA - orderB;
-
-        // Reverse sorting logic
-        return reverse ? -comparison : comparison;
+    
+        if (featureTypeA === 'Point' && featureTypeB !== 'Point') {
+            return -1;
+        } else if (featureTypeB === 'Point' && featureTypeA !== 'Point') {
+            return 1;
+        } else if (featureTypeA === 'MultiPoint' && featureTypeB !== 'MultiPoint') {
+            return -1;
+        } else if (featureTypeB === 'MultiPoint' && featureTypeA !== 'MultiPoint') {
+            return 1;
+        } else if (featureTypeA === 'LineString' && featureTypeB !== 'LineString') {
+            return -1;
+        } else if (featureTypeB === 'LineString' && featureTypeA !== 'LineString') {
+            return 1;
+        } else if (featureTypeA === 'MultiLineString' && featureTypeB !== 'MultiLineString') {
+            return -1;
+        } else if (featureTypeB === 'MultiLineString' && featureTypeA !== 'MultiLineString') {
+            return 1;
+        } else if (featureTypeA === 'Polygon' && featureTypeB !== 'Polygon') {
+            return -1;
+        } else if (featureTypeB === 'Polygon' && featureTypeA !== 'Polygon') {
+            return 1;
+        } else if (featureTypeA === 'MultiPolygon' && featureTypeB !== 'MultiPolygon') {
+            return -1;
+        } else if (featureTypeB === 'MultiPolygon' && featureTypeA !== 'MultiPolygon') {
+            return 1;
+        } else {
+            return featureTypeA.localeCompare(featureTypeB);
+        }
     });
-};
+}
 
 const transformGeoJSONCoordinates = async (coordinates, source, target) => {
     const source_text = `EPSG:${source}`
