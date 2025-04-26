98a358e9c172704fd76ca531722c4af1a439266d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-27 00:43:40 +0530

    updated base.html template

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -20,42 +20,27 @@ const handleGeoJSON = async (geojson, {
     }
 }
 
-const sortGeoJSONFeaturesByType = (geojson, {
-    reverse=false,
-}) => {
+const sortGeoJSONFeaturesByType = (geojson, { reverse = false }) => {
     geojson.features.sort((a, b) => {
         const featureTypeA = a.geometry.type;
         const featureTypeB = b.geometry.type;
-    
-        if (featureTypeA === 'Point' && featureTypeB !== 'Point') {
-            return -1;
-        } else if (featureTypeB === 'Point' && featureTypeA !== 'Point') {
-            return 1;
-        } else if (featureTypeA === 'MultiPoint' && featureTypeB !== 'MultiPoint') {
-            return -1;
-        } else if (featureTypeB === 'MultiPoint' && featureTypeA !== 'MultiPoint') {
-            return 1;
-        } else if (featureTypeA === 'LineString' && featureTypeB !== 'LineString') {
-            return -1;
-        } else if (featureTypeB === 'LineString' && featureTypeA !== 'LineString') {
-            return 1;
-        } else if (featureTypeA === 'MultiLineString' && featureTypeB !== 'MultiLineString') {
-            return -1;
-        } else if (featureTypeB === 'MultiLineString' && featureTypeA !== 'MultiLineString') {
-            return 1;
-        } else if (featureTypeA === 'Polygon' && featureTypeB !== 'Polygon') {
-            return -1;
-        } else if (featureTypeB === 'Polygon' && featureTypeA !== 'Polygon') {
-            return 1;
-        } else if (featureTypeA === 'MultiPolygon' && featureTypeB !== 'MultiPolygon') {
-            return -1;
-        } else if (featureTypeB === 'MultiPolygon' && featureTypeA !== 'MultiPolygon') {
-            return 1;
-        } else {
-            return featureTypeA.localeCompare(featureTypeB);
-        }
+
+        const featureOrder = [
+            "Point",
+            "MultiPoint",
+            "LineString",
+            "MultiLineString",
+            "Polygon",
+            "MultiPolygon",
+        ];
+
+        const orderA = featureOrder.indexOf(featureTypeA);
+        const orderB = featureOrder.indexOf(featureTypeB);
+
+        const comparison = orderA - orderB;
+        return reverse ? -comparison : comparison;
     });
-}
+};
 
 const transformGeoJSONCoordinates = async (coordinates, source, target) => {
     const source_text = `EPSG:${source}`
@@ -417,6 +402,9 @@ const fetchGeoJSON = async ({
                 const cachedGeoJSON = cachedData.geojson
                 const cachedQueryExtent = cachedData.queryExtent
                 
+                L.geoJSON(queryExtent).addTo(map)
+                // L.geoJSON(cachedQueryExtent).addTo(map)
+
                 try {
                     const equalBounds = turf.booleanEqual(queryExtent, cachedQueryExtent)
                     const withinBounds = turf.booleanWithin(queryExtent, cachedQueryExtent)
