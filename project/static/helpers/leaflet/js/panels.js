02b0bc64cd12f77672609ab5934d72044c4741a2
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-04-26 14:33:23 +0530

    updated base.html template

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -868,7 +868,7 @@ const handleLeafletStylePanel = (map, parent) => {
         const styleParams = style.styleParams
         
         const parent = customCreateElement({
-            className:'d-flex gap-2 flex-column ps-2 border-start border-3 flex-grow-1',
+            className:'d-flex gap-2 flex-column ps-2 flex-grow-1',
         })
 
         const collapseId = generateRandomString()
@@ -953,7 +953,7 @@ const handleLeafletStylePanel = (map, parent) => {
 
         const collapseDiv = customCreateElement({
             id: collapseId,
-            className:'collapse show',
+            className:'collapse show border-start border-3',
             parent,
         })
 
