3c61d2817b0906a2e4a454583a57f81b5a701783
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-04 15:22:05 +0530

    refactor addLeafletGeoJSONData function for improved readability and efficiency

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -93,6 +93,7 @@ const createGeoJSONChecklist = async (layerList, group, {
     for (const geojsonLayer of layerList) {
         if (controller?.signal.aborted) return
 
+        console.log(geojsonLayer)
         const featureLayers = geojsonLayer.getLayers()
         if (!featureLayers.length) continue
 
