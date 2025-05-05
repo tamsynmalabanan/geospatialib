6437f12eaefe84b52943274987f7eb5ddd8c34d6
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-05 18:38:19 +0530

    refactor sortGeoJSONFeatures for improved readability and comparison logic

--- project/static/helpers/leaflet/js/geojson.js
+++ project/static/helpers/leaflet/js/geojson.js
@@ -346,7 +346,7 @@ const getLeafletGeoJSONData = async (layer, {
 }
 
 const updateLeafletGeoJSONLayer = async (layer, {controller, abortBtns} = {}) => {
-    await getLeafletGeoJSONData(layer, {controller, abortBtns})
+    const data = await getLeafletGeoJSONData(layer, {controller, abortBtns})
 
     if (controller?.signal?.aborted) return
     const renderer = (data.features.length ?? 0) > 1000 ? L.Canvas : L.SVG
