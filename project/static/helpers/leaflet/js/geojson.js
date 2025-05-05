bf5d1c7076d512d9e74023fcb2ed599c01bddb23
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-05 21:29:17 +0530

    refactor sortGeoJSONFeatures for improved readability and comparison logic

--- project/static/helpers/leaflet/js/geojson.js
+++ project/static/helpers/leaflet/js/geojson.js
@@ -336,6 +336,7 @@ const getLeafletGeoJSONData = async (layer, {
         if (controller?.signal?.aborted) return
         const scale = getLeafletMeterScale(map)
         const tolerance = scale > 1000 ?  scale/5000000 : 0
+        console.log(map.getZoom(), scale, tolerance)
         turf.simplify(data, {
             mutate: true,
             tolerance, 
