04367e6abde07a3abe9ad018e1eac3d43fba547a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-08 19:56:30 +0530

    refactor sortGeoJSONFeatures for improved readability and comparison logic

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -405,6 +405,7 @@ const handleLeafletLegendPanel = (map, parent) => {
                                 
                                 const format = data?.format
                                 formatField.classList.toggle('is-invalid', format === false)
+                                console.log(url, format, formatField.value)
                                 formatField.value = url ? format || format === false ? formatField.value : '' : ''
                                 namesField.DOM.scope.setAttribute('disabled', !format)
                                 
