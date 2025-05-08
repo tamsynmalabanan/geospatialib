d7300872d87d85eeefbfde1db14cf35360aa7615
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-08 11:24:00 +0530

    refactor sortGeoJSONFeatures for improved readability and comparison logic

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -297,7 +297,7 @@ const handleLeafletLegendPanel = (map, parent) => {
                                     fileInput.classList.toggle('d-none', checked)
                                     
                                     urlContainer.classList.toggle('d-none', !checked)
-                                    console.log(Tagify(form.elements['newLayerNames']))
+                                    console.log(Tagify(parent.elements['newLayerNames']))
                                 }
                             }
                         },
