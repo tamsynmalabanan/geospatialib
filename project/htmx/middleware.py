1351ba96b2cb3e3fe1f5e97bd63fd1e057536415
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-09 11:32:59 +0530

    refactor sortGeoJSONFeatures for improved readability and comparison logic

--- project/htmx/middleware.py
+++ project/htmx/middleware.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.urls import resolve
 from django.conf import settings
-from django.http import HttpResponseForbidden
+# from django.http import HttpResponseForbidden
 
 class HTMXDomainRestriction:
     def __init__(self, get_response):
@@ -13,7 +13,9 @@ class HTMXDomainRestriction:
             not_htmx = not request.htmx
             not_allowed_host = request.META.get('HTTP_HOST') not in settings.ALLOWED_HOSTS
             if not_htmx or not_allowed_host:
-                return HttpResponseForbidden('You do not have permission to access this resource.')
+                return redirect('main:index')
+                # return HttpResponseForbidden('You do not have permission to access this resource.')
+
 
         response = self.get_response(request)
         return response
