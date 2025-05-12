2264954a2a7e665e7369ab8ee604597335742bde
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-12 18:16:54 +0530

    refactor sortGeoJSONFeatures for improved readability and comparison logic

--- project/static/helpers/gis/js/proj4.js
+++ project/static/helpers/gis/js/proj4.js
@@ -9,18 +9,16 @@ const fetchProj4Def = async (crs, {
         const def = await fetchTimeout(url, {
             abortBtns,
             controller,
+            callback: (response) => {
+                const def = response.text()
+                const crs_text = `EPSG:${crs}`
+                proj4.defs(crs_text, def)
+                return proj4.defs(crs_text)
+            },
             fetchParams: {headers: {'HX-Request': 'true'}}
-        }).then(response => {
-            if (!response.ok && (response.status < 200 || response.status > 300)) {
-                throw new Error('Response not ok.')
-            }
-    
-            return response.text()
-        }).then(def => {
-            const crs_text = `EPSG:${crs}`
-            proj4.defs(crs_text, def)
-            return proj4.defs(crs_text)
-        }).catch(error => console.error(error))
+        }).catch(error => {
+            console.error(error)
+        })
 
         if (def) return def
     }
