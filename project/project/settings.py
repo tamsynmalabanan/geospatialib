0ce54f2f4a85301dbd98225892cd6eb9b15c1514
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-13 18:20:27 +0530

    add popup configuration options to handleLeafletStylePanel

--- project/project/settings.py
+++ project/project/settings.py
@@ -120,6 +120,7 @@ LEAFLET_CONFIG = {
 }
 
 MIDDLEWARE = [
+    'csp.middleware.CSPMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -218,8 +219,18 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 60*60*24*365
-# SECURE_HSTS_SECONDS = 86400
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+
+# Content Security Policy
+
+CSP_IMG_SRC = ("'self'")
+
+CSP_STYLE_SRC = ("'self'")
+
+CSP_SCRIPT_SRC = ("'self'")
+
+CSP_INCLUDE_NONCE_IN = ['script-src']
