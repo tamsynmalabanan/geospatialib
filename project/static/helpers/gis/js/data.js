369d179f54f76e00432a8f31ef7f4d9babc4fe60
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-14 21:44:18 +0530

    add popup configuration options to handleLeafletStylePanel

--- project/static/helpers/gis/js/data.js
+++ project/static/helpers/gis/js/data.js
@@ -18,13 +18,10 @@ const fetchFileData = async (url, name, {abortBtns, controller} = {}) => {
                 new File([content],
                     url.split('/')[url.split('/').length-1])
             ])
-            console.log(name, filesArray)
             const file = filesArray.find(file => file.name === name)
-            console.log(file)
             if (!file) throw new Error('Filename not found.')
                 
             const data = await getFileData(file)
-            console.log(data)
             return data
         },
     }).catch(error => {
