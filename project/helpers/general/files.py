6c0b74913231b88808e6f06f1744d17902f87960
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-15 09:50:42 +0530

    add popup configuration options to handleLeafletStylePanel

--- project/helpers/general/files.py
+++ project/helpers/general/files.py
@@ -8,19 +8,22 @@ from io import BytesIO
 
 def extract_zip(zip_file, base_path=""):
     files = {}
+    
+    zip_filename = os.path.basename(zip_file)
+    prefix = os.path.join(base_path, zip_filename)
     with zipfile.ZipFile(zip_file, 'r') as zf:
         for file in zf.namelist():
             if file.endswith('/'):
                 continue
 
-            full_path = os.path.join(base_path, file)
+            full_path = os.path.join(prefix, file)
             if file.endswith('.zip'):
                 with zf.open(file) as sub_zip:
                     files.update(extract_zip(BytesIO(sub_zip.read()), full_path))
             else:
                 files[full_path] = file.split('.')[0]
+    
     return files
-
 
 def get_file_info(url):
     response = requests.get(url)
--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -378,6 +378,7 @@ const handleLeafletLegendPanel = (map, parent) => {
                     } catch (error) {
                         data = {url:false}
                     } finally {
+                        console.log(data)
                         const {url, format, names} = data
 
                         urlField.classList.toggle('is-invalid', url === false && urlField.value !== '')
