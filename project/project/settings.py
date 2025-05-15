d9f042a3ad3f821d0660c64299f43fbc3cb040c5
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-15 17:31:58 +0530

    add popup configuration options to handleLeafletStylePanel

--- project/project/settings.py
+++ project/project/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
 # celery task
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 # BROKER_URL = 'redis://127.0.0.1:6379'
 # CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
 # CELERY_ACCEPT_CONTENT = ['application/json']
