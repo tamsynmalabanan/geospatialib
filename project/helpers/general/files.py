62b57d8dac8a9e0518f7d445f9ed04a5294e9839
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-15 10:11:07 +0530

    add popup configuration options to handleLeafletStylePanel

--- project/helpers/general/files.py
+++ project/helpers/general/files.py
@@ -9,14 +9,12 @@ from io import BytesIO
 def extract_zip(zip_file, base_path=""):
     files = {}
     
-    zip_filename = os.path.basename(zip_file)
-    prefix = os.path.join(base_path, zip_filename)
     with zipfile.ZipFile(zip_file, 'r') as zf:
         for file in zf.namelist():
             if file.endswith('/'):
                 continue
 
-            full_path = os.path.join(prefix, file)
+            full_path = os.path.join(base_path, file)
             if file.endswith('.zip'):
                 with zf.open(file) as sub_zip:
                     files.update(extract_zip(BytesIO(sub_zip.read()), full_path))
