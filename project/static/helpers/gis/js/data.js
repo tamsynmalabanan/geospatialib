cca3f8f4f34e183805599d8a062c5fb20b61453d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-15 11:52:10 +0530

    add popup configuration options to handleLeafletStylePanel

--- project/static/helpers/gis/js/data.js
+++ project/static/helpers/gis/js/data.js
@@ -8,23 +8,39 @@ const fetchGeoJSON = async (url, {abortBtns, controller} = {}) => {
     })
 }
 
+const mapForFetchFileData = new Map()
 const fetchFileData = async (url, name, {abortBtns, controller} = {}) => {
-    return await fetchTimeout(url, {
+    const handler = async (filesArray) => {
+        const file = filesArray.find(file => file.name === name)
+        if (!file) throw new Error('Filename not found.')
+            
+        const data = await getFileData(file)
+        return data
+    }
+
+    const mapKey = `${url};${controller?.id}` 
+    if (mapForFetchFileData.has(mapKey)) {
+        console.log('here')
+        return handler(await mapForFetchFileData.get(mapKey))
+    }
+
+    const filesArrayPromise = fetchTimeout(url, {
         abortBtns,
         controller,
         callback: async (response) => {
             const content = await response.blob()
             const filesArray = await getValidFilesArray([
                 new File([content],
-                    url.split('/')[url.split('/').length-1])
+                url.split('/')[url.split('/').length-1])
             ])
-            const file = filesArray.find(file => file.name === name)
-            if (!file) throw new Error('Filename not found.')
-                
-            const data = await getFileData(file)
-            return data
+            return filesArray
         },
     }).catch(error => {
         console.log(error.message, url, name)
+    }).finally(() => {
+        setTimeout(() => mapForFetchFileData.delete(mapKey), 1000)
     })
+
+    mapForFetchFileData.set(mapKey, filesArrayPromise)
+    return handler(await filesArrayPromise)
 }
