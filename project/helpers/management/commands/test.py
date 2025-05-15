6e8389f58010f37eba47c4909e859db6ecd85b16
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-15 16:17:48 +0530

    add popup configuration options to handleLeafletStylePanel

--- project/helpers/management/commands/test.py
+++ project/helpers/management/commands/test.py
@@ -3,10 +3,11 @@ from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Q
 
 from helpers.general.files import get_file_info
+from helpers.gis.layers import get_collection
 
 class Command(BaseCommand):
     help = 'Test'
 
     def handle(self, *args, **kwargs):
-        get_file_info('https://github.com/tamsynmalabanan/gis-data/raw/refs/heads/main/zip.zip')
+        get_collection('https://github.com/tamsynmalabanan/gis-data/raw/refs/heads/main/zip.zip')
         self.stdout.write(self.style.SUCCESS('Test'))
