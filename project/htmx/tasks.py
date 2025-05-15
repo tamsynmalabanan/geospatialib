f968ad2830b72fbaa60bfb8ce91b9360366ca35f
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-15 18:40:35 +0530

    add popup configuration options to handleLeafletStylePanel

--- project/htmx/tasks.py
+++ project/htmx/tasks.py
@@ -1,6 +1,11 @@
 from celery import shared_task
 
-@shared_task
+@shared_task(
+    bind=True, 
+    autoretry_for=(Exception,), 
+    retry_backoff=0.5, 
+    retry_kwargs={'max_retries':5}
+)
 def test_task(value):
     print(value)
     return {
