80276806ec25ebf1ab70da08bd7f8a032ca1ca3a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-17 11:06:06 +0530

    add layers form modal

--- project/static/helpers/gis/js/add_layers.js
+++ project/static/helpers/gis/js/add_layers.js
@@ -53,7 +53,7 @@ const handleAddLayersForm = () => {
         clearTimeout(toggleSubmitBtnTimeout)
         toggleSubmitBtnTimeout = setTimeout(() => {
             const layerNames = form.querySelector(`#addLayersForm-layerNames`)
-            console.log(layerNames)
+            console.log(layerNames, layerNames.innerHTML === '')
             submitBtn.disabled = isFileSource() ? !fileInput.files.length : layerNames.innerHTML === ''
         }, 100);
     }
