7831dd9087571ca2262515fd01bab826224fa1f0
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-17 11:51:52 +0530

    add layers form modal

--- project/static/helpers/gis/js/add_layers.js
+++ project/static/helpers/gis/js/add_layers.js
@@ -124,3 +124,19 @@ const handleAddLayersForm = () => {
 document.addEventListener('DOMContentLoaded', () => {
     handleAddLayersForm()
 })
+
+document.body.addEventListener('htmx:afterRequest', function (evt) {
+    const targetError = evt.target.attributes.getNamedItem('hx-target-error')
+    // if (evt.detail.failed && targetError) {
+    //   document.getElementById(targetError.value).style.display = "inline";
+    // }
+    console.log(targetError)
+});
+
+document.body.addEventListener('htmx:beforeRequest', function (evt) {
+    const targetError = evt.target.attributes.getNamedItem('hx-target-error')
+    // if (targetError) {
+    //     document.getElementById(targetError.value).style.display = "none";
+    // }
+    console.log(targetError)
+});
