97d6b7128b2b5b3a292fce36d0575e900d8e8a31
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-19 13:48:58 +0530

    add layers form modal

--- project/htmx/views.py
+++ project/htmx/views.py
@@ -25,7 +25,10 @@ def validate_collection(request):
 
 @require_http_methods(['GET'])
 def get_file_forms(request):
-    return render(request, 'helpers/partials/add_layers/file_forms.html')
+    
+    return render(request, 'helpers/partials/add_layers/file_forms.html', {
+        'request_vars': vars(request)
+    })
 
 @require_http_methods(['POST', 'GET'])
 def cors_proxy(request):
