675cdf98b4d14319bdc4e8c0a70c6770167860dc
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-19 21:24:18 +0530

    add layers form modal

--- project/htmx/views.py
+++ project/htmx/views.py
@@ -11,19 +11,24 @@ from main.forms import ValidateCollectionForm
 
 @require_http_methods(['POST'])
 def validate_collection(request):
+    temp = None
+
     data = request.POST.dict()
     raw_format = data.get('format')
-    layers = {}
     form = ValidateCollectionForm(data)
     if form.is_valid():
         layers = get_collection_layers(form.cleaned_data)
+        temp = form.cleaned_data
         if layers == {}:
             form.data.update({'format':raw_format})
             if raw_format:
                 form.add_error('format', 'No layers retrieved.')
+    else:
+        layers = {}
     return render(request, 'helpers/partials/add_layers/url_fields.html', {
         'form': form,
         'layers': layers,
+        'temp': temp,
     })
 
 @require_http_methods(['GET'])
