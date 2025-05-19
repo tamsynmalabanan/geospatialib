5734bfc0b59d622bc50e1bf566543def96539730
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-19 17:19:20 +0530

    add layers form modal

--- project/static/helpers/gis/js/add_layers.js
+++ project/static/helpers/gis/js/add_layers.js
@@ -59,8 +59,7 @@ const handleAddLayersForm = () => {
 
             const inputGroup = i.closest('.input-group')
             const params = {title: inputGroup.querySelector('input[name="title"]')?.value ?? i.value}
-            console.log(inputGroup, inputGroup.children)
-            Array.from(inputGroup.lastElementChild.children ?? []).forEach(i => {
+            Array.from(inputGroup.lastElementChild.children).forEach(i => {
                 const name = i.getAttribute('name')
                 const value = i.value
                 console.log(name, value)
