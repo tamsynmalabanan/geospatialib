443032cdcd81b859a24b126648b6dd4ad4cd742f
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-19 21:41:45 +0530

    add layers form modal

--- project/helpers/management/commands/test.py
+++ project/helpers/management/commands/test.py
@@ -10,4 +10,6 @@ class Command(BaseCommand):
     help = 'Test'
 
     def handle(self, *args, **kwargs):
+        value = get_collection_layers('https://raw.githubusercontent.com/tamsynmalabanan/gis-data/refs/heads/main/OpenStreetMap%20via%20Overpass%20(51).geojson')
+        print(value)
         self.stdout.write(self.style.SUCCESS('Test'))
