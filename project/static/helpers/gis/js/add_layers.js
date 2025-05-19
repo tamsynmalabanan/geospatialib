b4ce65f773e74ef44d8bf006af07210359577ba6
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-19 13:34:22 +0530

    add layers form modal

--- project/static/helpers/gis/js/add_layers.js
+++ project/static/helpers/gis/js/add_layers.js
@@ -101,15 +101,15 @@ const handleAddLayersForm = () => {
         })
     })
     
-    fileInput.addEventListener('change', async (e) => {
+    fileInput.addEventListener('htmx:configRequest', async (e) => {
         if (fileInput.files.length) {
             const filenames = (await getValidFilesArray(fileInput.files)).map(i => i.name).join(',')
-            console.log(filenames)
-            fileInput.setAttribute('hx-vals', `{"filenames": "${filenames}", "filenames": "TESTINGDUPLICATEFILEDNAME"}`)
+            // fileInput.setAttribute('hx-vals', `{"filenames": "${filenames}"}`)
+            e.detail.parameters['filenames'] = filenames
         }
 
-        const event = new Event("get-file-forms", { bubbles: true })
-        fileInput.dispatchEvent(event)
+        // const event = new Event("get-file-forms", { bubbles: true })
+        // fileInput.dispatchEvent(event)
     })
     
     form.addEventListener('htmx:beforeRequest', (e) => {
