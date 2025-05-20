e01c47e0499a0471f9284353fefa46e8e066e794
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-20 16:27:29 +0530

    add layers form modal

--- project/static/helpers/gis/js/add_layers.js
+++ project/static/helpers/gis/js/add_layers.js
@@ -101,7 +101,7 @@ const handleAddLayersForm = () => {
         } else {
             const url = form.elements.url.value
             const format = form.elements.format.value
-
+            console.log(includedLayers)
             for (const name in includedLayers) {
                 urlToLeafletLayer({
                     url,
