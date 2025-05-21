5cb832fc398db948c53f011faeb4104ba0dfa2dc
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-21 15:08:16 +0530

    add layers form modal

--- project/helpers/management/commands/test.py
+++ project/helpers/management/commands/test.py
@@ -3,7 +3,7 @@ from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Q
 
 from helpers.general.files import get_file_names
-from helpers.gis.layers import get_collection_layers, get_layers
+from helpers.gis.layers import get_collection_layers, get_layers, get_file_names
 from main.tasks import onboard_collection
 
 import requests
@@ -17,6 +17,6 @@ class Command(BaseCommand):
         # url = 'https://raw.githubusercontent.com/tamsynmalabanan/gis-data/refs/heads/main/OpenStreetMap%20via%20Overpass%20(51).geojson'
         # url = 'https://raw.githubusercontent.com/tamsynmalabanan/gis-data/refs/heads/main/centroid.csv'
         # url = 'https://nominatim.openstreetmap.org/reverse?lat=28.619166999999997&lon=77.4210995&zoom=18&format=geojson&polygon_geojson=1&polygon_threshold=0'
-        get_layers(url, 'geojson')
+        get_file_names(url)
         
         # self.stdout.write(self.style.SUCCESS(value))
