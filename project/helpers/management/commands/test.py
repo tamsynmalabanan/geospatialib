e81f1d35eb1ba5a78637fc9d25134ec66d5a1fed
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-21 15:27:02 +0530

    add layers form modal

--- project/helpers/management/commands/test.py
+++ project/helpers/management/commands/test.py
@@ -12,10 +12,10 @@ class Command(BaseCommand):
     help = 'Test'
 
     def handle(self, *args, **kwargs):
-        url = 'https://github.com/tamsynmalabanan/gis-data/raw/refs/heads/main/centroid.zip'
+        # url = 'https://github.com/tamsynmalabanan/gis-data/raw/refs/heads/main/centroid.zip'
         # url = 'https://github.com/tamsynmalabanan/gis-data/raw/refs/heads/main/zip.zip'
         # url = 'https://raw.githubusercontent.com/tamsynmalabanan/gis-data/refs/heads/main/OpenStreetMap%20via%20Overpass%20(51).geojson'
         # url = 'https://raw.githubusercontent.com/tamsynmalabanan/gis-data/refs/heads/main/centroid.csv'
-        # url = 'https://nominatim.openstreetmap.org/reverse?lat=28.619166999999997&lon=77.4210995&zoom=18&format=geojson&polygon_geojson=1&polygon_threshold=0'
-        value = get_file_names(url)
+        url = 'https://nominatim.openstreetmap.org/reverse?lat=28.619166999999997&lon=77.4210995&zoom=18&format=geojson&polygon_geojson=1&polygon_threshold=0'
+        value = get_layers(url, 'geojson')
         self.stdout.write(self.style.SUCCESS(value))
