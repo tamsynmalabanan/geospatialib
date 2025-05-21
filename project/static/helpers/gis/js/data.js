ba88cda0edddd4d4be52e42cbf618f43f313cab9
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-21 13:10:36 +0530

    add layers form modal

--- project/static/helpers/gis/js/data.js
+++ project/static/helpers/gis/js/data.js
@@ -74,7 +74,7 @@ const fetchFileData = async (url, name, type, xField, yField, {abortBtns, contro
                     new File([content],
                     url.split('/')[url.split('/').length-1])
                 ])
-                console.log(filesArray, type)
+                console.log(filesArray, name, type)
                 return filesArray
             } catch (error) {
                 console.log(error)
