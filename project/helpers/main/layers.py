52ac72a8fd14b6dd32d1f61809d477fbca9a28c3
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-23 21:21:15 +0530

    add layers form modal

--- project/helpers/main/layers.py
+++ project/helpers/main/layers.py
@@ -115,15 +115,16 @@ def validate_file(url, name, params):
             geojson_obj, params = csv_to_geojson(file, params)
 
         if name.endswith('.geojson'):
-            text = file.read().decode("utf-8")
-            geojson_obj = json.loads(text)
-            print(geojson_obj)
-            # features = []
-            # file.seek(0)
-            # for feature in stream_geojson(file):
-            #     features.append(feature)
-            # print(features)
-            # geojson_obj = {'features': features}
+            # text = file.read().decode("utf-8")
+            # geojson_obj = json.loads(text)
+            # print(geojson_obj)
+
+            features = []
+            file.seek(0)
+            for feature in stream_geojson(file):
+                features.append(feature)
+            print(features)
+            geojson_obj = {'features': features}
 
         if not geojson_obj:
             raise Exception('No valid geojson.')
