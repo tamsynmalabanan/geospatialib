57b114be6e8a25bc717ff367a19f19e1113fc59d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-24 22:09:28 +0530

    add layers form modal

--- project/helpers/main/layers.py
+++ project/helpers/main/layers.py
@@ -132,15 +132,13 @@ def validate_file(url, name, params):
             with MemoryFile(file) as memfile:
                 with memfile.open() as src:
                     w,s,e,n = src.bounds
-                    print(geojson.FeatureCollection([
-                        geojson.Polygon([[
-                            (w, s), 
-                            (e, s), 
-                            (e, n), 
-                            (w, n), 
-                            (w, s)
-                        ]])
-                    ]))
+                    print(geojson.FeatureCollection([geojson.Feature(geojson.Polygon([[
+                        (w, s), 
+                        (e, s), 
+                        (e, n), 
+                        (w, n), 
+                        (w, s)
+                    ]]))]))
 
         if not geojson_obj:
             raise Exception('No valid geojson.')
