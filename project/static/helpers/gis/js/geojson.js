cec85bae0427a8aa1760f8a721049d3278d3de5a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-25 18:01:28 +0530

    add layers form modal

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -70,6 +70,8 @@ const transformGeoJSONCoordinates = async (coordinates, source, target) => {
     const source_text = `EPSG:${source}`
     const target_text = `EPSG:${target}`
     
+    console.log(coordinates, source, target)
+    console.log(source_text, target_text)
     [source_text, target_text].forEach(async (crs) => {
         if (!proj4.defs(crs)) await fetchProj4Def(crs)
     })
