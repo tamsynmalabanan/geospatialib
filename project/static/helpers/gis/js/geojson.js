1c2e866fc170ee2f98adfafec7fe0aa3c2a783ef
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-25 18:52:49 +0530

    add layers form modal

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -75,6 +75,7 @@ const transformGeoJSONCoordinates = async (coordinates, source, target) => {
 
     if (proj4.defs(source_text) && proj4.defs(target_text)) {
         loopThroughCoordinates(coordinates, (coords) => {
+            console.log(coords)
             coords[0], coords[1] = proj4(source_text, target_text, coords)
         })
     }
