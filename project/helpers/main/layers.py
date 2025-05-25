71d4e1a2945700b84faec53c116d677fab0d49a2
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-25 19:39:35 +0530

    add layers form modal

--- project/helpers/main/layers.py
+++ project/helpers/main/layers.py
@@ -44,8 +44,8 @@ def csv_to_geojson(file, params):
     try:
         df = pd.read_csv(file)
 
-        xField = params['xField'] = params.get('xField', ([i for i in df.columns if any([j for j in LONGITUDE_ALIASES if j in i.strip().lower()])]+[None])[0])
-        yField = params['yField'] = params.get('yField', ([i for i in df.columns if any([j for j in LATITUDE_ALIASES if j in i.strip().lower()])]+[None])[0])
+        xField = params['xField'] = params.get('xField', ([i for i in df.columns if i.strip().lower() in LONGITUDE_ALIASES]+[None])[0])
+        yField = params['yField'] = params.get('yField', ([i for i in df.columns if i.strip().lower() in LATITUDE_ALIASES]+[None])[0])
         if not xField or not yField:
             raise Exception('No valid coordinate fields.')
         
