40d07aa8f625d4d9b105a3e1610ea19115e30ecb
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-26 14:52:04 +0530

    add layers form modal

--- project/static/helpers/leaflet/js/geojson.js
+++ project/static/helpers/leaflet/js/geojson.js
@@ -175,6 +175,7 @@ const getLeafletGeoJSONLayer = async ({
     }
 
     if (group?._name !== 'query') {
+        console.log(geojson)
         geojsonLayer._dbIndexedKey = dbIndexedKey || geojson ? await (async () => {
             await normalizeGeoJSON(geojson)
             return saveToGeoJSONDB(geojson)
