1d967d27f4627b7495889083ba1df50a6826a6ae
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-26 17:42:37 +0530

    add layers form modal

--- project/helpers/main/layers.py
+++ project/helpers/main/layers.py
@@ -65,11 +65,9 @@ def validate_geojson(url, name, params):
         if not geojson_obj.is_valid:
             raise Exception('Invalid geojson.')
         
-        print(int(geojson_obj.get('crs',{}).get('properties',{}).get('name','').split('EPSG::')[-1] or 4326))
-
         params.update({
             'bbox':get_geojson_bbox_polygon(geojson_obj),
-            # 'srid__id': 
+            'srid__id': int(geojson_obj.get('crs',{}).get('properties',{}).get('name','').split('EPSG::')[-1] or 4326)
         })
 
         return params
