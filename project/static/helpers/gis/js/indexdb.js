faf004f0abc4d08740864962ae5d5f8478a1082c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-26 10:00:11 +0530

    add layers form modal

--- project/static/helpers/gis/js/indexdb.js
+++ project/static/helpers/gis/js/indexdb.js
@@ -29,7 +29,6 @@ const saveToGeoJSONDB = (geojson, {
 
         const expirationTime = Date.now() + (expirationDays*1000*60*60*24)
 
-        console.log(db, geojson, queryExtent, expirationTime)
         objectStore.put({id, geojson, queryExtent, expirationTime})
     }
 
