2c815f5faf9484c2cf851bc5a462603f80dd10c8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-28 14:29:59 +0530

    add layers form modal

--- project/static/helpers/leaflet/js/geojson.js
+++ project/static/helpers/leaflet/js/geojson.js
@@ -469,7 +469,7 @@ const createGeoJSONLayerLegend = (layer, parent) => {
 
     if (!styles.reduce((acc, num) => acc + num[1].totalCount, 0)) {
         const tr = document.createElement('tr')
-        tr.innerHTML = '<i class="bi bi-circle m-1" title="No visible features"></i>'
+        tr.innerText = 'No visible features'
         tbody.appendChild(tr)
         return
     }
