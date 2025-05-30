9e2adfe37b3c866338e30a81d7893c8eafacd922
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-05-30 19:52:35 +0530

    add layers form modal

--- project/helpers/management/commands/test.py
+++ project/helpers/management/commands/test.py
@@ -27,7 +27,8 @@ def test_get_collection_data():
     # # url = 'https://github.com/tamsynmalabanan/gis-data/raw/refs/heads/main/cinemas.zip'
     # url = 'https://github.com/tamsynmalabanan/gis-data/raw/refs/heads/main/Special%20Protection%20and%20Conservation%20Areas%20GeoJson.zip'
     # url = 'https://github.com/tamsynmalabanan/gis-data/raw/refs/heads/main/Special%20Protection%20and%20Conservation%20Areas%20GeoJson.zip'
-    url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+    # url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+    url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
     
     value = get_collection_data(url, delay=False)
     print(value)
