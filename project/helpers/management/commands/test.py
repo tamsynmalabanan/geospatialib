6386eb9a0b8656a0429d489ce4cd9d0ebe4ac0df
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-03 18:32:40 +0530

    add layers form modal

--- project/helpers/management/commands/test.py
+++ project/helpers/management/commands/test.py
@@ -63,16 +63,4 @@ class Command(BaseCommand):
 
         # test_get_collection_data()
 
-        # url = 'https://www.mrlc.gov/geoserver/ows?' # 52000
-        # url = 'https://ows.emodnet-bathymetry.eu/wms' #512
-        # url = 'https://www.cmar.csiro.au/geoserver/wms?' #18M
-        url = 'https://geoserver.geoportal.gov.ph/geoserver/wms?' # 7M
-        # url = 'https://services.ga.gov.au/gis/services/2023GHG_AcreageReleaseAreas/MapServer/WMSServer?' # 322
-
-        # wms = WebMapService(url)
-        # print(wms)
-
-        response = get_wms_layers(url)
-        print(response)
-
         self.stdout.write(self.style.SUCCESS('Done.'))
