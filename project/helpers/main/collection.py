eb3a5a01fb263ad9a5842d3cd593706bd3199a53
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-03 11:04:52 +0530

    add layers form modal

--- project/helpers/main/collection.py
+++ project/helpers/main/collection.py
@@ -64,7 +64,9 @@ def get_layers(url, format):
             }}
             
         if format == 'xyz':
+            print('here')
             name = (' '.join([get_domain_name(url)]+[i for i in url.split(get_domain(url))[-1].split('/') if i != '' and not any([j for j in XYZ_TILES_CHARS if j in i])])).strip()
+            print(name)
             return {name: {
                 'title': name,
                 'type': format,
