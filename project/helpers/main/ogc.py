a7425237a43fb69ff1bcbcdac8aba10a34cfe485
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-03 17:15:03 +0530

    add layers form modal

--- project/helpers/main/ogc.py
+++ project/helpers/main/ogc.py
@@ -1,30 +1,31 @@
 from owslib.wms import WebMapService
 import psutil
 
+from helpers.base.utils import get_response
+
 def get_wms_layers(url):
     layers = {}
     
     try:
-        mem = psutil.virtual_memory()
-        print(mem)
-        if mem.available < 200_000_000:  # If available RAM is less than ~200MB, skip
-            raise Exception("Skipping WebMapService due to low memory")
+        response = get_response(f'{url}?request=GetCapabilities', raise_for_status=False)
+        print(response)
 
-        wms = WebMapService(url)
-        layer_names = list(wms.contents)
-        for i in layer_names:
-            layer = wms[i]
-            params = {'type': 'wms', 'title': layer.title} 
 
-            bbox = layer.boundingBoxWGS84 or layer.boundingBox
-            w,s,e,n,*srid = bbox or (-180, -90, 180, 90, 4326)
-            srid = int(srid[0].split(':')[-1]) if len(srid) > 0 else 4326
-            # transform bbox if not 4326
-            params.update({
-                'bbox': bbox, 
-                'srid': srid, 
-            })
-            layers[i] = params
+        # wms = WebMapService(url)
+        # layer_names = list(wms.contents)
+        # for i in layer_names:
+        #     layer = wms[i]
+        #     params = {'type': 'wms', 'title': layer.title} 
+
+        #     bbox = layer.boundingBoxWGS84 or layer.boundingBox
+        #     w,s,e,n,*srid = bbox or (-180, -90, 180, 90, 4326)
+        #     srid = int(srid[0].split(':')[-1]) if len(srid) > 0 else 4326
+        #     # transform bbox if not 4326
+        #     params.update({
+        #         'bbox': bbox, 
+        #         'srid': srid, 
+        #     })
+        #     layers[i] = params
     except Exception as e:
         print('get_wms_layers', e)
     
