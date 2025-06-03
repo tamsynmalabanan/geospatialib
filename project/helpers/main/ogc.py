679884eec0330fbdb5e71bd7400f548e3e2546f8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-03 17:29:13 +0530

    add layers form modal

--- project/helpers/main/ogc.py
+++ project/helpers/main/ogc.py
@@ -19,21 +19,21 @@ def get_wms_layers(url):
 
 
 
-        wms = WebMapService(url)
-        layer_names = list(wms.contents)
-        for i in layer_names:
-            layer = wms[i]
-            params = {'type': 'wms', 'title': layer.title} 
+        # wms = WebMapService(url)
+        # layer_names = list(wms.contents)
+        # for i in layer_names:
+        #     layer = wms[i]
+        #     params = {'type': 'wms', 'title': layer.title} 
 
-            bbox = layer.boundingBoxWGS84 or layer.boundingBox
-            w,s,e,n,*srid = bbox or (-180, -90, 180, 90, 4326)
-            srid = int(srid[0].split(':')[-1]) if len(srid) > 0 else 4326
-            # transform bbox if not 4326
-            params.update({
-                'bbox': bbox, 
-                'srid': srid, 
-            })
-            layers[i] = params
+        #     bbox = layer.boundingBoxWGS84 or layer.boundingBox
+        #     w,s,e,n,*srid = bbox or (-180, -90, 180, 90, 4326)
+        #     srid = int(srid[0].split(':')[-1]) if len(srid) > 0 else 4326
+        #     # transform bbox if not 4326
+        #     params.update({
+        #         'bbox': bbox, 
+        #         'srid': srid, 
+        #     })
+        #     layers[i] = params
     except Exception as e:
         print('get_wms_layers', e)
     
