c70878a2d270160dea7be037730760ff995452ea
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-03 21:33:43 +0530

    add layers form modal

--- project/static/helpers/gis/js/add_layers.js
+++ project/static/helpers/gis/js/add_layers.js
@@ -116,7 +116,8 @@ const handleAddLayersForm = () => {
             const element = getLayerNamesContainer(source).querySelector('[hx-trigger="update-collection"')
             if (element && Object.values(includedLayers).some(i => Object.keys(i).some(j => {
                 if (j === 'title') return false
-                if (form.elements[j].hidden) return false
+                if (form.elements[j].getAttribute('hidden')) return false
+                console.log(j)
                 return i[j] !== ''
             }))) {
                 try {
