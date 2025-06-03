64691acdb9427324618b5de0fa6d4a45f7d5398b
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-03 19:08:02 +0530

    add layers form modal

--- project/main/models.py
+++ project/main/models.py
@@ -57,6 +57,7 @@ class Layer(models.Model):
     srid = models.ForeignKey("main.SpatialRefSys", verbose_name='SRID', on_delete=models.PROTECT, default=4326)
     bbox = models.PolygonField('Bounding Box', blank=True, null=True)
     keywords = models.JSONField('Keywords', default=list)
+    abstract = models.TextField('Abstract', blank=True, null=True)
 
 
     class Meta:
