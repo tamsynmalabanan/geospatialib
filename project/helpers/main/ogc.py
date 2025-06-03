80f7398ca047b9e109f0ca73a6a5688127741da4
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-03 18:03:27 +0530

    add layers form modal

--- project/helpers/main/ogc.py
+++ project/helpers/main/ogc.py
@@ -18,7 +18,7 @@ def get_wms_layers(url):
         if len(content) > 99999:
             root = ET.fromstring(content)
             for layer in root.findall(f".//{NAMESPACE}Layer"):
-                print(layer)
+                print(layer.text)
                 name_elem = layer.find(f"{NAMESPACE}Name")
                 if not name_elem:
                     continue
