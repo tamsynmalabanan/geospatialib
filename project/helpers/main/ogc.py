f0c11243ea0dfa084469088c430acc871fe9d9ac
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-04 14:53:40 +0530

    add layers form modal

--- project/helpers/main/ogc.py
+++ project/helpers/main/ogc.py
@@ -73,11 +73,10 @@ def get_layers_via_et(content, format):
                 bbox = [-180, -90, 180, 90, 'EPSG:4326']
             w,s,e,n,*crs = bbox
             srid = int(crs[0].split(':')[-1]) if len(crs) > 0 else 4326
-            print(w,s,e,n,crs,srid)
-            # if srid != 4326:
-            #     geom = Polygon([(w,s), (e,s), (e,n), (w,n), (w,s)], srid=srid)
-            #     bbox = geom.transform(4326).extent
-            # print(bbox, srid)
+            if srid != 4326:
+                geom = Polygon([(w,s), (e,s), (e,n), (w,n), (w,s)], srid=srid)
+                print(geom)
+                # bbox = geom.transform(4326).extent
 
             layers[name.text] = params
 
