86c4c59ec67573f19e589f2ffcbb1ed8838cc59f
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-04 11:44:42 +0530

    add layers form modal

--- project/htmx/urls.py
+++ project/htmx/urls.py
@@ -6,8 +6,8 @@ app_name = 'htmx'
 
 urlpatterns = [
     path('cors_proxy/', views.cors_proxy, name='cors_proxy'),
-    path('validate_collection/', views.validate_collection, name='validate_collection'),
-    path('update_collection/', views.update_collection, name='update_collection'),
+    path('collection/validate/', views.validate_collection, name='validate_collection'),
+    path('collection/update/', views.update_collection, name='update_collection'),
     path('get_layer_forms/', views.get_layer_forms, name='get_layer_forms'),
     path('srs_wkt/<int:srid>/', views.srs_wkt, name='srs_wkt'),
 ]
