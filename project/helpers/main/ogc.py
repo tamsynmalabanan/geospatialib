37b0652f2039ac4fcde3bebe0956fb88494c30e8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-04 13:30:05 +0530

    add layers form modal

--- project/helpers/main/ogc.py
+++ project/helpers/main/ogc.py
@@ -1,5 +1,7 @@
 from django.contrib.gis.geos import GEOSGeometry, Polygon
 
+import requests
+import xml.etree.ElementTree as ET
 from owslib.wms import WebMapService
 import psutil
 import json
@@ -8,37 +10,46 @@ from helpers.base.utils import get_response
 
 NAMESPACE = '{http://www.opengis.net/wms}'
 
+def get_layers_via_owslib(service, format):
+    service_id = service.identification
+    service_tags = service_id.keywords or []
+    service_abstract = service_id.abstract or ''
+    layer_names = list(service.contents)
+    
+    layers = {}
+    for i in layer_names:
+        layer = service[i]
+        params = {'type': format, 'title': layer.title} 
+
+        bbox = layer.boundingBoxWGS84 or layer.boundingBox or (-180, -90, 180, 90, 'EPSG:4326')
+        w,s,e,n,*crs = bbox
+        srid = int(crs[0].split(':')[-1]) if len(crs) > 0 else 4326
+        if srid != 4326:
+            geom = Polygon([(w,s), (e,s), (e,n), (w,n), (w,s)], srid=srid)
+            bbox = geom.transform(4326).extent
+
+        params.update({
+            'bbox': list(bbox), 
+            'srid': srid, 
+            'keywords': service_tags + (layer.keywords or []), 
+            'abstract': ('<br><br>'.join([i for i in [service_abstract, (layer.abstract or '')] if i != ''])).strip(), 
+            'attribution': service_id.accessconstraints or '',
+            'fees': service_id.fees or '',
+            'styles': json.dumps(layer.styles)
+        })
+        layers[i] = params
+    return layers
+
 def get_wms_layers(url):
     layers = {}
     
-    try:        
-        wms = WebMapService(url)
-        wms_id = wms.identification
-        wms_tags = wms_id.keywords or []
-        wms_abstract = wms_id.abstract or ''
-        
-        layer_names = list(wms.contents)
-        for i in layer_names:
-            layer = wms[i]
-            params = {'type': 'wms', 'title': layer.title} 
-
-            bbox = layer.boundingBoxWGS84 or layer.boundingBox or (-180, -90, 180, 90, 'EPSG:4326')
-            w,s,e,n,*crs = bbox
-            srid = int(crs[0].split(':')[-1]) if len(crs) > 0 else 4326
-            if srid != 4326:
-                geom = Polygon([(w,s), (e,s), (e,n), (w,n), (w,s)], srid=srid)
-                bbox = geom.transform(4326).extent
-
-            params.update({
-                'bbox': list(bbox), 
-                'srid': srid, 
-                'keywords': wms_tags + (layer.keywords or []), 
-                'abstract': ('<br><br>'.join([i for i in [wms_abstract, (layer.abstract or '')] if i != ''])).strip(), 
-                'attribution': wms_id.accessconstraints or '',
-                'fees': wms_id.fees or '',
-                'styles': json.dumps(layer.styles)
-            })
-            layers[i] = params
+    try:
+        response = get_response(f'{url}?service=WMS&request=GetCapabilities', raise_for_status=False)
+        response.raise_for_status()
+        content = response.content
+        if len(content) < 100000:
+            wms = WebMapService(url)
+            layers = get_layers_via_owslib(wms, 'wms')
     except Exception as e:
         print('get_wms_layers', e)
     
