05cd1dde60f1b7d0066ac3f348b535312acd2a74
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-05 18:51:18 +0530

    add layers form modal

--- project/static/helpers/gis/js/geojson.js
+++ project/static/helpers/gis/js/geojson.js
@@ -472,14 +472,12 @@ const getGeoJSON = async (dbKey, {
                         }
                     )
 
-                    console.log(geojson)
-
                     if (!geojson) return new Error('No geojson retrieved.')
                     
                     if (geojson.features?.length) {
                         if (controller?.signal.aborted) return
                         await normalizeGeoJSON(geojson, {queryGeom, controller, abortBtns})
-                        
+                        console.log(geojson)
                         if (controller?.signal.aborted) return
                         if (handlerName !== 'nominatim') {
                             await updateGeoJSONOnDB(
