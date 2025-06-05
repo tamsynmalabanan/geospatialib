ab6ef0e896d8d85b802db8ad2823548065ffeff0
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-05 12:28:52 +0530

    add layers form modal

--- project/static/helpers/gis/js/add_layers.js
+++ project/static/helpers/gis/js/add_layers.js
@@ -132,6 +132,7 @@ const handleAddLayersForm = () => {
         }
         
         // resetForm()
+        Array.from(getLayerNamesContainer(source).querySelectorAll('.form-check-input[type="checkbox"]')).forEach(i => i.checked = false)
         modalInstance.hide()
     })
 
