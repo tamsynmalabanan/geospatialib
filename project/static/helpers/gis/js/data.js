25ab9d04645bdb510b4f8cd0b182ca180a49b4a6
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-06 15:55:01 +0530

    add layers form modal

--- project/static/helpers/gis/js/data.js
+++ project/static/helpers/gis/js/data.js
@@ -87,7 +87,44 @@ const fetchWFSData = async (params, {queryGeom, zoom, abortBtns, controller, eve
     }
 
     const url = pushQueryParamsToURLString(cleanURL, getParams)
-    console.log(getParams, url)
+        return await fetchTimeout(url, {
+        abortBtns,
+        controller,
+        callback: async (response) => {
+            const contentType = response.headers.get('Content-Type')
+            if (contentType.includes('json')) {
+                try {
+                    return parseJSONResponse(response)
+                } catch {
+                    throw new Error('Failed to parse JSON.')
+                }
+            } else if (contentType.includes('xml')) {
+                return response.text()
+                .then(xmlString => {
+                    const features = []
+
+                    const [namespace, rootElement] = parseXML(xmlString)
+                    if (namespace === 'http://www.esri.com/wms') {
+                        rootElement.childNodes.forEach(child => {
+                            const tagName = child.tagName
+                            if (!tagName || tagName.toLowerCase() !== 'fields') return
+                            
+                            const attributes = Object.values(child.attributes)
+                            if (attributes.length == 0) return
+                            
+                            const feature = {type: "Feature", properties:{}}
+                            attributes.forEach(attr => feature.properties[attr.name] = attr.value)
+                            features.push(feature)
+                        })
+                    }
+                    
+                    return geojson = turf.featureCollection(features)
+                })
+            }
+        }
+    }).catch(error => {
+        console.log(error)
+    })
 }
 
 const fetchGeoJSON = async (params, {abortBtns, controller} = {}) => {
