66013e7a3c384c6b748e998f5b6fe998eae2408a
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-06 13:04:12 +0530

    add layers form modal

--- project/helpers/main/ogc.py
+++ project/helpers/main/ogc.py
@@ -124,7 +124,7 @@ def get_ogc_layers(url, format):
         response.raise_for_status()
         content = response.content
         print(len(content))
-        if len(content) < 1000000:
+        if len(content) < 100000:
             service = None
             if type == 'wms':
                 service = WebMapService(url)
