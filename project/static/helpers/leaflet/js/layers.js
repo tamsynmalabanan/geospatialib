95853db0ebcc008537eda368c4cdf4d44400c555
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-11 19:27:36 +0530

    add layers form modal

--- project/static/helpers/leaflet/js/layers.js
+++ project/static/helpers/leaflet/js/layers.js
@@ -1,4 +1,4 @@
-await getBootstrapIcons()
+getBootstrapIcons()
 
 const addLeafletBasemapLayer = (map) => L.tileLayer("//tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
