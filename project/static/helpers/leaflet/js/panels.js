833e1cfb76fa4f6083ec748b37c77ef6cea27b90
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-13 14:02:04 +0530

    add layers form modal

--- project/static/helpers/leaflet/js/panels.js
+++ project/static/helpers/leaflet/js/panels.js
@@ -1013,7 +1013,7 @@ const handleLeafletStylePanel = (map, parent) => {
                 'img': 'image url',
                 'svg': 'svg element',
                 'html': 'html element',
-                'property': 'feature property',
+                // 'property': 'feature property',
             },
             currentValue: styleParams.iconType,
             events: {
