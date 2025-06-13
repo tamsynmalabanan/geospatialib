533e74e0ef6186745961fd487021477d94dc72b5
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-13 18:42:03 +0530

    add layers form modal

--- project/helpers/main/ogc.py
+++ project/helpers/main/ogc.py
@@ -20,6 +20,7 @@ def get_layers_via_owslib(service, format):
     
     for i in list(service.contents):
         layer = service[i]
+        print(layer)
         params = {'type': format, 'title': layer.title} 
 
         if format == 'wcs':
