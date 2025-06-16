09103687b63c4dd5e03f70d94b8a7b7d5fce8ded
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-16 16:28:45 +0530

    add layers form modal

--- project/static/helpers/leaflet/js/panels/legend.js
+++ project/static/helpers/leaflet/js/panels/legend.js
@@ -472,7 +472,7 @@ const handleLeafletLegendPanel = async (map, parent) => {
         }
 
         if ((isHidden || isInvisible)) {
-            console.log('here')
+            console.log('isHidden, isInvisible', isHidden, isInvisible)
             map.removeLayer(layer)
         } else {
             map._ch.updateCachedLegendLayers({layer})
