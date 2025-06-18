0030b2da4fb8d13214bedc4ccf7d9d3e7e075af3
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-18 11:07:02 +0530

    add layers form modal

--- project/main/forms.py
+++ project/main/forms.py
@@ -17,7 +17,7 @@ class SearchForm(forms.Form):
         required=True,
         widget=forms.TextInput(attrs={
             'type':'search',
-            'class':'form-control border-0 rounded-0 field-underline-primary box-shadow-none bg-transparent',
+            'class':'form-control border-0 rounded-0 focus-underline-primary box-shadow-none bg-transparent',
         })
     )
 
