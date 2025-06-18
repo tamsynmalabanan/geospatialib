e40e2d89d99abb064fd1040c57009f8618c75ca4
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-18 19:38:42 +0530

    add layers form modal

--- project/htmx/middleware.py
+++ project/htmx/middleware.py
@@ -8,13 +8,13 @@ class HTMXDomainRestriction:
         self.get_response = get_response
 
     def __call__(self, request):
-        path = resolve(request.path)
-        if path.app_name == 'htmx':
-            not_htmx = not request.htmx
-            not_allowed_host = request.META.get('HTTP_HOST') not in settings.ALLOWED_HOSTS
-            if not_htmx or not_allowed_host:
-                return redirect('main:index')
-                # return HttpResponseForbidden('You do not have permission to access this resource.')
+        # path = resolve(request.path)
+        # if path.app_name == 'htmx':
+        #     not_htmx = not request.htmx
+        #     not_allowed_host = request.META.get('HTTP_HOST') not in settings.ALLOWED_HOSTS
+        #     if not_htmx or not_allowed_host:
+        #         return redirect('main:index')
+        #         # return HttpResponseForbidden('You do not have permission to access this resource.')
 
 
         response = self.get_response(request)
