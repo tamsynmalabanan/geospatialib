adcf0798ae020ac485bd31c8f26cc9347da902ce
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-18 12:13:39 +0530

    add layers form modal

--- project/static/main/js/index.js
+++ project/static/main/js/index.js
@@ -1,9 +1,5 @@
 const handleSearchForm = () => {
     const form = document.querySelector('#searchLibraryForm')
-
-    form.addEventListener('submit', (e) => e.preventDefault())
-
-    
 }
 
 document.addEventListener('DOMContentLoaded', () => {
