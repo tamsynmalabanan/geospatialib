f3017a1b4616786c6ab7bb5bc5ceb8277ecfea3c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-18 17:31:42 +0530

    add layers form modal

--- project/main/views.py
+++ project/main/views.py
@@ -6,8 +6,6 @@ from . import forms
 def index(request):
     data = request.GET.dict()
     context = {'form': forms.SearchForm(data)}
-
     if data.get('query', '') == '':
         context['featured'] = 'featured content'
-
     return render(request, 'main/index.html', context)
