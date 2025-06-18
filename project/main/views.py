71184ce25a8edf7b0821a7397802396f4a87baa4
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-18 10:24:34 +0530

    add layers form modal

--- project/main/views.py
+++ project/main/views.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect, HttpResponse
 
+from . import forms
+
 def index(request):
     # messages.success(request, 'test', 'main-index-map')
-    context = {}
-    return render(request, 'main/index.html', context)
+    return render(request, 'main/index.html', {
+        'search_form': forms.SearchForm()
+    })
