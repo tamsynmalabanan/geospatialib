702ad639be101595573c0d5f9d0c395a19827c2d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-18 13:11:15 +0530

    add layers form modal

--- project/static/main/js/index.js
+++ project/static/main/js/index.js
@@ -5,19 +5,21 @@ const handleSearchForm = () => {
     form.addEventListener('submit', (e) => {
         e.preventDefault()
 
-        const params = {}
-        Array.from(form.elements).forEach(i => {
-            const name = i.getAttribute('name')
-            if (!name) return
-            params[name] = i.value
-        })
-        setURLParams(params)
+        if (queryField.value) {
+            const params = {}
+            Array.from(form.elements).forEach(i => {
+                const name = i.getAttribute('name')
+                if (!name) return
+                params[name] = i.value
+            })
+            setURLParams(params)
+        }
     })
 
     form.addEventListener('htmx:beforeRequest', (e) => {
-        if (queryField.value) return
-
-        e.preventDefault()
+        if (!queryField.value) {
+            e.preventDefault()
+        }
     })
 }
 
