59268e7a6e4a88858683648a9f6d6841d67f3ca6
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-19 10:35:06 +0530

    add layers form modal

--- project/static/main/js/index.js
+++ project/static/main/js/index.js
@@ -30,7 +30,8 @@ const handleSearchForm = () => {
     
     form.addEventListener('htmx:beforeRequest', (e) => {
         if (queryField.value) {
-            document.querySelector('#searchResultsFiltersContainer').lastElementChild.innerHTML = ''
+            const resultsLabel = document.querySelector('#searchResultsFiltersContainer').lastElementChild
+            if (resultsLabel) resultsLabel.innerHTML = ''
             document.querySelector('#searchResults').innerHTML = removeWhitespace(`
                 <div class="flex-grow-1 d-flex justify-content-center mt-5">
                     <div class="spinner-border" role="status"></div>
