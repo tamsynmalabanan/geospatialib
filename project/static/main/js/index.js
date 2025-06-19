f51c8db182267c3d6352d838a67bb2a42ac600af
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-19 10:05:04 +0530

    add layers form modal

--- project/static/main/js/index.js
+++ project/static/main/js/index.js
@@ -6,7 +6,6 @@ const handleSearchForm = () => {
 
     form.addEventListener('htmx:configRequest', (e) => {
         const requestParams = e.detail.parameters
-
         if (!Object.keys(requestParams).includes('clear')){
             const urlParams = Object.fromEntries(new URLSearchParams(window.location.search))
             console.log('urlParams',urlParams)
@@ -16,15 +15,12 @@ const handleSearchForm = () => {
                 if (!urlParams[key]) continue
                 requestParams[key] = urlParams[key]
             }
-        } else {
-            delete requestParams.clear
         }
 
-        console.log(requestParams)
-        // Object.keys(requestParams).forEach(key => {
-        //     if (requestParams.get(key)) return
-        //     delete 
-        // })
+        Object.keys(requestParams).forEach(key => {
+            if (requestParams.get(key)) return
+            requestParams.deleteProperty(key)
+        })
 
         console.log('query',requestParams.get('query'))
         console.log('type',requestParams.get('type'))
