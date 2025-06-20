d9ef4acac7b9e50ce21456bdad9488cb92237fd1
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-20 09:05:19 +0530

    add layers form modal

--- project/static/main/js/index.js
+++ project/static/main/js/index.js
@@ -2,7 +2,7 @@ const getSearchForm = () => document.querySelector('#searchForm')
 
 const getSearchMap = () => maps.find(map => map.getContainer().id === getSearchForm().getAttribute('data-search-map-id'))
 
-const zoomToSearchResultBbox = (event) => getSearchMap()?.fitBounds(L.geoJSON(turf.bboxPolygon(JSON.parse(event.target.dataset.layerBbox))).getBounds())
+const zoomToSearchResultBbox = () => getSearchMap()?.fitBounds(L.geoJSON(turf.bboxPolygon(JSON.parse(event.target.dataset.layerBbox))).getBounds())
 
 const addSearchResultToMap = async () => {
     const el = event.target
