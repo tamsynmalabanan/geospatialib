e40537d9bf706fd10414016a2d727107cadd3705
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-20 15:42:35 +0530

    add layers form modal

--- project/static/helpers/base/js/utils.js
+++ project/static/helpers/base/js/utils.js
@@ -141,10 +141,12 @@ const hslToHex = ({h, s, l}={}) => {
 
 const hexToHSLA = (hex) => {
     // Convert hex to RGB
-    let r = parseInt(hex.substring(1, 3), 16) / 255;
-    let g = parseInt(hex.substring(3, 5), 16) / 255;
-    let b = parseInt(hex.substring(5, 7), 16) / 255;
+    let r = parseInt(hex.split(',')[0]) / 255;
+    let g = parseInt(hex.split(',')[1]) / 255;
+    let b = parseInt(hex.split(',')[2]) / 255;
   
+    console.log(hex, r, g, b)
+
     // Find max and min values of RGB
     let max = Math.max(r, g, b);
     let min = Math.min(r, g, b);
