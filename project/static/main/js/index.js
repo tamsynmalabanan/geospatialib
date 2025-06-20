324d92207b920f82ed755ae9f078adcbd39c11e6
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-20 12:14:58 +0530

    add layers form modal

--- project/static/main/js/index.js
+++ project/static/main/js/index.js
@@ -39,18 +39,23 @@ const addSearchResultBboxToMap = async (el) => {
         dashArray: '1 3'
     }
 
-    const layer = await getLeafletGeoJSONLayer({
-        geojson: turf.bboxPolygon(
-            JSON.parse(el.dataset.layerBbox), 
-            {properties:JSON.parse(el.previousElementSibling.dataset.layerData)}
-        ),
-        pane: 'searchPane',
-        group,
-        customStyleParams,
-        params: {type: 'geojson', title: 'Search result'}
-    })
-
-    if (layer) group?.addLayer(layer)
+    try {
+        const layer = await getLeafletGeoJSONLayer({
+            geojson: turf.bboxPolygon(
+                JSON.parse(el.dataset.layerBbox), 
+                {properties:JSON.parse(el.previousElementSibling.dataset.layerData)}
+            ),
+            pane: 'searchPane',
+            group,
+            customStyleParams,
+            params: {type: 'geojson', title: 'Search result'}
+        })
+    
+        if (layer) group?.addLayer(layer)
+    } catch (error) {
+        console.log(error)
+        console.log(el)
+    }
 }
 
 const toggleSearchResultBbox = async () => {
