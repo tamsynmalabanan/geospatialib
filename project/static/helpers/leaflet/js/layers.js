2a6147bb3ce0a4d52f10ea66463a95e8e058fab8
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-20 14:43:57 +0530

    add layers form modal

--- project/static/helpers/leaflet/js/layers.js
+++ project/static/helpers/leaflet/js/layers.js
@@ -809,8 +809,10 @@ const getLeafletLayerContextMenu = async (e, layer, {
             }
         },
         legend: {
-            innerText: isLegendGroup && !feature ? `Duplicate ${typeLabel}` : 'Add to legend',
+            innerText: (
+                isLegendGroup && !feature ? `Duplicate ${typeLabel}` : 'Add to legend'),
             btnCallback: async () => {
+                console.log(layer)
                 createLeafletLayer(layer._params, {
                     dbIndexedKey: layer._dbIndexedKey,
                     data: layerGeoJSON,
