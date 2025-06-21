af6f977657298e4bb2c4fdb8b14c339588e82daa
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-21 23:30:03 +0530

    add layers form modal

--- project/static/helpers/gis/js/add_layers.js
+++ project/static/helpers/gis/js/add_layers.js
@@ -217,7 +217,9 @@ const handleAddLayersForm = () => {
     })
 
     form.addEventListener('htmx:beforeSwap', (e) => {
+        console.log(e)
         if (e.target === form.elements.url || e.target === form.elements.format) {
+
             getLayerNamesContainer('url').innerHTML = ''
         }
     })
