84513c368ed8546e2a61edea7eb58c91cd753742
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-23 15:05:29 +0530

    add layers form modal

--- project/static/helpers/gis/js/export_layers.js
+++ project/static/helpers/gis/js/export_layers.js
@@ -29,8 +29,8 @@ const handleExportLayersForm = () => {
     resetBtn.addEventListener('click', (e) => resetLayers())
 
     submitBtn.addEventListener('click', (e) => {
-        const layers = JSON.stringify(layers)
-        const blob = new Blob([layers], {type:'application/json'})
+        const data = JSON.stringify(layers)
+        const blob = new Blob([data], {type:'application/json'})
         const url = URL.createObjectURL(blob)
         const a = document.createElement('a')
         a.href = url
