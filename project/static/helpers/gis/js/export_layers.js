ef19ca6bf4b7e80c7a3cd66d267102a49cfec0fe
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-06-23 15:28:41 +0530

    add layers form modal

--- project/static/helpers/gis/js/export_layers.js
+++ project/static/helpers/gis/js/export_layers.js
@@ -35,7 +35,7 @@ const handleExportLayersForm = () => {
         const url = URL.createObjectURL(blob)
         const a = document.createElement('a')
         a.href = url
-        a.download = `geospatialib_map.json`
+        a.download = `map.gsl`
         document.body.appendChild(a)
         a.click()
         document.body.removeChild(a)
