fc4c5169288992eaf8b8ef895af2a8e52696f442
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-01 08:23:59 +0530

    add layers form modal

--- project/static/helpers/leaflet/js/init.js
+++ project/static/helpers/leaflet/js/init.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener("map:init", (event) => {
-        const map = event.detail.map
-        const container = map.getContainer()
-        const dataset = container.parentElement.dataset
+window.addEventListener("map:init", (event) => {
+    const map = event.detail.map
+    const container = map.getContainer()
+    const dataset = container.parentElement.dataset
 
-        container.className = `bg-${getPreferredTheme()} ${container.className} ${dataset.mapClass || ''}`
-        addLeafletBasemapLayer(map)
-        handleLeafletLayerGroups(map)   
-        if (dataset.mapPanels === 'true') handleLeafletMapPanels(map)
-        handleLeafletMapControls(map)
-        elementResizeObserver(container, () => map.invalidateSize())
-        assignMapObservers(map)
+    container.className = `bg-${getPreferredTheme()} ${container.className} ${dataset.mapClass || ''}`
+    addLeafletBasemapLayer(map)
+    handleLeafletLayerGroups(map)   
+    if (dataset.mapPanels === 'true') handleLeafletMapPanels(map)
+    handleLeafletMapControls(map)
+    elementResizeObserver(container, () => map.invalidateSize())
+    assignMapObservers(map)
 
-        map._initComplete = true
-        map.fire('initComplete')
-    })
+    map._initComplete = true
+    map.fire('initComplete')
 })
+// document.addEventListener('DOMContentLoaded', () => {
+// })
