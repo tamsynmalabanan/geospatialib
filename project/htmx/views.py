51a6fcf54dbab2b7198a0daa1c6256ab20c47c05
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-16 21:32:09 +0800

    config search view

--- project/htmx/views.py
+++ project/htmx/views.py
@@ -8,7 +8,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 from django.db.models import QuerySet, Count, Sum, F, IntegerField, Value, Q, Case, When, Max, TextField, CharField, FloatField
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
-
+from django.utils.functional import cached_property
 
 import json
 import requests
@@ -16,6 +16,7 @@ import validators
 import os
 from functools import reduce
 from operator import or_, add
+from urllib.parse import urlparse
 
 from helpers.main.collection import get_collection_data, sort_layers, update_collection_data
 from main.models import SpatialRefSys, URL, Layer
@@ -31,60 +32,58 @@ class SearchList(ListView):
     context_object_name = 'layers'
     paginate_by = 24
 
-    @property
-    def query(self):
-        query = self.request.GET.get('query', '').strip()
-
-        exclusions = []
-
-        if validators.url(query) == True:
-            query = query.split('?')[0]
-        else:
-            if ' -' in f' {query}':
-                keywords = query.split(' ')
-                exclusions = [i[1:] for i in keywords if i.startswith('-') and len(i) > 1]
-                query = ' '.join([i for i in keywords if not i.startswith('-') and i != ''])
-
-            query = query.replace(' ', ' OR ')
-
-        return (query, exclusions)
-
-    @property
+    @cached_property
     def filter_fields(self):
         return [
             'type',
         ]
 
-    @property
+    @cached_property
     def filter_expressions(self):
         return [
             'bbox__bboverlaps'
         ]
 
-    @property
-    def filters(self):
+    @cached_property
+    def query_filters(self):
         return self.filter_fields + self.filter_expressions
 
-    @property
+    @cached_property
+    def query_values(self):
+        query = self.request.GET.get('query', '').strip()
+        exclusions = []
+
+        if validators.url(query) == True:
+            query = urlparse(query).path
+        else:
+            if ' -' in f' {query}':
+                keywords = query.split(' ')
+                exclusions = [i[1:] for i in keywords if i.startswith('-') and len(i) > 1]
+                query = ' '.join([i for i in keywords if not i.startswith('-') and i != ''])
+            query = query.replace(' ', ' OR ')
+
+        return (query, exclusions)
+
+    @cached_property
     def cache_key(self):
-        return create_cache_key([
+        return ';'.join([str(i) for i in [
             'SearchList',
             self.request.GET.get('query', '').strip()
-        ])
-
-    def apply_query_filters(self, queryset):
-        return queryset.filter(**{
-            param : value 
-            for param, value in self.request.GET.items() 
-            if value and param in self.filters
-        })
+        ]])
 
     def perform_full_text_search(self):
-        query, exclusions = self.query
+        query, exclusions = self.query_values
 
-        queryset = (super().get_queryset().select_related('collection__url'))
-
-        queryset = queryset.exclude(reduce(or_, (Q(name__icontains=word) | Q(title__icontains=word) for word in exclusions), Q()))
+        queryset = (
+            super().get_queryset()
+            .select_related(
+                'collection__url',
+            )
+        ).exclude(reduce(
+            or_, 
+            (Q(name__icontains=word) | Q(title__icontains=word) for word in exclusions), 
+            Q()
+        ))
 
         search_query = SearchQuery(query, search_type='plain' if validators.url(query) == True else 'websearch')
 
@@ -105,6 +104,36 @@ class SearchList(ListView):
         )
         
         return queryset
+
+    def get_queryset(self):
+        if not hasattr(self, 'queryset') or getattr(self, 'queryset') is None:
+            queryset = cache.get(self.cache_key)
+
+            if not queryset or not queryset.exists():
+                queryset = self.perform_full_text_search()
+
+            if queryset.exists():
+                cache.set(self.cache_key, queryset, timeout=60*15)
+                queryset = self.apply_query_filters(queryset)
+                
+            self.queryset = queryset
+
+        queryset = self.queryset
+        if queryset and queryset.exists():
+            queryset = (
+                self.queryset
+                .annotate(rank=Max('rank'))
+                .order_by(*['-rank', 'title','type'])
+            )
+
+        return queryset
+
+    def apply_query_filters(self, queryset):
+        return queryset.filter(**{
+            param : value 
+            for param, value in self.request.GET.items() 
+            if value and param in self.query_filters
+        })
 
     def get_filters(self):
         filters = {
@@ -132,29 +161,6 @@ class SearchList(ListView):
             context['filters'] = self.get_filters()
             context['params'] = [i for i in list(self.request.GET.values()) if i and i != '']
         return context
-
-    def get_queryset(self):
-        if not hasattr(self, 'queryset') or getattr(self, 'queryset') is None:
-            queryset = cache.get(self.cache_key)
-
-            if not queryset or not queryset.exists():
-                queryset = self.perform_full_text_search()
-
-            if queryset.exists():
-                cache.set(self.cache_key, queryset, timeout=60*15)
-                queryset = self.apply_query_filters(queryset)
-                
-            self.queryset = queryset
-
-        queryset = self.queryset
-        if queryset and queryset.exists():
-            queryset = (
-                self.queryset
-                .annotate(rank=Max('rank'))
-                .order_by(*['-rank', 'title','type'])
-            )
-
-        return queryset
 
 @require_http_methods(['GET'])
 def validate_collection(request):
