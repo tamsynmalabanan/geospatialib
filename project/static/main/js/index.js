6caa53ccd7e8692dc2b6f893b80542daf4e4863c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-16 20:18:11 +0800

    config to show search sidebar when there is a query

--- project/static/main/js/index.js
+++ project/static/main/js/index.js
@@ -80,25 +80,25 @@ const toggleSearchResultBbox = async () => {
     el.classList.toggle('bi-eye-slash', hasLayers)
 }
 
-const handleSearchForm = () => {
+document.addEventListener('DOMContentLoaded', () => {
     const form = getSearchForm()
     const queryField = form.elements.query
-
+    
     form.addEventListener('submit', (e) => e.preventDefault())
-
+    
     form.addEventListener('htmx:configRequest', (e) => {
         const requestParams = e.detail.parameters
-
+    
         if (!Object.keys(requestParams).includes('clear')){
             const urlParams = Object.fromEntries(new URLSearchParams(window.location.search))
-
+    
             for (const key in urlParams) {
                 if (Object.keys(requestParams).includes(key)) continue
                 if (!urlParams[key]) continue
                 requestParams[key] = urlParams[key]
             }
         }
-
+    
         Object.keys(requestParams).forEach(key => {
             if (requestParams.get(key)) return
             requestParams.delete(key)
@@ -107,7 +107,7 @@ const handleSearchForm = () => {
     
     form.addEventListener('htmx:beforeRequest', (e) => {
         if (!queryField.value) return e.preventDefault()
-
+    
         document.querySelector('#searchResultsFiltersContainer').innerHTML = ''
         document.querySelector('#searchResults').innerHTML = removeWhitespace(`
             <div class="flex-grow-1 mt-5 d-flex justify-content-center gap-3">
@@ -116,38 +116,37 @@ const handleSearchForm = () => {
                 <div class="spinner-grow spinner-grow-sm text-primary my-3" role="status"></div>
             </div>
         `)
-
+    
         getSearchMap()?._handlers.getLayerGroups().search.clearLayers()
-
+    
         const urlParams = e.detail.pathInfo.finalRequestPath.split('?')
         window.history.pushState(
             {}, '', `${window.location.pathname}?${urlParams[urlParams.length-1]}`
         )
     })
-
+    
     form.addEventListener('htmx:afterSwap', (e) => {
         const map = getSearchMap()
         if (map) Array.from(form.querySelectorAll(`[data-map-bbox-field="true"]`)).forEach(i => {
             i.value = JSON.stringify(turf.bboxPolygon(getLeafletMapBbox(map)).geometry)
         })
-
+    
     })
-
+    
     const searchResults = document.querySelector('#searchResults')
     searchResults.parentElement.addEventListener('htmx:afterSwap', (e) => {
         if (e.target.id === searchResults.id) return
         if (!form.querySelector(`[onclick="toggleSearchResultBbox()"]`).classList.contains('bi-eye-slash')) return
-
+    
         const el = e.target.querySelector(`[onclick="zoomToSearchResultBbox()"]`)
         if (el) addSearchResultBboxToMap(el)
     })
-
+    
     if (queryField.value) {
         const submitEvent = new Event('submit')
         form.dispatchEvent(submitEvent)
-    }
-}
 
-document.addEventListener('DOMContentLoaded', () => {
-    handleSearchForm()
+        const offcanvas = form.closest('.offcanvas')
+        if (offcanvas) bootstrap.Offcanvas.getOrCreateInstance(offcanvas).show()
+    }
 })
