ee1c11fcf94ea514dc1f89ace6fc1d8696d5ec3e
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-22 19:15:26 +0530

    config search view to remove ' OR '

--- project/helpers/management/commands/test.py
+++ project/helpers/management/commands/test.py
@@ -41,7 +41,7 @@ def test_get_collection_data():
     # url = 'http://88.99.52.155/cgi-bin/tapp/tilecache.py/1.0.0/topomapper_v2/%7Bz%7D/%7Bx%7D/%7By%7D.jpg'
     # url = 'https://wms.gebco.net/mapserv?request=getcapabilities&service=wms&version=1.3.0'
     # url = 'https://geoserver.geoportal.gov.ph/geoserver/wms?request=GetCapabilities&service=WMS'
-    url = 'https://ows.emodnet-bathymetry.eu/wfs?request=GetCapabilities&service=WFS'
+    url = 'https://services.ga.gov.au/gis/eggs/aus_chronostrat_v1/wms?request=GetCapabilities&service=WMS'
     value = get_collection_data(url, delay=False)
     print(value)
     print('layers count', len((value or {}).get('layers', {}).keys()))
@@ -51,6 +51,6 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # URL.objects.all().delete()
 
-        # test_get_collection_data()
+        test_get_collection_data()
 
         self.stdout.write(self.style.SUCCESS('Done.'))
