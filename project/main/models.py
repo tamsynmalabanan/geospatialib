953d95c57230e717240e1a87dfce0cbb967405f3
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-22 21:43:41 +0530

    config search view to return ' OR '

--- project/main/models.py
+++ project/main/models.py
@@ -1,14 +1,18 @@
 from django.contrib.gis.db import models
+from django.db.models import Func
 from django.forms.models import model_to_dict
 from django.contrib.postgres.search import SearchVectorField
 from django.contrib.postgres.indexes import GinIndex
 from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVectorField
+from django.db.models.functions import Coalesce
+from django.db.models import GeneratedField
+from django.db.models.expressions import RawSQL
 
 from urllib.parse import urlparse
 
 from helpers.base.models import dict_to_choices
 from helpers.main.constants import WORLD_GEOM
-
 from . import choices
 
 class SpatialRefSys(models.Model):
@@ -55,7 +59,11 @@ class Collection(models.Model):
 
     def get_layer_data(self):
         return {layer.name: layer.data for layer in self.layers.all()}
-    
+
+class ToTSVector(Func):
+    function = 'to_tsvector'
+    template = "%(function)s('english', %(expressions)s)"
+
 class Layer(models.Model):
     collection = models.ForeignKey("main.Collection", verbose_name='Collection', on_delete=models.CASCADE, related_name='layers')
     name = models.CharField('Name', max_length=512)
@@ -70,7 +78,22 @@ class Layer(models.Model):
     attribution = models.TextField('Attribution', blank=True, null=True)
     fees = models.TextField('Fees', blank=True, null=True)
     styles = models.JSONField('Styles', default=dict, blank=True, null=True)
-    search_vector = SearchVectorField(null=True)
+    # search_vector = SearchVectorField(null=True)
+
+    search_vector = GeneratedField(
+        expression=ToTSVector(
+            Coalesce(
+                'name',
+                'title',
+                'abstract',
+                RawSQL("keywords::text", []),
+                'attribution',
+                RawSQL("styles::text", [])
+            )
+        ),
+        output_field=SearchVectorField(),
+        db_persist=True
+    )
 
     class Meta:
         unique_together = ['collection', 'name']
