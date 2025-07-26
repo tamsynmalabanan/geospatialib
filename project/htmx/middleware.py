df47f0dfef6505130cb35871964bd346234af304
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-27 02:25:39 +0530

    Fix search caching

--- project/htmx/middleware.py
+++ project/htmx/middleware.py
@@ -8,12 +8,12 @@ class HTMXDomainRestriction:
         self.get_response = get_response
 
     def __call__(self, request):
-        path = resolve(request.path)
-        if path.app_name == 'htmx':
-            not_htmx = not request.htmx
-            not_allowed_host = not any([i for i in settings.ALLOWED_HOSTS if i in request.META.get('HTTP_HOST')])
-            if not_htmx or not_allowed_host:
-                return redirect('main:index')
+        # path = resolve(request.path)
+        # if path.app_name == 'htmx':
+        #     not_htmx = not request.htmx
+        #     not_allowed_host = not any([i for i in settings.ALLOWED_HOSTS if i in request.META.get('HTTP_HOST')])
+        #     if not_htmx or not_allowed_host:
+        #         return redirect('main:index')
                 # return HttpResponseForbidden('You do not have permission to access this resource.')
 
         response = self.get_response(request)
