55b6ad01ad33913f2f3035be87a18db6a56e51da
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-27 20:16:49 +0530

    Set timeout for search and handle errors

--- project/static/main/js/index.js
+++ project/static/main/js/index.js
@@ -134,9 +134,9 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     form.addEventListener('htmx:responseError', (e) => {
+        console.log(e)
         if (e.detail.requestConfig.path.includes('htmx/library/search')) {
             const target = e.detail.target
-            console.log(e)
             target.classList.remove('htmx-indicator')
             target.innerHTML = ''
             target.appendChild(customCreateElement({tag:'div', innerHTML: 'Server error. Please try again.', className: 'd-flex w-100 justify-content-center'}))
