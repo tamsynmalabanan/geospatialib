31eaba54e02afd8f55bbeb923f1c19d7cd11358d
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-27 12:20:47 +0530

    shorten collection validity to 1 dat

--- project/htmx/views.py
+++ project/htmx/views.py
@@ -44,29 +44,29 @@ class LayerList(ListView):
 
         if ' -' in f' {query}':
             exclusions = [i[1:] for i in query.split() if i.startswith('-') and len(i) > 2]
-            query = ' '.join([i for i in query.split() if not i.startswith('-') and len(i) > 2])
+            query = ' '.join([i for i in query.split() if not i.startswith('-') and len(i) > 1])
       
         query = query.replace('_', ' ').replace('"', '').split()
         return f'({' | '.join([f"'{i}'" for i in query])}){f' & !({' | '.join([f"'{i}'" for i in exclusions])})' if exclusions else ''}'
 
     @property
-    def query_values(self):
-        return [str(v).strip() for k, v in self.request.GET.items() if k != 'page' and v != '']
+    def filter_values(self):
+        return [str(v).strip() for k, v in self.request.GET.items() if k not in ['query', 'page'] and v != '']
 
     @property
     def cache_key(self):
-        return create_cache_key(['layer_list']+self.query_values)
+        return create_cache_key(['layer_list']+[self.raw_query]+self.filter_values)
 
     @property
     def filtered_queryset(self):
         queryset = (
             super().get_queryset()
-            .select_related(
-                'collection__url',
-            )
+            # .select_related(
+            #     'collection__url',
+            # )
         )
 
-        if len(self.query_values) > 1:
+        if self.filter_values:
             queryset = queryset.filter(**{
                 param : value 
                 for param, value in self.request.GET.items()
@@ -132,7 +132,7 @@ class LayerList(ListView):
         context = super().get_context_data(**kwargs)
         if context['page_obj'].number == 1:
             context['filters'] = self.query_filters
-            context['values'] = self.query_values
+            context['is_filtered'] = self.filter_values > 0
         return context
 
 @require_http_methods(['GET'])
