c35cc9887187fc5e164d95f06f8609243fa2796c
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-27 19:53:44 +0530

    Set timeout for search and handle errors

--- project/htmx/views.py
+++ project/htmx/views.py
@@ -30,7 +30,7 @@ class LayerList(ListView):
     template_name = 'main/search/results.html'
     model = Layer
     context_object_name = 'layers'
-    paginate_by = 24
+    paginate_by = 30
 
     @property
     def filter_fields(self):
