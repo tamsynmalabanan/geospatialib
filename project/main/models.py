2da53ad5d05c144ad52dbffd0c88474eb3bc2dc3
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-28 10:45:04 +0530

    Translate layer data before onboarding

--- project/main/models.py
+++ project/main/models.py
@@ -136,7 +136,9 @@ class Layer(models.Model):
             print(e)
 
     def normalize_keywords(self):
-        self.keywords = sorted(set([str(k).strip().lower() for k in self.keywords]))
+        # db_version = self.db_version
+        if self.keywords: # and (not db_version or (set(db_version.keywords) != set(self.keywords))):
+            self.keywords = sorted(set([str(k).strip().lower() for k in self.keywords]))
 
     def save(self, *args, **kwargs):
         self.normalize_keywords()
