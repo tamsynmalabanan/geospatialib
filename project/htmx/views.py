6faa09428dec4d35db9d789454789d382daa96dd
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-31 12:43:08 +0530

    polish search query

--- project/htmx/views.py
+++ project/htmx/views.py
@@ -113,6 +113,9 @@ class LayerList(ListView):
             .annotate(
                 rank=SearchRank(F('search_vector'), SearchQuery(' OR '.join(self.clean_keywords[0]), search_type='websearch'))
             )
+            .filter(
+                rank__gte=0.025
+            )
         )
 
         return queryset
