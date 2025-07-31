9ed4fe6c6aa7e6f929c284fb90abe415f759fd95
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-07-31 12:10:11 +0530

    polish search query

--- project/htmx/views.py
+++ project/htmx/views.py
@@ -108,8 +108,8 @@ class LayerList(ListView):
                 rank=SearchRank(F('search_vector'), search_query)
             )
             .filter(
-                # search_vector=search_query,
-                rank__gte=0.001
+                search_vector=search_query,
+                # rank__gte=0.001
             )
         )
 
