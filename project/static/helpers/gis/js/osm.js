765881e5d12c021ac205d183b130eb8bfe3e15c7
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-08-14 13:25:01 +0530

    Onboard osmtogeojson

--- project/static/helpers/gis/js/osm.js
+++ project/static/helpers/gis/js/osm.js
@@ -83,7 +83,7 @@ const fetchOverpass = async (params, {
 } = {}) => {
     const url = 'https://overpass-api.de/api/interpreter'    
     const body = "data="+encodeURIComponent(`[out:json][timeout:180];${query}out tags geom body;`)
-    
+
     return fetchTimeout(url, {
         abortBtns,
         controller,
@@ -91,9 +91,7 @@ const fetchOverpass = async (params, {
             const data = await parseJSONResponse(response)
             if (!data) return
 
-            const elements = data.elements?.filter(element => element.tags)
-            delete data.elements
-            return await overpassToGeoJSON(elements, {controller, properties:data})
+            return osmtogeojson(data)
         },
         fetchParams: {
             method: "POST",
@@ -102,116 +100,4 @@ const fetchOverpass = async (params, {
     }).catch(error => {
         console.log(error)
     })
-}
-
-const overpassToGeoJSON = async (data, {
-    controller,
-    properties = {},
-} = {}) => {
-    const geojson = turf.featureCollection([])
-    for (const key in properties) geojson[key] = properties[key]
-
-    for (const element of data) {
-        if (controller?.signal?.aborted) return
-
-        const id = element.id
-        const type = element.type
-        const tags = element.tags || {}
-    
-        const feature = turf.feature(
-            geom=null,
-            properties={...tags,
-                osm_id: id,
-                osm_type: type,
-            }
-        )
-    
-        if (type === 'relation') {
-            const points = []
-            const polygons = []
-            const linestrings = []
-    
-            element.members.forEach(member => {
-                const memberType = member.type
-    
-                if (memberType === 'node') {
-                    points.push(member)
-                } else if (member.geometry) {
-                    const firstCoords = member.geometry[0]
-                    const lastCoords = member.geometry[member.geometry.length-1]
-    
-                    if (firstCoords.lat === lastCoords.lat && firstCoords.lon === lastCoords.lon) {
-                        polygons.push(member)
-                    } else {
-                        linestrings.push(member)
-                    }
-                }
-            })
-    
-            if (points.length) {
-                const featureMpt = turf.clone(feature)
-                featureMpt.geometry = {
-                    type: 'MultiPoint',
-                    coordinates: points.map(point => [parseFloat(point.lon), parseFloat(point.lat)])
-                }
-                geojson.features.push(featureMpt)
-            }
-    
-            if (linestrings.length) {
-                const featureMls = turf.clone(feature)
-                featureMls.geometry = {
-                    type: 'MultiLineString',
-                    coordinates: linestrings.map(line => line.geometry.map(coords => [parseFloat(coords.lon), parseFloat(coords.lat)]))
-                }
-                geojson.features.push(featureMls)
-    
-            }
-    
-            if (polygons.length) {
-                const outerGeoms = []
-                const innerGeoms = []
-    
-                polygons.forEach(polygon => {
-                    const polygonGeom = polygon.geometry.map(coords => [parseFloat(coords.lon), parseFloat(coords.lat)])
-                    if (polygon.role === 'inner') {
-                        innerGeoms.push(polygonGeom)
-                    } else {
-                        outerGeoms.push(polygonGeom)
-                    }
-                })
-    
-                const featureMp = turf.clone(feature)
-                featureMp.geometry = {
-                    type: 'MultiPolygon',
-                    coordinates: [outerGeoms, innerGeoms].filter(i => i.length)
-                }
-                geojson.features.push(featureMp)
-            }
-    
-        } else {
-            if (type === 'node') {
-                feature.geometry = {
-                    type: 'Point',
-                    coordinates: [parseFloat(element.lon), parseFloat(element.lat)]
-                }
-            }
-    
-            if (type === 'way') {
-                const firstCoords = element.geometry[0]
-                const lastCoords = element.geometry[element.geometry.length-1]
-                
-                const featureType = firstCoords.lat === lastCoords.lat && firstCoords.lon === lastCoords.lon ? 'Polygon' : 'LineString'
-                const coordinates = element.geometry.map(coords => [parseFloat(coords.lon), parseFloat(coords.lat)])
-                
-                feature.geometry = {
-                    type: featureType,
-                    coordinates: featureType === 'Polygon' ? [coordinates] : coordinates
-                }
-            }
-    
-            geojson.features.push(feature)
-        }
-    }
-
-    return geojson
 }
