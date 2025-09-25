5fde6460eb4784c7dab82541594ab52ce9ce8047
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-09-25 14:17:55 +0530

    Config web socket

--- project/project/settings.py
+++ project/project/settings.py
@@ -416,11 +416,10 @@ ASGI_APPLICATION = 'project.asgi.application'
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
-    # if DEBUG else {
-    #     "BACKEND": "channels_redis.core.RedisChannelLayer",
-    #     "CONFIG": {
-    #         "hosts": [("{REDIS_IP}", 6379)],
-    #     },
-    # },
+    } if DEBUG else {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("{REDIS_IP}", 6379)],
+        },
+    },
 }
