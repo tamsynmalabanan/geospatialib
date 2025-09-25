a3d0b442e3ce9c95ce7214d9f7cab43af7007120
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-09-25 14:04:56 +0530

    Config web socket

--- project/main/consumers.py
+++ project/main/consumers.py
@@ -30,9 +30,3 @@ class ThematicMapConsumer(WebsocketConsumer):
             'data': event['data']
         })
         self.send(text_data=html)
-
-    # def receive(self, text_data):
-    #     text_data_json = json.loads(text_data)
-    #     message = text_data_json["message"]
-
-    #     self.send(text_data=json.dumps({"message": message}))
