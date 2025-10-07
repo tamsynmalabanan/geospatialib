e3eb8e051d58206ac8fc5ae509446abbb44a35fc
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-10-07 11:11:36 +0530

    Config web socket

--- project/main/middleware.py
+++ project/main/middleware.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseForbidden
 class BlockScannerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.blocked_paths = ['/version', '/.env', '/.env.save']
+        self.blocked_paths = ['/version', '/.env', '/.env.save', '/robots.txt']
 
     def __call__(self, request):
         if request.path in self.blocked_paths:
