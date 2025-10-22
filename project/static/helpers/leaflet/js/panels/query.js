e36249d3425ae5eb1e9fa1bedb0159560c380608
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2025-10-23 00:00:32 +0530

    query osm overpass suggest tags based on keyword

--- project/static/helpers/leaflet/js/panels/query.js
+++ project/static/helpers/leaflet/js/panels/query.js
@@ -191,16 +191,16 @@ const handleLeafletQueryPanel = (map, parent) => {
                                 filterDatalist.innerHTML = ''
     
                                 let tags = []
-    
-                                const by_keyword = await (await fetchTimeout(`https://taginfo.openstreetmap.org/api/4/search/by_keyword?query=${e.target.value}`))?.json() ?? []
-                                if (by_keyword.total <= 100) {
-                                    by_keyword.data.forEach(i => tags.push(`["${i.key}"${i.value ? `="${i.value}"` : ''}]`))
-                                }
                                 
                                 const by_value = await (await fetchTimeout(`https://taginfo.openstreetmap.org/api/4/search/by_value?query=${e.target.value}`))?.json() ?? []
-                                by_value.data.reverse().splice(0, 100).forEach(i => {
+                                by_value.data.reverse().splice(0, 50).forEach(i => {
                                     tags.push(`["${i.key}"${i.value ? `="${i.value}"` : ''}]`)
                                 })
+    
+                                const by_keyword = await (await fetchTimeout(`https://taginfo.openstreetmap.org/api/4/search/by_keyword?query=${e.target.value}`))?.json() ?? []
+                                if (by_keyword.total <= 50) {
+                                    by_keyword.data.forEach(i => tags.push(`["${i.key}"${i.value ? `="${i.value}"` : ''}]`))
+                                }
     
                                 tags = Array.from(new Set(tags))
                                 tags.forEach(i => filterDatalist.appendChild(customCreateElement({
