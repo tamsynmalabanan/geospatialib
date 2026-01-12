a8753ed4b3f5c224724cc7c6f4a5260a0e2fca35
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2026-01-12 10:09:16 +0530

    updated logout to logout_account

--- project/customuser/urls.py
+++ project/customuser/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'customuser'
 
 urlpatterns = [
-        path("logout/", LogoutView.as_view(), name="logout"),
+        path("logout/", LogoutView.as_view(), name="logout_account"),
         path("delete/", views.delete_account, name="delete_account"),
 ]
