059153db868f6b55c4c2ea8b7d9cc837cb541507
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2026-01-12 10:02:31 +0530

    init new

--- project/static/helpers/gis/js/init.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    window.SQL = await initSqlJs({
-        locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.6.2/${file}`
-    })
-    window.Buffer = require('buffer').Buffer
-    window.wkx = require('wkx')
-})
