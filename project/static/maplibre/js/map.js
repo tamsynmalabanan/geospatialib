6e1fcafbdcf5f51338cefa0587a88436ab7aa7ca
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2026-01-13 11:28:23 +0530

    new

---
+++ project/static/maplibre/js/map.js
@@ -0,0 +1,53 @@
+const createNewMap = (el) => {
+    const map = new maplibregl.Map({
+        container: el.id,
+        zoom: 0,
+        center: [0,0],
+        pitch: 0,
+        hash: false,
+        style: {
+            version: 8,
+            sources: {
+                basemap: {
+                    type: 'raster',
+                    tileSize: 256,
+                    maxzoom: 20,
+                    tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+                    attribution: '&copy; OpenStreetMap Contributors',
+                },
+                terrain: {
+                    type: 'raster-dem',
+                    tiles: ['https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png'],
+                    tileSize: 256,
+                    attribution: 'Terrain Tiles © Mapzen, <a href="https://registry.opendata.aws/terrain-tiles/" target="_blank">Registry of Open Data on AWS</a>',
+                    encoding: 'terrarium' 
+                },
+            },
+            layers: [
+                {
+                    id: 'basemap',
+                    type: 'raster',
+                    source: 'basemap',
+                },         
+            ],
+            sky: {
+                "sky-color": "#88C6FC",
+                "horizon-color": "#ffffff",
+                "fog-color": "#ffffff",
+                "fog-ground-blend": 0.5,
+                "horizon-fog-blend": 0.8,
+                "sky-horizon-blend": 0.8,
+                "atmosphere-blend": 0.8
+            }
+        },
+        maxZoom: 22,
+        maxPitch: 85
+    })
+
+    map.on('style.load', () => {
+        // const settings = new SettingsControl()
+        // map.addControl(settings, 'bottom-right')
+        // settings.syncTheme()
+        console.log(map)
+    })   
+}
